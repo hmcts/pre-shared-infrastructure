de42253065740c3e65c17efa2adc2558e32b84a6
Damon Green <damon.green@btinternet.com>
2021-10-22 11:01:14 +0100

    Initial

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "key-vault" {
   resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_platformengineering group object ID
-  product_group_name = "dcd_divorce"
+  product_group_name = "dcd_pre"
   common_tags                = var.common_tags
   create_managed_identity    = true
 }
@@ -69,27 +69,4 @@ resource "azurerm_application_insights" "appinsights_preview" {
       application_type,
     ]
   }
-}
-
-
-
-resource "azurerm_application_insights_web_test" "appinsights-2" {
-  name = "nfdiv-webtest"
-  location = var.appinsights_location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_insights_id = azurerm_application_insights.appinsights.id
-  kind = "ping"
-  frequency = 300
-  timeout = 60
-  enabled = true
-  retry_enabled = true
-  geo_locations = [
-    "emea-se-sto-edge",
-    "apac-sg-sin-azr",
-    "us-il-ch1-azr",
-    "emea-gb-db3-azr",
-    "emea-ru-msa-edge"]
-
-  configuration = "<WebTest Name=\"manual1-dg\" Id=\"2723c2f5-fa3a-4ac2-832c-8444bd8f8da5\" Enabled=\"True\"   CssProjectStructure=\"\"         CssIteration=\"\"         Timeout=\"120\"         WorkItemIds=\"\"         xmlns=\"http://microsoft.com/schemas/VisualStudio/TeamTest/2010\"         Description=\"\"         CredentialUserName=\"\"         CredentialPassword=\"\"         PreAuthenticate=\"True\"         Proxy=\"default\"         StopOnError=\"False\"         RecordedResultFile=\"\"         ResultsLocale=\"\">        <Items>        <Request         Method=\"GET\"         Guid=\"a5eb315b-d699-bdd6-e527-43120955cb86\"         Version=\"1.1\"         Url=\"http://www.google.com\"         ThinkTime=\"0\"         Timeout=\"120\"         ParseDependentRequests=\"False\"         FollowRedirects=\"True\"         RecordResult=\"True\"         Cache=\"False\"         ResponseTimeGoal=\"0\"         Encoding=\"utf-8\"         ExpectedHttpStatusCode=\"200\"         ExpectedResponseUrl=\"\"         ReportingName=\"\"         IgnoreHttpStatusCode=\"False\" />        </Items>        </WebTest>"
-  count = var.custom_alerts_enabled ? 1 : 0
 }
