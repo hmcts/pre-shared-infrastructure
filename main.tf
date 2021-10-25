2dc491127a2a6a983d1da799ea6761b27b6edd03
Damon Green <damon.green@btinternet.com>
2021-10-25 16:52:56 +0100

    remove AI secret copy & preview AI

--- main.tf
+++ main.tf
@@ -23,11 +23,11 @@ module "key-vault" {
   create_managed_identity    = true
 }
 
-resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
-  name         = "AppInsightsInstrumentationKey"
-  value        = azurerm_application_insights.appinsights.instrumentation_key
-  key_vault_id = module.key-vault.key_vault_id
-}
+//resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
+//  name         = "AppInsightsInstrumentationKey"
+//  value        = azurerm_application_insights.appinsights.instrumentation_key
+//  key_vault_id = module.key-vault.key_vault_id
+//}
 
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-appinsights-${var.env}"
@@ -46,27 +46,5 @@ resource "azurerm_application_insights" "appinsights" {
   }
 }
 
-resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY_PREVIEW" {
-  name         = "AppInsightsInstrumentationKey-Preview"
-  value        = azurerm_application_insights.appinsights_preview[0].instrumentation_key
-  key_vault_id = module.key-vault.key_vault_id
-  count = var.env == "aat" ? 1 : 0
-}
 
-resource "azurerm_application_insights" "appinsights_preview" {
-  name                = "${var.product}-appinsights-preview"
-  location            = var.appinsights_location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
-  count = var.env == "aat" ? 1 : 0
-
-  tags = var.common_tags
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
-      # destroys and re-creates this appinsights instance..
-      application_type,
-    ]
-  }
 }
