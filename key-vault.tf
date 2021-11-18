c804ed59a3848bc177de250411c3072ba0e70cfa
Jason Paige <jason.r.paige@gmail.com>
2021-11-18 15:14:40 +0000

    fix for referencing username and password strings

--- key-vault.tf
+++ key-vault.tf
@@ -30,13 +30,13 @@ resource "random_password" "vm_password" {
 resource "azurerm_key_vault_secret" "vm_username_secret" {
   count        = var.num_vid_edit_vms
   name         = "videditvm${count.index}-username"
-  value        = random_string.vm_username[count.index]
+  value        = random_string.vm_username[count.index].result
   key_vault_id = module.key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "vm_password_secret" {
   count        = var.num_vid_edit_vms
   name         = "videditvm${count.index}-password"
-  value        = random_password.vm_password[count.index]
+  value        = random_password.vm_password[count.index].result
   key_vault_id = module.key-vault.key_vault_id
 }
