a8fbf18aab6c9fa5b14a4627162ef87aa3261899
Jason Paige <jason.r.paige@gmail.com>
2021-11-18 16:55:27 +0000

    default computer name is too long

--- vms.tf
+++ vms.tf
@@ -14,6 +14,7 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_windows_virtual_machine" "vm" {
   count               = var.num_vid_edit_vms
   name                = "${var.product}-videditvm${count.index}-${var.env}"
+  computer_name       = "PREVIDED0${count.index}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = var.vid_edit_vm_spec
