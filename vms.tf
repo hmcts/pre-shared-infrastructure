224fa7a62f304eec5bc635db5455a054632571de
Jason Paige <jason.r.paige@gmail.com>
2021-11-19 11:56:13 +0000

    sort username out

--- vms.tf
+++ vms.tf
@@ -27,7 +27,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = var.vid_edit_vm_spec
-  admin_username      = random_string.vm_username[count.index].result
+  admin_username      = "videdit${count.index}_${random_string.vm_username[count.index].result}"
   admin_password      = random_password.vm_password[count.index].result
   network_interface_ids = [
     azurerm_network_interface.nic[count.index].id,
