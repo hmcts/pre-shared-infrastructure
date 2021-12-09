3a8271f44e8cc54c00f0abc5dc01787fc0017d56
Jason Paige <jason.r.paige@gmail.com>
2021-12-09 14:08:48 +0000

    fix

--- sa.tf
+++ sa.tf
@@ -24,7 +24,7 @@ module "final_storage_account" {
   sa_subnets               = []
   containers = [{
     name        = "final"
-    access_type = "private"
+    access_type = "blob"
   }]
 
   common_tags = var.common_tags
