928384cc5a2a5b90f267f8c4d77c0b10777cb362
Jason Paige <jason.r.paige@gmail.com>
2021-12-10 10:25:55 +0000

    Enable AMS again

--- ams.tf
+++ ams.tf
@@ -1,10 +1,10 @@
-//resource "azurerm_media_services_account" "ams" {
-//  name                = "${var.product}ams${var.env}"
-//  location            = azurerm_resource_group.rg.location
-//  resource_group_name = azurerm_resource_group.rg.name
-//
-//  storage_account {
-//    id         = module.ams_storage_account.storageaccount_id
-//    is_primary = true
-//  }
-//}
+resource "azurerm_media_services_account" "ams" {
+  name                = "${var.product}ams${var.env}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  storage_account {
+    id         = module.ams_storage_account.storageaccount_id
+    is_primary = true
+  }
+}
