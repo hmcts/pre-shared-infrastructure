5396eda12962f767c2addb7ae2a916a33166d263
Don-Olu <98063921+Don-Olu@users.noreply.github.com>
2022-02-16 13:58:25 +0000

    PR-19 (#19)
    * vnet and storage lock issue
    
    * vnet and storage lock issue
    
    * tags
    
    * tags
    
    * update the networks
    
    * vms
    
    * jenkins parameter
    
    * jenkins parameter
    
    * jenkins parameter
    
    * vm pip
    
    * vm pip
    
    * streaming sa
    
    * streaming sa
    
    * streaming sa
    
    * feedback from PR
    
    * feedback from PR

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "location" {
 variable "env" {}
 variable "jenkins_AAD_objectId" {}
 
+
 variable "common_tags" {
   type = map(string)
 }
@@ -21,25 +22,25 @@ variable "sa_replication_type" {
 
 variable "vnet_address_space" {}
 
-variable "snet01_address_prefix" {}
+variable "video_edit_vm_snet_address" {}
 
-variable "snet02_address_prefix" {}
+variable "privatelink_snet_address" {}
 
-variable "snet03_address_prefix" {}
+variable "bastion_snet_address" {}
 
-variable "snet04_address_prefix" {}
+variable "data_gateway_snet_address" {}
 
 variable "mgmt_net_name" {}
 
-variable mgmt_net_rg_name {}
-
+variable "mgmt_net_rg_name" {}
 
 variable "num_vid_edit_vms" {
   default = 1
 }
 variable "vid_edit_vm_spec" {
-  default = "Standard_E4s_v3"
+  default = "Standard_E2s_v4"
 }
+
 variable "mgmt_subscription_id" {}
 variable "power_app_user_oid" {
   default = "56a29187-3d5f-4262-99d6-c635776e0eac"
