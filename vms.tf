916c5f31b0d5e8c61d6ba4c96a797481171a2021
Olu Ayodele <olu.ayodele@hmcts.net>
2022-02-16 13:01:42 +0000

    feedback from PR

--- vms.tf
+++ vms.tf
@@ -71,7 +71,7 @@ resource "azurerm_bastion_host" "bastion" {
 
   ip_configuration {
     name                          = "bastionpublic"
-    subnet_id                     = azurerm_virtual_network.vnet.subnet.AzureBastionSubnet.id
+    subnet_id                     = azurerm_virtual_network.vnet.subnet.*.id[2]
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
   tags = var.common_tags
