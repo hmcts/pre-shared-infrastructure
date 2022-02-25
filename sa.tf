3586c5755f72870e0534761b2ae1aa7ca5b8ae79
Olu Ayodele <olu.ayodele@hmcts.net>
2022-02-25 00:04:14 +0000

    storageprivate endpoint

--- sa.tf
+++ sa.tf
@@ -79,32 +79,16 @@ module "streaming_storage_account" {
   account_replication_type = var.sa_replication_type
   # sa_subnets               = concat([data.azurerm_subnet.jenkins_subnet.id], slice(azurerm_virtual_network.vnet.subnet.*.id, 0, 1))
   sa_subnets               = concat([data.azurerm_subnet.jenkins_subnet.id], azurerm_virtual_network.vnet.subnet.*.id)
+  ip_rules                 = []
+  allow_blob_public_access = false
+  default_action           = "Deny"
   containers = [{
     name        = "streaming"
     access_type = "private"
   }]
-
+  depends_on = [azurerm_virtual_network.vnet.subnet.*.id]
   common_tags = var.common_tags
 }
-
-# module "sa_storage_account" {
-#   source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-#   env                      = var.env
-#   storage_account_name     = replace("${var.product}sa${var.env}", "-", "")
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_kind             = "StorageV2"
-#   account_tier             = var.sa_account_tier
-#   account_replication_type = var.sa_replication_type
-#   # sa_subnets               = concat([data.azurerm_subnet.jenkins_subnet.id], slice(azurerm_virtual_network.vnet.subnet.*.id, 0, 1))
-#   sa_subnets               = concat([data.azurerm_subnet.jenkins_subnet.id], azurerm_virtual_network.vnet.subnet.*.id)
-#   containers = [{
-#     name        = "final"
-#     access_type = "private"
-#   }]
-
-#  common_tags = var.common_tags
-# }
 
 ###################################################
 #                PRIVATE ENDPOINTS FOR STORAGES              #
@@ -180,11 +164,7 @@ resource "azurerm_key_vault_secret" "streaming_storage_account_connection_string
 }
 
 
-resource "azurerm_key_vault_secret" "sa_storage_account_connection_string" {
-  name         = "sa-storage-account-connection-string"
-  value        = module.sa_storage_account.storageaccount_primary_connection_string
-  key_vault_id = module.key-vault.key_vault_id
-}
+
 
 output "ams_storage_account_name" {
   value = module.ams_storage_account.storageaccount_name
@@ -197,19 +177,10 @@ output "final_storage_account_name" {
 output "final_storage_account_id" {
   value = module.final_storage_account.storageaccount_id
 }
-
-output "sa_storage_account_id" {
-  value = module.sa_storage_account.storageaccount_id
-}
-
-
 output "streaming_storage_account_name" {
   value = module.streaming_storage_account.storageaccount_name
 }
 
-output "sa_storage_account_name" {
-  value = module.sa_storage_account.storageaccount_name
-}
 output "ams_storage_account_primary_key" {
   sensitive = true
   value     = module.ams_storage_account.storageaccount_primary_access_key
@@ -222,9 +193,4 @@ output "final_storage_account_primary_key" {
 output "streaming_storage_account_primary_key" {
   sensitive = true
   value     = module.streaming_storage_account.storageaccount_primary_access_key
-}
-
-output "sa_storage_account_primary_key" {
-  sensitive = true
-  value     = module.sa_storage_account.storageaccount_primary_access_key
 }
