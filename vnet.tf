04d4723d7b73a56a092288b3314f201019963df9
Don-Olu <98063921+Don-Olu@users.noreply.github.com>
2022-02-28 22:09:08 +0000

    service sendpoint (#33)
    * service endpoint

--- vnet.tf
+++ vnet.tf
@@ -7,14 +7,17 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = [var.vnet_address_space]
 
   subnet {
-    name           = "${var.product}-videoeditvm-snet-${var.env}"
-    address_prefix = var.video_edit_vm_snet_address
+    name                  = "${var.product}-videoeditvm-snet-${var.env}"
+    address_prefix        = var.video_edit_vm_snet_address
   }
 
-  subnet {
-    name           = "${var.product}-privatelink-snet-${var.env}"
-    address_prefix = var.privatelink_snet_address
-  }
+  # subnet {
+  #   name           = "${var.product}-privatendpt-snet-${var.env}"
+  #   address_prefix = var.privatendpt_snet_address
+  #   service_endpoints     = ["Microsoft.Storage"]
+  #   # enforce_private_link_endpoint_network_policies = false
+  # // enforce_private_link_service_network_policies = false
+  # }
   subnet {
     name           = "AzureBastionSubnet" 
     address_prefix = var.bastion_snet_address
@@ -36,16 +39,26 @@ resource "azurerm_virtual_network" "vnet" {
 //  service_endpoints    = ["Microsoft.Storage"]
 //}
 //
-//resource "azurerm_subnet" "vm_subnet" {
-//  name                 = "${var.product}-snet02-${var.env}"
-//  resource_group_name  = azurerm_resource_group.rg.name
-//  virtual_network_name = azurerm_virtual_network.vnet.name
-//  address_prefixes     = var.privatelink_snet_address
-//  service_endpoints    = ["Microsoft.Storage"]
-//}
 
 
+#------------------------------------------------------###################
+# ENDPOINT SUBNET
+#------------------------------------------------------###################
+resource "azurerm_subnet" "endpoint_subnet" {
+ name                  = "${var.product}-privatendpt-snet-${var.env}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes       = [var.privatendpt_snet_address]
+  service_endpoints    = ["Microsoft.Storage"]
+  enforce_private_link_endpoint_network_policies = true
+}
 
-# output "subnet_ids" {
-#    value = (azurerm_virtual_network.vnet.subnet)[*].id
-# }
+output "subnet_ids" {
+   value = azurerm_virtual_network.vnet.subnet[*].id
+}
+
+
+output "private_endpt_subnet_ids" {
+   value = azurerm_subnet.endpoint_subnet.id
+  #  "${data.azurerm_virtual_network.test.id}"
+}
