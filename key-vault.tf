82150a3814554279ece00762ec0c7e7510f09579
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-01 16:33:57 +0000

    clean all

--- key-vault.tf
+++ key-vault.tf
@@ -1,16 +1,16 @@
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
 
-module "key-vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product                 = var.product
-  env                     = var.env
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = var.jenkins_AAD_objectId
-  resource_group_name     = azurerm_resource_group.rg.name
-  product_group_name      = "DTS Pre-recorded Evidence"
-  common_tags             = var.common_tags
-  create_managed_identity = true
-}
+# module "key-vault" {
+#   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+#   product                 = var.product
+#   env                     = var.env
+#   tenant_id               = data.azurerm_client_config.current.tenant_id
+#   object_id               = var.jenkins_AAD_objectId
+#   resource_group_name     = azurerm_resource_group.rg.name
+#   product_group_name      = "DTS Pre-recorded Evidence"
+#   common_tags             = var.common_tags
+#   create_managed_identity = true
+# }
 
 # // Power App Permissions
 # resource "azurerm_key_vault_access_policy" "power_app_access" {
