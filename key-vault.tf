aef9989230eb03164271d77337be4393e82f48c5
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-01 15:34:56 +0000

    clean all

--- key-vault.tf
+++ key-vault.tf
@@ -1,80 +1,80 @@
-# data "azurerm_client_config" "current" {}
+data "azurerm_client_config" "current" {}
 
-# module "key-vault" {
-#   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-#   product                 = var.product
-#   env                     = var.env
-#   tenant_id               = data.azurerm_client_config.current.tenant_id
-#   object_id               = var.jenkins_AAD_objectId
-#   resource_group_name     = azurerm_resource_group.rg.name
-#   product_group_name      = "DTS Pre-recorded Evidence"
-#   common_tags             = var.common_tags
-#   create_managed_identity = true
-# }
+module "key-vault" {
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_name      = "DTS Pre-recorded Evidence"
+  common_tags             = var.common_tags
+  create_managed_identity = true
+}
 
-# // Power App Permissions
-# resource "azurerm_key_vault_access_policy" "power_app_access" {
-#   key_vault_id = module.key-vault.key_vault_id
-#   object_id    = var.power_app_user_oid
-#   tenant_id    = data.azurerm_client_config.current.tenant_id
+// Power App Permissions
+resource "azurerm_key_vault_access_policy" "power_app_access" {
+  key_vault_id = module.key-vault.key_vault_id
+  object_id    = var.power_app_user_oid
+  tenant_id    = data.azurerm_client_config.current.tenant_id
 
-#   key_permissions = [
-#     "list",
-#     "update",
-#     "create",
-#     "import",
-#     "delete",
-#   ]
+  key_permissions = [
+    "list",
+    "update",
+    "create",
+    "import",
+    "delete",
+  ]
 
-#   certificate_permissions = [
-#     "list",
-#     "update",
-#     "create",
-#     "import",
-#     "delete",
-#     "managecontacts",
-#     "manageissuers",
-#     "getissuers",
-#     "listissuers",
-#     "setissuers",
-#     "deleteissuers",
-#   ]
+  certificate_permissions = [
+    "list",
+    "update",
+    "create",
+    "import",
+    "delete",
+    "managecontacts",
+    "manageissuers",
+    "getissuers",
+    "listissuers",
+    "setissuers",
+    "deleteissuers",
+  ]
 
-#   secret_permissions = [
-#     "list",
-#     "set",
-#     "delete",
-#   ]
-# }
+  secret_permissions = [
+    "list",
+    "set",
+    "delete",
+  ]
+}
 
-# // VM credentials
+// VM credentials
 
-# resource "random_string" "vm_username" {
-#   count   = var.num_vid_edit_vms
-#   length  = 4
-#   special = false
-# }
+resource "random_string" "vm_username" {
+  count   = var.num_vid_edit_vms
+  length  = 4
+  special = false
+}
 
-# resource "random_password" "vm_password" {
-#   count            = var.num_vid_edit_vms
-#   length           = 16
-#   special          = true
-#   override_special = "_%@$"
-# }
+resource "random_password" "vm_password" {
+  count            = var.num_vid_edit_vms
+  length           = 16
+  special          = true
+  override_special = "_%@$"
+}
 
-# resource "azurerm_key_vault_secret" "vm_username_secret" {
-#   count        = var.num_vid_edit_vms
-#   name         = "videditvm${count.index}-username"
-#   value        = "videdit${count.index}_${random_string.vm_username[count.index].result}"
-#   key_vault_id = module.key-vault.key_vault_id
-# }
+resource "azurerm_key_vault_secret" "vm_username_secret" {
+  count        = var.num_vid_edit_vms
+  name         = "videditvm${count.index}-username"
+  value        = "videdit${count.index}_${random_string.vm_username[count.index].result}"
+  key_vault_id = module.key-vault.key_vault_id
+}
 
-# resource "azurerm_key_vault_secret" "vm_password_secret" {
-#   count        = var.num_vid_edit_vms
-#   name         = "videditvm${count.index}-password"
-#   value        = random_password.vm_password[count.index].result
-#   key_vault_id = module.key-vault.key_vault_id
-# }
+resource "azurerm_key_vault_secret" "vm_password_secret" {
+  count        = var.num_vid_edit_vms
+  name         = "videditvm${count.index}-password"
+  value        = random_password.vm_password[count.index].result
+  key_vault_id = module.key-vault.key_vault_id
+}
 
 # ###################################################
 # #                PRIVATE ENDPOINT                 #
