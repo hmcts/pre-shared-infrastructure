b66eaffb8ba45f8692e226a9eeee50463a0e986e
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-01 18:16:07 +0000

    clean all

--- main.tf
+++ main.tf
@@ -3,10 +3,3 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = var.common_tags
 }
-
-resource "null_resource" "cleanall" {
-  provisioner "local-exec" {
-    command = "$resources = az resource list --resource-group pre-stg | ConvertFrom-Json; foreach ($resource in $resources) {az resource delete --resource-group pre-sbox --ids $resource.id --verbose}"
-    interpreter = ["PowerShell", "-Command"]
-  }
-}
