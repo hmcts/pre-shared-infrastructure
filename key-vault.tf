4dc01dea2cab9f6ff7fe5e2b911eb56c11b25481
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-01 15:40:45 +0000

    clean all

--- key-vault.tf
+++ key-vault.tf
@@ -49,32 +49,32 @@ resource "azurerm_key_vault_access_policy" "power_app_access" {
 
 // VM credentials
 
-resource "random_string" "vm_username" {
-  count   = var.num_vid_edit_vms
-  length  = 4
-  special = false
-}
+# resource "random_string" "vm_username" {
+#   count   = var.num_vid_edit_vms
+#   length  = 4
+#   special = false
+# }
 
-resource "random_password" "vm_password" {
-  count            = var.num_vid_edit_vms
-  length           = 16
-  special          = true
-  override_special = "_%@$"
-}
+# resource "random_password" "vm_password" {
+#   count            = var.num_vid_edit_vms
+#   length           = 16
+#   special          = true
+#   override_special = "_%@$"
+# }
 
-resource "azurerm_key_vault_secret" "vm_username_secret" {
-  count        = var.num_vid_edit_vms
-  name         = "videditvm${count.index}-username"
-  value        = "videdit${count.index}_${random_string.vm_username[count.index].result}"
-  key_vault_id = module.key-vault.key_vault_id
-}
+# resource "azurerm_key_vault_secret" "vm_username_secret" {
+#   count        = var.num_vid_edit_vms
+#   name         = "videditvm${count.index}-username"
+#   value        = "videdit${count.index}_${random_string.vm_username[count.index].result}"
+#   key_vault_id = module.key-vault.key_vault_id
+# }
 
-resource "azurerm_key_vault_secret" "vm_password_secret" {
-  count        = var.num_vid_edit_vms
-  name         = "videditvm${count.index}-password"
-  value        = random_password.vm_password[count.index].result
-  key_vault_id = module.key-vault.key_vault_id
-}
+# resource "azurerm_key_vault_secret" "vm_password_secret" {
+#   count        = var.num_vid_edit_vms
+#   name         = "videditvm${count.index}-password"
+#   value        = random_password.vm_password[count.index].result
+#   key_vault_id = module.key-vault.key_vault_id
+# }
 
 # ###################################################
 # #                PRIVATE ENDPOINT                 #
