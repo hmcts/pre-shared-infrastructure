02402d739c76699617bf0014d0ac2f386fae4610
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-04 14:27:27 +0000

    remove ams

--- ams.tf
+++ ams.tf
@@ -1,19 +1,19 @@
-resource "azurerm_media_services_account" "ams" {
-  name                = "${var.product}ams${var.env}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_media_services_account" "ams" {
+#   name                = "${var.product}ams${var.env}"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  storage_account {
-    id         = module.ams_storage_account.storageaccount_id 
-    is_primary = true
-  }
+#   storage_account {
+#     id         = module.ams_storage_account.storageaccount_id 
+#     is_primary = true
+#   }
 
-  storage_account {
-    id         = module.final_storage_account.storageaccount_id 
-    is_primary = false
-  }
-  tags             = var.common_tags
-}
+#   storage_account {
+#     id         = module.final_storage_account.storageaccount_id 
+#     is_primary = false
+#   }
+#   tags             = var.common_tags
+# }
 
 #   key_delivery_access_control {
 #     ip_allow_list = []
