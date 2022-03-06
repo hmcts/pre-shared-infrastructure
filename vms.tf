757df8e17b220f88ce20a33e650a3f1063120e01
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-06 19:27:03 +0000

    2

--- vms.tf
+++ vms.tf
@@ -34,8 +34,10 @@ resource "azurerm_windows_virtual_machine" "vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = var.vid_edit_vm_spec
-  admin_username      = "videdit${count.index}_${random_string.vm_username[count.index].result}"
-  admin_password      = random_password.vm_password[count.index].result
+  admin_username      = "videdit${count.index}-pc-${var.env}"
+  #admin_username      = "videdit${count.index}_${random_string.vm_username[count.index].result}"
+  #admin_password      = random_password.vm_password[count.index].result
+  admin_password      = "Pa55W0rd**2020"
   network_interface_ids = [azurerm_network_interface.nic[count.index].id,]
 
   os_disk {
