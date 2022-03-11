4930e5e11a63947c1211213aa9ba6e1d87b2386e
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-11 20:42:26 +0000

    jenkins subnet

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-2-${var.env}"
+  name     = "${var.product}-${var.env}"
   location = var.location
   tags     = var.common_tags
 }
