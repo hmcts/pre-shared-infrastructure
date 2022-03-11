f2af5d804221143e2ddad083d69eb6019c54512a
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-11 22:19:50 +0000

    rg2

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ locals {
   key_vault_name      = "${var.product}-kv2-${var.env}"
   env_long_name       = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
 }
-data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = local.resource_group_name
