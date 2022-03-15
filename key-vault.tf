7c5bab8994b59eb2e5581a8ead5ff911005a8f45
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-15 23:18:55 +0000

    kvt

--- key-vault.tf
+++ key-vault.tf
@@ -149,18 +149,30 @@ resource "azurerm_key_vault_secret" "vm_password_secret" {
   key_vault_id = module.key-vault.key_vault_id
 }
 
+resource "random_string" "dtgtwy_username" {
+  count   = var.num_datagateway
+  length  = 4
+  special = false
+}
+
+resource "random_password" "dtgtwy_password" {
+  count            = var.num_datagateway
+  length           = 16
+  special          = true
+  override_special = "_%@$"
+}
 
 resource "azurerm_key_vault_secret" "dtgtwy_username_secret" {
-  count        = var.num_vid_edit_vms
+  count        = var.num_datagateway
   name         = "Dtgtwy${count.index}-username"
-  value        = "Dtgtwy${count.index}_${random_string.vm_username[count.index].result}"
+  value        = "Dtgtwy${count.index}_${random_string.dtgtwy_username[count.index].result}"
   key_vault_id = module.key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "dtgtwy_password_secret" {
-  count        = var.num_vid_edit_vms
+  count        = var.num_datagateway
   name         = "Dtgtwy${count.index}-password"
-  value        = random_password.vm_password[count.index].result
+  value        = random_password.dtgtwy_password[count.index].result
   key_vault_id = module.key-vault.key_vault_id
 }
 
