aeed1f5dd8c20455589bedf307d26b8421ee076c
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-21 23:03:05 +0000

    ams to ingestsa

--- ams.tf
+++ ams.tf
@@ -4,7 +4,7 @@ resource "azurerm_media_services_account" "ams" {
   resource_group_name = azurerm_resource_group.rg.name
 
   storage_account {
-    id         = module.ingest_storage_account.storageaccount_id 
+    id         = module.ingestsa_storage_account.storageaccount_id 
     is_primary = true
   }
 
