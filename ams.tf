aef8bff1d6a9dffa90dbc0d7f88fc3f6089dd0a0
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-22 00:09:22 +0000

    ams added

--- ams.tf
+++ ams.tf
@@ -1,19 +1,19 @@
-# resource "azurerm_media_services_account" "ams" {
-#   name                = "${var.product}ams${var.env}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_media_services_account" "ams" {
+  name                = "${var.product}ams${var.env}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-#   storage_account {
-#     id         = module.ingestsa_storage_account.storageaccount_id 
-#     is_primary = true
-#   }
+  storage_account {
+    id         = module.ingestsa_storage_account.storageaccount_id 
+    is_primary = true
+  }
 
-#   storage_account {
-#     id         = module.finalsa_storage_account.storageaccount_id 
-#     is_primary = false
-#   }
-#   tags             = var.common_tags
-# }
-# #####TODO
-# # Modify the stroage account
+  storage_account {
+    id         = module.finalsa_storage_account.storageaccount_id 
+    is_primary = false
+  }
+  tags             = var.common_tags
+}
+#####TODO
+# Modify the stroage account
 
