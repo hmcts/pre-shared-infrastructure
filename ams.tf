00050605a83bbc8db3069b1dd50e82571bbd3ab6
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-24 15:33:52 +0000

    Revert "media transform"
    This reverts commit 2c6efa266ad97392f6664eff744b24416bdb9059.

--- ams.tf
+++ ams.tf
@@ -18,7 +18,7 @@
 # Modify the stroage account
 
 
-resource "azurerm_media_transform " "analysevideo" {
+resource "azurerm_media_transform" "analysevideo" {
   name                        = "AnalyseVideo"
   resource_group_name         = azurerm_resource_group.rg.name
   media_services_account_name = azurerm_media_services_account.ams.name
@@ -32,7 +32,7 @@ resource "azurerm_media_transform " "analysevideo" {
   }
 }
 
-resource "azurerm_media_transform " "EncodeToMP4" {
+resource "azurerm_media_transform" "EncodeToMP4" {
   name                        = "EncodeToMP4"
   resource_group_name         = azurerm_resource_group.rg.name
   media_services_account_name = azurerm_media_services_account.ams.name
