3d2b5e8161c109ca8dfb7395331976f56da49a56
Olu Ayodele <olu.ayodele@hmcts.net>
2022-03-25 18:17:45 +0000

    stgwithams

--- ams.tf
+++ ams.tf
@@ -15,19 +15,19 @@
   tags             = var.common_tags
 }
 
-# resource "azurerm_media_transform" "analysevideo" {
-#   name                        = "AnalyseVideo"
-#   resource_group_name         = azurerm_resource_group.rg.name
-#   media_services_account_name = azurerm_media_services_account.ams.name
-#   description                 = "AnalyseVideo"
-#   output {
-#     relative_priority = "Normal"
-#     on_error_action   = "ContinueJob"
-#     builtin_preset {
-#       preset_name = "H264SingleBitrate1080p"
-#     }
-#   }
-# }
+resource "azurerm_media_transform" "analysevideo" {
+  name                        = "AnalyseVideo"
+  resource_group_name         = azurerm_resource_group.rg.name
+  media_services_account_name = azurerm_media_services_account.ams.name
+  description                 = "AnalyseVideo"
+  output {
+    relative_priority = "Normal"
+    on_error_action   = "ContinueJob"
+    builtin_preset {
+      preset_name = "H264SingleBitrate1080p"
+    }
+  }
+}
 
 resource "azurerm_media_transform" "EncodeToMP4" {
   name                        = "EncodeToMP4"
