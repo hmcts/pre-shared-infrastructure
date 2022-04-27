6d87e2795b3ae1957a29dc773b7d5ba6bfe065b2
Olu Ayodele <olu.ayodele@hmcts.net>
2022-04-27 10:44:13 +0100

    ip -30

--- key-vault.tf
+++ key-vault.tf
@@ -16,26 +16,26 @@ module "key-vault" {
 
 // Power App Permissions
 resource "azurerm_key_vault_access_policy" "power_app_access" {
-  key_vault_id = module.key-vault.key_vault_id
-  object_id    = var.power_app_user_oid
-  tenant_id    = data.azurerm_client_config.current.tenant_id
+  key_vault_id            = module.key-vault.key_vault_id
+  object_id               = var.power_app_user_oid
+  tenant_id               = data.azurerm_client_config.current.tenant_id
   key_permissions         = [ "List", "Update", "Create", "Import", "Delete", "Get", ]
   certificate_permissions = [ "List", "Update", "Create", "Import", "Delete", "ManageContacts", "ManageIssuers", "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers", ]
   secret_permissions      = [ "List", "Set", "Delete", "Get", ]
 }
 
 
-# // Jenkins management Permissions
-# resource "azurerm_key_vault_access_policy" "jenkins_access" {
-#   key_vault_id            = module.key-vault.key_vault_id
-#   # application_id        = var.app_id
-#   object_id               = "7ef3b6ce-3974-41ab-8512-c3ef4bb8ae01"
-#   tenant_id               = data.azurerm_client_config.current.tenant_id
-#   key_permissions         = [ "List","Update","Create","Import","Delete", "Get" ]
-#   certificate_permissions = [ "List", "Update", "Create", "Import", "Delete", "ManageContacts", "ManageIssuers", "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers", ]
-#   secret_permissions      = [ "List", "Set", "Delete", "Get", ]
-#   storage_permissions     = [ "List", "Set", "Delete", "Get", ]
-# }
+// Jenkins management Permissions
+resource "azurerm_key_vault_access_policy" "jenkins_access" {
+  key_vault_id            = module.key-vault.key_vault_id
+  # application_id        = var.app_id
+  object_id               = "7ef3b6ce-3974-41ab-8512-c3ef4bb8ae01"
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  key_permissions         = [ "List", "Update", "Create"," Import", "Delete", "Get" ]
+  certificate_permissions = [ "List", "Update", "Create", "Import", "Delete", "ManageContacts", "ManageIssuers", "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers", ]
+  secret_permissions      = [ "List", "Set", "Delete", "Get", ]
+  storage_permissions     = [ "List", "Set", "Delete", "Get", ]
+}
 
 #####################################
 #    Managed Identity Access to KV
