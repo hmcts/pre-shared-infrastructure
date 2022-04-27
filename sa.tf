df739ac3a17e924ca5905a0f4b90bf061a61f148
Olu Ayodele <olu.ayodele@hmcts.net>
2022-04-27 18:38:14 +0100

    mediaservice

--- sa.tf
+++ sa.tf
@@ -85,7 +85,7 @@ module "ingestsa_storage_account" {
   env                             = var.env
   storage_account_name            = replace("${var.product}ingestsa${var.env}", "-", "")
   resource_group_name             = azurerm_resource_group.rg.name
-  location                        = azurerm_resource_group.rg.location #"UKWest" #As recommended by MS azurerm_resource_group.rg.location
+  location                        = "UKWest" #As recommended by MS azurerm_resource_group.rg.location
   account_kind                    = "StorageV2"
   account_tier                    = var.sa_account_tier
   account_replication_type        = var.sa_replication_type
