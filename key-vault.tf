9dd23e5aae53c7e503eb2359c02c1fe7c738fc62
Rees Pozzi <47995122+reespozzi@users.noreply.github.com>
2022-04-28 19:53:49 +0100

    Update provider.tf (#85)
    * Update provider.tf
    
    Testing if 3.3.0 removes pipeline error
    
    * Fix capitalisation
    
    * Correct value
    
    Co-authored-by: Rees Pozzi <rees.pozzi@kainos.com>

--- key-vault.tf
+++ key-vault.tf
@@ -19,9 +19,9 @@ resource "azurerm_key_vault_access_policy" "power_app_access" {
   key_vault_id = module.key-vault.key_vault_id
   object_id    = var.power_app_user_oid
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  key_permissions         = [ "list", "update", "create", "import", "delete", "Get", ]
-  certificate_permissions = [ "list", "update", "create", "import", "delete", "managecontacts", "manageissuers", "getissuers", "listissuers", "setissuers", "deleteissuers", ]
-  secret_permissions      = [ "list", "set", "delete", "get", ]
+  key_permissions         = [ "List", "Update", "Create", "Import", "Delete", "Get", ]
+  certificate_permissions = [ "List", "Update", "Create", "Import", "Delete", "ManageContacts", "ManageIssuers", "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers", ]
+  secret_permissions      = [ "List", "Set", "Delete", "Get", ]
 }
 
 
@@ -45,10 +45,10 @@ resource "azurerm_key_vault_access_policy" "managedid_access" {
   # application_id        = var.app_id
   object_id               = var.managed_oid
   tenant_id               = data.azurerm_client_config.current.tenant_id
-  key_permissions         = [ "list","update","create","import","delete", "Get",]
-  certificate_permissions = [ "list", "get", "getissuers", "listissuers", ]
-  secret_permissions      = [ "list", "set", "delete", "Get", ]
-  storage_permissions     = [ "list", "set", "delete", "Get", ]
+  key_permissions         = [ "List","Update","Create","Import","Delete", "Get",]
+  certificate_permissions = [ "List", "Get", "GetIssuers", "ListIssuers", ]
+  secret_permissions      = [ "List", "Set", "Delete", "Get", ]
+  storage_permissions     = [ "List", "Set", "Delete", "Get", ]
 }
 
 #####################################
@@ -59,10 +59,10 @@ resource "azurerm_key_vault_access_policy" "dts_pre_access" {
   # application_id        = var.app_id
   object_id               = var.dts_pre_oid 
   tenant_id               = data.azurerm_client_config.current.tenant_id
-  key_permissions         = [ "list","get",]
-  certificate_permissions = [ "list", "get", "getissuers", "listissuers", ]
-  secret_permissions      = [ "list", "Get", ]
-  storage_permissions     = [ "list", "Get", ]
+  key_permissions         = [ "List","Get",]
+  certificate_permissions = [ "List", "Get", "GetIssuers", "ListIssuers", ]
+  secret_permissions      = [ "List", "Get", ]
+  storage_permissions     = [ "List", "Get", ]
 }
 
 #####################################
@@ -73,10 +73,10 @@ resource "azurerm_key_vault_access_policy" "dts_cft_developers_access" {
   # application_id        = var.app_id
   object_id               = var.dts_cft_developers_oid
   tenant_id               = data.azurerm_client_config.current.tenant_id
-  key_permissions         = [ "list","get",]
-  certificate_permissions = [ "list", "get", "getissuers", "listissuers", ]
-  secret_permissions      = [ "list", "Get", ]
-  storage_permissions     = [ "list", "Get", ]
+  key_permissions         = [ "List","Get",]
+  certificate_permissions = [ "List", "Get", "GetIssuers", "ListIssuers", ]
+  secret_permissions      = [ "List", "Get", ]
+  storage_permissions     = [ "List", "Get", ]
 }
 // VM credentials
 
