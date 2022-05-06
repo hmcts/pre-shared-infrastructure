cf3ba6c17017f9b4e34298ba6078955d6a309868
Olu Ayodele <olu.ayodele@hmcts.net>
2022-05-06 01:36:10 +0100

    acl

--- key-vault.tf
+++ key-vault.tf
@@ -18,7 +18,7 @@ module "key-vault" {
   product_group_name      = "DTS Pre-recorded Evidence"
   common_tags             = var.common_tags
   create_managed_identity = true
-  network_acls_allowed_subnet_id = [concat([data.azurerm_subnet.jenkins_subnet.id],[azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id],[azurerm_subnet.videoeditvm_subnet.id])]
+  network_acls_allowed_subnet_id = concat([data.azurerm_subnet.jenkins_subnet.id],[azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id],[azurerm_subnet.videoeditvm_subnet.id])
   # purge_protection_enabled    = true
 
 }
