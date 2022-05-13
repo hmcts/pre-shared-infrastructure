f1ca7b3fe9d7101894e0d59a947e44a526031cfd
Olu Ayodele <olu.ayodele@hmcts.net>
2022-05-13 14:57:35 +0100

    dns

--- vnet.tf
+++ vnet.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.product}-vnet01-${var.env}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  # address_space       = [var.vnet_address_space]
+  address_space       = [var.vnet_address_space]
   # ddos_protection_plan {
   #   id          = azurerm_network_ddos_protection_plan.pre-ddos.id
   #   enable      = true
