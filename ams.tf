85f8282c771f58d01ed5af33f0486dee702923d3
Olu Ayodele <olu.ayodele@hmcts.net>
2022-05-17 13:05:26 +0100

    ams managed id

--- ams.tf
+++ ams.tf
@@ -37,7 +37,7 @@
 resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
   scope                            = azurerm_resource_group.rg.id
   role_definition_name             = "Storage Blob Data Contributor"
-  principal_id                     = [azurerm_media_services_account.ams.identity[0].principal_id, data.azurerm_user_assigned_identity.managed-identity.principal_id] #var.pre_mi_principal_id
+  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id #var.pre_mi_principal_id
   skip_service_principal_aad_check = true
 }
 
@@ -45,13 +45,13 @@ resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
 resource "azurerm_role_assignment" "pre_reader_mi" {
   scope                            = azurerm_resource_group.rg.id
   role_definition_name             = "Reader"
-  principal_id                     = azurerm_media_services_account.ams.identity[0].principal_id # var.pre_mi_principal_id 
+  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_i # var.pre_mi_principal_id 
   skip_service_principal_aad_check = true
 }
 
 
 resource "azurerm_media_transform" "analysevideo" {
-  name                        = "AnalyseVideo"
+  name                        = "AnalyseVideos"
   resource_group_name         = azurerm_resource_group.rg.name
   media_services_account_name = azurerm_media_services_account.ams.name
   description                 = "AnalyseVideo"
