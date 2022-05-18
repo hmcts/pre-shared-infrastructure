a996d5ca03d0d7866d5b5cf51507e4c2e56fe630
Don-Olu <98063921+Don-Olu@users.noreply.github.com>
2022-05-18 14:11:35 +0100

    Demo (#89)
    * kv deny
    
    * ams managed id
    
    * ams managed id
    
    * ams managed id
    
    * ams managed id
    
    * ams managed id
    
    * rebuild demo
    
    * rebuild demo
    
    * ams mi
    
    * ams id

--- ams.tf
+++ ams.tf
@@ -18,38 +18,45 @@
     is_primary = false
     
   }
-  # identity {
-  #     principal_id = azurerm_media_services_account.ams.identity[0].principal_id #var.pre_mi_principal_id
-  #     tenant_id    = azurerm_media_services_account.ams.identity[0].tenant_id
-  #     type         = "ManagedIdentity" 
-  # }
-  # storage_authentication_type   = "ManagedIdentity"
-  storage_authentication_type   = "System"
+ 
+  storage_authentication_type   = "ManagedIdentity"
+  # storage_authentication_type   = "System"
+  lifecycle {
+    ignore_changes= [storage_authentication_type,identity]
+  }
   tags         = var.common_tags
   
 }
 
 # #Storage Blob Data Contributor Role Assignment for Managed Identity
-# resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
-#   scope                            = azurerm_resource_group.rg.id
-#   role_definition_name             = "Storage Blob Data Contributor"
-#   principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id #var.pre_mi_principal_id
-#   skip_service_principal_aad_check = true
-# }
+
+resource "azurerm_role_assignment" "pre_amsblobdatacontributor_mi" {
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_name             = "Storage Blob Data Contributor"
+  principal_id                     = azurerm_media_services_account.ams.identity[0].principal_id #var.pre_mi_principal_id
+  skip_service_principal_aad_check = true
+  depends_on = [
+    azurerm_media_services_account.ams
+  ]
+}
 
 #Reader Role Assignment for Managed Identity
-# resource "azurerm_role_assignment" "pre_reader_mi" {
-#   scope                            = azurerm_resource_group.rg.id
-#   role_definition_name             = "Reader"
-#   principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
-#   skip_service_principal_aad_check = true
-# }
+resource "azurerm_role_assignment" "pre_amsreader_mi" {
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_name             = "Reader"
+  principal_id                     = azurerm_media_services_account.ams.identity[0].principal_id # var.pre_mi_principal_id 
+  skip_service_principal_aad_check = true
+  
+  depends_on = [
+    azurerm_media_services_account.ams
+  ]
+}
 
 resource "azurerm_media_transform" "analysevideo" {
-  name                        = "AnalyseVideo"
+  name                        = "AnalyseVideos"
   resource_group_name         = azurerm_resource_group.rg.name
   media_services_account_name = azurerm_media_services_account.ams.name
-  description                 = "AnalyseVideo"
+  description                 = "pre-AnalyseVideo"
   output {
     relative_priority = "Normal"
     on_error_action   = "ContinueJob"
@@ -57,12 +64,13 @@ resource "azurerm_media_transform" "analysevideo" {
       preset_name = "H264SingleBitrate1080p"
     }
   }
+
 }
 resource "azurerm_media_transform" "EncodeToMP4" {
   name                        = "EncodeToMP4"
   resource_group_name         = azurerm_resource_group.rg.name
   media_services_account_name = azurerm_media_services_account.ams.name
-  description                 = "EncodeToMP4"
+  description                 = "pre-EncodeToMP4"
   output {
     relative_priority = "Normal"
     on_error_action   = "ContinueJob"
