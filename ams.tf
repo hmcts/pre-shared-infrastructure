646e12d874e7476722276cb0b318cc60289b92fb
Don-Olu <98063921+Don-Olu@users.noreply.github.com>
2022-05-18 12:21:39 +0100

    Demo (#88)
    * kv deny
    
    * ams managed id
    
    * ams managed id
    
    * ams managed id
    
    * ams managed id
    
    * ams managed id
    
    * rebuild demo

--- ams.tf
+++ ams.tf
@@ -29,22 +29,21 @@
   
 }
 
-#Storage Blob Data Contributor Role Assignment for Managed Identity
-resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
-  scope                            = azurerm_resource_group.rg.id
-  role_definition_name             = "Storage Blob Data Contributor"
-  principal_id                     = azurerm_media_services_account.ams.identity[0].principal_id #var.pre_mi_principal_id
-  skip_service_principal_aad_check = true
-}
+# #Storage Blob Data Contributor Role Assignment for Managed Identity
+# resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
+#   scope                            = azurerm_resource_group.rg.id
+#   role_definition_name             = "Storage Blob Data Contributor"
+#   principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id #var.pre_mi_principal_id
+#   skip_service_principal_aad_check = true
+# }
 
 #Reader Role Assignment for Managed Identity
-resource "azurerm_role_assignment" "pre_reader_mi" {
-  scope                            = azurerm_resource_group.rg.id
-  role_definition_name             = "Reader"
-  principal_id                     = azurerm_media_services_account.ams.identity[0].principal_id # var.pre_mi_principal_id 
-  skip_service_principal_aad_check = true
-}
-
+# resource "azurerm_role_assignment" "pre_reader_mi" {
+#   scope                            = azurerm_resource_group.rg.id
+#   role_definition_name             = "Reader"
+#   principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
+#   skip_service_principal_aad_check = true
+# }
 
 resource "azurerm_media_transform" "analysevideo" {
   name                        = "AnalyseVideo"
