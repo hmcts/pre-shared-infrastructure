7d65026cbf5426e10407cd552e4d0dcef6a8d7a1
Olu Ayodele <olu.ayodele@hmcts.net>
2022-05-27 18:33:03 +0100

    edit vm

--- key-vault.tf
+++ key-vault.tf
@@ -223,6 +223,32 @@ resource "azurerm_key_vault_access_policy" "pre-des-disk" {
   ]
 }
 
+
+#### West
+
+resource "azurerm_disk_encryption_set" "pre-des-west" {
+  name                = "pre-des-west-${var.env}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = "UKWest"
+  key_vault_key_id    = azurerm_key_vault_key.pre_kv_key.id
+  identity {
+    type = "SystemAssigned"
+  }
+  tags                = var.common_tags
+}
+
+resource "azurerm_key_vault_access_policy" "pre-des-disk" {
+  key_vault_id = module.key-vault.key_vault_id
+
+  tenant_id = azurerm_disk_encryption_set.pre-des-west.identity.0.tenant_id
+  object_id = azurerm_disk_encryption_set.pre-des-west.identity.0.principal_id
+
+  key_permissions = [
+    "Get",
+    "WrapKey",
+    "UnwrapKey"
+  ]
+}
 # resource "azurerm_key_vault_access_policy" "pre-deskv-user" {
 #   key_vault_id = module.key-vault.key_vault_id
 
