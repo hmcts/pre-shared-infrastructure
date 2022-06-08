66028079a04c6134ea44d3fb5faf835c55f8f499
Olu Ayodele <olu.ayodele@hmcts.net>
2022-06-08 20:06:27 +0100

    Clean Vms

--- vms.tf
+++ vms.tf
@@ -32,7 +32,7 @@ resource "azurerm_bastion_host" "bastion" {
 #                EDIT VIRTUAL MACHINE                 #
 ###################################################
 
-resource "azurerm_network_interface" "nic" {
+resource "azurerm_network_interface" "nics" {
   count               = var.num_vid_edit_vms
   name                = "${var.product}-videditnic${count.index}-${var.env}"
   location            = azurerm_resource_group.rg.location
@@ -54,7 +54,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   size                        = var.vid_edit_vm_spec
   admin_username              = "videdit${count.index}_${random_string.vm_username[count.index].result}"
   admin_password              = random_password.vm_password[count.index].result
-  network_interface_ids       = [azurerm_network_interface.nic[count.index].id]
+  network_interface_ids       = [azurerm_network_interface.nics[count.index].id]
   encryption_at_host_enabled  = true
 
   # additional_capabilities {
