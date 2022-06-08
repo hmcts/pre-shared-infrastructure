f82656054b0257aa081126ec38a1d6959973eabe
Don-Olu <98063921+Don-Olu@users.noreply.github.com>
2022-06-08 20:56:19 +0100

    Demo (#97)
    ITHC -v1

--- ams.tf
+++ ams.tf
@@ -11,17 +11,14 @@
   storage_account {
     id         = module.ingestsa_storage_account.storageaccount_id 
     is_primary = true
-   
-
   }
 
   storage_account {
     id         = module.finalsa_storage_account.storageaccount_id 
     is_primary = false
  }
-
  
-  storage_authentication_type   = "ManagedIdentity"
+  # storage_authentication_type   = "ManagedIdentity"
   # storage_authentication_type   = "System"
   lifecycle {
     ignore_changes= [storage_authentication_type,identity]
@@ -55,25 +52,6 @@ resource "azurerm_role_assignment" "pre_amsreader_mi" {
 }
 
 resource "azurerm_media_transform" "analysevideo" {
-  name                        = "AnalyseVideos"
-  resource_group_name         = azurerm_resource_group.rg.name
-  media_services_account_name = azurerm_media_services_account.ams.name
-  description                 = "pre-AnalyseVideo"
-  output {
-    relative_priority = "Normal"
-    on_error_action   = "ContinueJob"
-    builtin_preset {
-      preset_name = "H264SingleBitrate1080p"
-    }
-  }
-
-}
-
-  }
-  tags             = var.common_tags
-}
-
-resource "azurerm_media_transform" "analysevideo" {
   name                        = "AnalyseVideo"
   resource_group_name         = azurerm_resource_group.rg.name
   media_services_account_name = azurerm_media_services_account.ams.name
@@ -101,5 +79,5 @@ resource "azurerm_media_transform" "EncodeToMP4" {
       preset_name = "H264SingleBitrate1080p"
     }
   }
-
 }
+
