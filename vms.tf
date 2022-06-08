f8319b49f9edc7eb84053be017a56ba718eb3352
Olu Ayodele <olu.ayodele@hmcts.net>
2022-06-08 20:11:42 +0100

    fix vm

--- vms.tf
+++ vms.tf
@@ -28,86 +28,86 @@ resource "azurerm_bastion_host" "bastion" {
 }
 
 
-###################################################
-#                EDIT VIRTUAL MACHINE                 #
-###################################################
+# ###################################################
+# #                EDIT VIRTUAL MACHINE                 #
+# ###################################################
 
-resource "azurerm_network_interface" "nics" {
-  count               = var.num_vid_edit_vms
-  name                = "${var.product}-videditnic${count.index}-${var.env}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_network_interface" "nics" {
+#   count               = var.num_vid_edit_vms
+#   name                = "${var.product}-videditnic${count.index}-${var.env}"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.videoeditvm_subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-   tags                = var.common_tags
-}
-resource "azurerm_windows_virtual_machine" "vm" {
-  count                       = var.num_vid_edit_vms
-  name                        = "${var.product}-videditvm${count.index}-${var.env}"
-  computer_name               = "PREVIDED0${count.index}-${var.env}"
-  resource_group_name         = azurerm_resource_group.rg.name
-  location                    = azurerm_resource_group.rg.location
-  size                        = var.vid_edit_vm_spec
-  admin_username              = "videdit${count.index}_${random_string.vm_username[count.index].result}"
-  admin_password              = random_password.vm_password[count.index].result
-  network_interface_ids       = [azurerm_network_interface.nics[count.index].id]
-  encryption_at_host_enabled  = true
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.videoeditvm_subnet.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+#    tags                = var.common_tags
+# }
+# resource "azurerm_windows_virtual_machine" "vm" {
+#   count                       = var.num_vid_edit_vms
+#   name                        = "${var.product}-videditvm${count.index}-${var.env}"
+#   computer_name               = "PREVIDED0${count.index}-${var.env}"
+#   resource_group_name         = azurerm_resource_group.rg.name
+#   location                    = azurerm_resource_group.rg.location
+#   size                        = var.vid_edit_vm_spec
+#   admin_username              = "videdit${count.index}_${random_string.vm_username[count.index].result}"
+#   admin_password              = random_password.vm_password[count.index].result
+#   network_interface_ids       = [azurerm_network_interface.nics[count.index].id]
+#   encryption_at_host_enabled  = true
 
-  # additional_capabilities {
-  #  ultra_ssd_enabled   =  true
-  # }
+#   # additional_capabilities {
+#   #  ultra_ssd_enabled   =  true
+#   # }
   
-  os_disk {
-    name                      = "${var.product}-videditvm${count.index}-osdisk-${var.env}"
-    caching                   = "ReadWrite"
-    storage_account_type      = "StandardSSD_LRS" #UltraSSD_LRS?
-    disk_encryption_set_id    = azurerm_disk_encryption_set.pre-des.id
-    # write_accelerator_enabled = true
-  }
+#   os_disk {
+#     name                      = "${var.product}-videditvm${count.index}-osdisk-${var.env}"
+#     caching                   = "ReadWrite"
+#     storage_account_type      = "StandardSSD_LRS" #UltraSSD_LRS?
+#     disk_encryption_set_id    = azurerm_disk_encryption_set.pre-des.id
+#     # write_accelerator_enabled = true
+#   }
 
   
-  source_image_reference {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-10"
-    sku       = "20h1-pro-g2"
-    version   = "latest"
-  }
-  # identity {
-  #   type = "SystemAssigned"
-  #   }
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsDesktop"
+#     offer     = "Windows-10"
+#     sku       = "20h1-pro-g2"
+#     version   = "latest"
+#   }
+#   # identity {
+#   #   type = "SystemAssigned"
+#   #   }
 
-  timezone                 = "GMT Standard Time"
-  enable_automatic_updates = true
-  provision_vm_agent       = true  
-  tags                     = var.common_tags
+#   timezone                 = "GMT Standard Time"
+#   enable_automatic_updates = true
+#   provision_vm_agent       = true  
+#   tags                     = var.common_tags
 
-  depends_on = [ module.key-vault, azurerm_disk_encryption_set.pre-des ]
-}
+#   depends_on = [ module.key-vault, azurerm_disk_encryption_set.pre-des ]
+# }
 
-resource "azurerm_managed_disk" "vmdatadisk" {
-  count                   = var.num_vid_edit_vms
-  name                    = "${var.product}-videditvm${count.index}-datadisk-${var.env}"
-  location                = azurerm_resource_group.rg.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  storage_account_type    = "StandardSSD_LRS"
-  create_option           = "Empty"
-  disk_size_gb            = 1000
-  disk_encryption_set_id  = azurerm_disk_encryption_set.pre-des.id
-  tags                    = var.common_tags
+# resource "azurerm_managed_disk" "vmdatadisk" {
+#   count                   = var.num_vid_edit_vms
+#   name                    = "${var.product}-videditvm${count.index}-datadisk-${var.env}"
+#   location                = azurerm_resource_group.rg.location
+#   resource_group_name     = azurerm_resource_group.rg.name
+#   storage_account_type    = "StandardSSD_LRS"
+#   create_option           = "Empty"
+#   disk_size_gb            = 1000
+#   disk_encryption_set_id  = azurerm_disk_encryption_set.pre-des.id
+#   tags                    = var.common_tags
 
      
-}
+# }
 
-resource "azurerm_virtual_machine_data_disk_attachment" "vmdatadisk" {
-  count              = var.num_vid_edit_vms
-  managed_disk_id    = azurerm_managed_disk.vmdatadisk.*.id[count.index]
-  virtual_machine_id = azurerm_windows_virtual_machine.vm.*.id[count.index]
-  lun                = "3"
-  caching            = "ReadWrite"
-}
+# resource "azurerm_virtual_machine_data_disk_attachment" "vmdatadisk" {
+#   count              = var.num_vid_edit_vms
+#   managed_disk_id    = azurerm_managed_disk.vmdatadisk.*.id[count.index]
+#   virtual_machine_id = azurerm_windows_virtual_machine.vm.*.id[count.index]
+#   lun                = "3"
+#   caching            = "ReadWrite"
+# }
 
 
