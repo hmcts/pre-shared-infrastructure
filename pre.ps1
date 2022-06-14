8da449c94f600dacc0a92881829f51ab7a3777ad
Olu Ayodele <olu.ayodele@hmcts.net>
2022-06-14 14:09:16 +0100

    ams id pshell

---
+++ pre.ps1
@@ -0,0 +1,8 @@
+
+$amsname
+$rg
+$mi
+
+az ams account identity assign -n $amsname -g $rg --user-assigned $mi
+az ams account storage set-authentication -n $amsname -g $rg --storage-auth ManagedIdentity --user-assigned $mi
+
