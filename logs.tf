ad16a6ff9c91f37b7dace80ddd2fc647520b672c
Olu Ayodele <olu.ayodele@hmcts.net>
2022-06-17 19:34:06 +0100

    demo azcli

--- logs.tf
+++ logs.tf
@@ -25,6 +25,194 @@ resource "azurerm_monitor_diagnostic_setting" "ams" {
   }
 }
 
+resource "azurerm_monitor_diagnostic_setting" "bastion" {
+  name                       = azurerm_bastion_host.bastion.name
+  target_resource_id         = azurerm_bastion_host.bastion.id
+  log_analytics_workspace_id = module.log_analytics_workspace.workspace_id
+
+  log {
+    category = "BastionAuditLogs"
+
+    retention_policy {
+      enabled = true
+      days    = 14
+    }
+  }
+    metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled =  true
+      days    = 14
+    }
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "nic" {
+  count               = var.num_vid_edit_vms
+  name                       = azurerm_network_interface.nic.name
+  target_resource_id         = azurerm_network_interface.nic.id
+  log_analytics_workspace_id = module.log_analytics_workspace.workspace_id
+
+    metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled =  true
+      days    = 14
+    }
+  }
+}
+resource "azurerm_monitor_diagnostic_setting" "vnet" {
+
+  name                       = azurerm_virtual_network.vnet.name
+  target_resource_id         = azurerm_virtual_network.vnet.id
+  log_analytics_workspace_id = module.log_analytics_workspace.workspace_id
+
+ log {
+    category = "VMProtectionAlerts"
+
+    retention_policy {
+      enabled = true
+      days    = 14
+    }
+  }
+    metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled =  true
+      days    = 14
+    }
+  }
+}
+resource "azurerm_monitor_diagnostic_setting" "storageblobsa" {
+   name                       = module.sa_storage_account.storageaccount_name
+   target_resource_id         = "${module.sa_storage_account.id}/blobServices/default"
+   log_analytics_workspace_id =  module.log_analytics_workspace.workspace_id
+  
+   log {
+    category = "StorageRead"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+   
+   log {
+    category = "StorageWrite"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+   
+   log {
+    category = "StorageDelete"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+
+   metric {
+     category = "Transaction"
+     enabled  = true
+
+     retention_policy {
+       enabled = true
+     }
+   }
+ }
+
+resource "azurerm_monitor_diagnostic_setting" "storageblobfinalsa" {
+   name                       = module.finalsa_storage_account.storageaccount_name
+   target_resource_id         = "${module.finalsa_storage_account.id}/blobServices/default"
+   log_analytics_workspace_id =  module.log_analytics_workspace.workspace_id
+  
+   log {
+    category = "StorageRead"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+   
+   log {
+    category = "StorageWrite"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+   
+   log {
+    category = "StorageDelete"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+
+   metric {
+     category = "Transaction"
+     enabled  = true
+
+     retention_policy {
+       enabled = true
+     }
+   }
+ }
+ 
+
+resource "azurerm_monitor_diagnostic_setting" "storageblobingestsa" {
+   name                       = module.ingestsa_storage_account.storageaccount_name
+   target_resource_id         = "${module.ingestsa_storage_account.id}/blobServices/default"
+   log_analytics_workspace_id =  module.log_analytics_workspace.workspace_id
+  
+   log {
+    category = "StorageRead"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+   
+   log {
+    category = "StorageWrite"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+   
+   log {
+    category = "StorageDelete"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+   }
+
+   metric {
+     category = "Transaction"
+     enabled  = true
+
+     retention_policy {
+       enabled = true
+     }
+   }
+ }
+ 
 module "log_analytics_workspace" {
   source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
   environment = var.env
