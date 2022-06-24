c1c8947499e073ab764d7744dc0d24353a512ffb
Olu Ayodele <olu.ayodele@hmcts.net>
2022-06-24 12:39:37 +0100

    prod kv

--- key-vault.tf
+++ key-vault.tf
@@ -12,9 +12,12 @@ module "key-vault" {
   common_tags             = var.common_tags
   create_managed_identity = true
   network_acls_allowed_subnet_ids = concat([data.azurerm_subnet.jenkins_subnet.id],[azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id],[azurerm_subnet.videoeditvm_subnet.id])
-  purge_protection_enabled    = false
+  purge_protection_enabled    = true
   network_acls_default_action = "Deny"
   network_acls_allowed_ip_ranges = [ "90.247.65.225" ]
+  lifecycle {
+    ignore_changes= [purge_protection_enabled ]
+  }
 }
 
 # module "key-vault2" {
