2a59a95e39f4318fbc510ab47077374df6576910
Olu Ayodele <olu.ayodele@hmcts.net>
2022-07-19 20:40:14 +0100

    ams02

--- ams.tf
+++ ams.tf
@@ -56,7 +56,7 @@ resource "azurerm_media_transform" "EncodeToMP4" {
 }
 
  resource "azurerm_media_services_account" "ams02" {
-  name                          = "${var.product}ams${var.env}"
+  name                          = "${var.product}ams02${var.env}"
   location                      = "${var.location}"
   resource_group_name           = azurerm_resource_group.rg.name
   
