4187f63a93a33490264fb268a557530c9ae96cd0
Olu Ayodele <olu.ayodele@hmcts.net>
2022-08-31 12:50:01 +0100

    autostop-start

--- runbook.tf
+++ runbook.tf
@@ -40,7 +40,7 @@ module "vm_automation" {
                      ]
   resource_group_name     = azurerm_resource_group.rg.name
   vm_names = azurerm_windows_virtual_machine.vm.*.name
-  mi_principal_id         =  "${module.key-vault.managed_identity_objectid}"
+  mi_principal_id         =  module.key-vault.managed_identity_objectid
 }
 
 
