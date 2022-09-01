7a24a60e84d9c5036502de53d1ee95da337e6857
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-01 20:38:22 +0100

    autostop-start

--- runbook.tf
+++ runbook.tf
@@ -1,6 +1,5 @@
 data "azurerm_automation_account" "pre-aa" {
   name                = "${var.product}-${var.env}-aa"
-  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 # resource "azurerm_automation_account" "pre-aa" {
