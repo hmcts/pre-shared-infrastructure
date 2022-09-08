449ebc8481089f17c869b8c5d77e4f2f4c550d20
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-08 16:19:10 +0100

    var

--- provider.tf
+++ provider.tf
@@ -22,6 +22,16 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {}
 }
+provider "azurerm" {
+  alias           = "oms"
+  subscription_id = module.log_analytics_workspace.subscription_id
+  features {}
+}
+
+data "azuread_service_principal" "kv" {
+  # display_name = "Azure Key Vault"
+  application_id = "cfa8b339-82a2-471a-a3c9-0fc0be7a4093"
+}
 
 
 
