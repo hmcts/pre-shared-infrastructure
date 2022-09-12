7ac0148ba480d58181f4fb4dad308ef64767196d
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-12 17:58:49 +0100

    keyvault

--- ams.tf
+++ ams.tf
@@ -56,35 +56,59 @@ resource "azurerm_media_transform" "EncodeToMP4" {
   }
 }
 
- resource "azurerm_media_services_account" "ams02" {
-  name                          = "${var.product}ams02${var.env}"
-  location                      = "${var.location}"
-  resource_group_name           = azurerm_resource_group.rg.name
+resource "null_resource" "amsid" {
+  # triggers = {
+  #   always_run = timestamp()
+  # }
+
+  depends_on = [azurerm_media_services_account.ams]
+ provisioner "local-exec" {
+   command = <<EOF
+    az login --identity
+    az account set -s dts-sharedservices-${var.env}
+    echo "ams account identity assign"
+    az ams account identity assign --name ${azurerm_media_services_account.ams.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
+    # az ams account identity assign --name ${azurerm_media_services_account.ams02.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
+   
+    echo "ams account storage"
+    az ams account storage set-authentication --account-name ${azurerm_media_services_account.ams.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi" --storage-auth ManagedIdentity --storage-account-id "/subscriptions/dts-sharedservices-${var.env}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/preingestsa${var.env}" 
+	  # az ams account identity assign --name ${azurerm_media_services_account.ams02.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
+    # az ams account storage set-authentication --account-name ${azurerm_media_services_account.ams02.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi" --storage-auth ManagedIdentity --storage-account-id "/subscriptions/dts-sharedservices-${var.env}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/preingestsa${var.env}" 
+	 EOF
+   } 
+}
+
+
+### AMS02 in Sandbox
+#  resource "azurerm_media_services_account" "ams02" {
+#   name                          = "${var.product}ams02${var.env}"
+#   location                      = "${var.location}"
+#   resource_group_name           = azurerm_resource_group.rg.name
   
-  # identity {
-  #   type = "SystemAssigned"
-  # } 
+#   # identity {
+#   #   type = "SystemAssigned"
+#   # } 
 
 
-  storage_account {
-    id         = module.ingestsa02_storage_account.storageaccount_id 
-    is_primary = true
-  }
+#   storage_account {
+#     id         = module.ingestsa02_storage_account.storageaccount_id 
+#     is_primary = true
+#   }
 
-  storage_account {
-    id         = module.finalsa02_storage_account.storageaccount_id 
-    is_primary = false
- }
+#   storage_account {
+#     id         = module.finalsa02_storage_account.storageaccount_id 
+#     is_primary = false
+#  }
  
   # storage_authentication_type   = "ManagedIdentity"
   # storage_authentication_type   = "System"
   # lifecycle {
   #   ignore_changes= [storage_authentication_type,identity]
   # }
-  tags         = var.common_tags
+#   tags         = var.common_tags
   
-}
-# resource "azurerm_media_transform" "analysevideo" {
+# }
+# # resource "azurerm_media_transform" "analysevideo" {
 #   name                        = "AnalyseVideo"
 #   resource_group_name         = azurerm_resource_group.rg.name
 #   media_services_account_name = azurerm_media_services_account.ams.name
@@ -118,28 +142,8 @@ resource "azurerm_media_transform" "EncodeToMP4" {
 # }
 
 
-resource "null_resource" "amsid" {
-  # triggers = {
-  #   always_run = timestamp()
-  # }
 
-  depends_on = [azurerm_media_services_account.ams02]
- provisioner "local-exec" {
-   command = <<EOF
-    az login --identity
-    az account set -s dts-sharedservices-${var.env}
-    echo "ams account identity assign"
-    az ams account identity assign --name ${azurerm_media_services_account.ams.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
-    # az ams account identity assign --name ${azurerm_media_services_account.ams02.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
-    EOF
-   }
-    # echo "ams account storage"
-    # az ams account storage set-authentication --account-name ${azurerm_media_services_account.ams.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi" --storage-auth ManagedIdentity --storage-account-id "/subscriptions/dts-sharedservices-${var.env}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/preingestsa${var.env}" 
-	  # az ams account identity assign --name ${azurerm_media_services_account.ams02.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
-    # az ams account storage set-authentication --account-name ${azurerm_media_services_account.ams02.name} -g ${azurerm_resource_group.rg.name} --user-assigned "/subscriptions/dts-sharedservices-${var.env}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi" --storage-auth ManagedIdentity --storage-account-id "/subscriptions/dts-sharedservices-${var.env}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/preingestsa${var.env}" 
-	 
 
-}
 
 # # #Storage Blob Data Contributor Role Assignment for Managed Identity
 
--- vms.tf
+++ vms.tf
@@ -319,211 +319,211 @@ module "dynatrace-oneagent" {
 #   tags = var.common_tags
 
 # }
-####
-## DataGateway VMs
-####
-###################################################
-#            Datagateway NETWORK INTERFACE CARD               #
-###################################################
-resource "azurerm_network_interface" "dtgwnic" {
-  count               = var.num_datagateway
-  name                = "${var.product}-dtgwnic${count.index}-${var.env}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# ####
+# ## DataGateway VMs
+# ####
+# ###################################################
+# #            Datagateway NETWORK INTERFACE CARD               #
+# ###################################################
+# resource "azurerm_network_interface" "dtgwnic" {
+#   count               = var.num_datagateway
+#   name                = "${var.product}-dtgwnic${count.index}-${var.env}"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.datagateway_subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-   tags                = var.common_tags
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.datagateway_subnet.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+#    tags                = var.common_tags
+# }
 
-###################################################
-#                DATAGATEWAY VIRTUAL MACHINE                 #
-###################################################
-resource "azurerm_windows_virtual_machine" "dtgtwyvm" {
-  count               = var.num_datagateway
-  zone                = 2
-  name                = "${var.product}dtgtwy${count.index}-${var.env}"
-  computer_name       = "PREDTGTW0${count.index}-${var.env}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.datagateway_spec
-  admin_username      = "Dtgtwy${count.index}_${random_string.dtgtwy_username[count.index].result}"
-  admin_password      = random_password.dtgtwy_password[count.index].result
-  network_interface_ids = [azurerm_network_interface.dtgwnic[count.index].id]
+# ###################################################
+# #                DATAGATEWAY VIRTUAL MACHINE                 #
+# ###################################################
+# resource "azurerm_windows_virtual_machine" "dtgtwyvm" {
+#   count               = var.num_datagateway
+#   zone                = 2
+#   name                = "${var.product}dtgtwy${count.index}-${var.env}"
+#   computer_name       = "PREDTGTW0${count.index}-${var.env}"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   size                = var.datagateway_spec
+#   admin_username      = "Dtgtwy${count.index}_${random_string.dtgtwy_username[count.index].result}"
+#   admin_password      = random_password.dtgtwy_password[count.index].result
+#   network_interface_ids = [azurerm_network_interface.dtgwnic[count.index].id]
 
-  os_disk {
-    name                 = "${var.product}dtgtwy${count.index}-osdisk-${var.env}"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    disk_encryption_set_id  = azurerm_disk_encryption_set.pre-des.id
-  }
-  # identity {
-  #   type = "SystemAssigned"
-  # }module
+#   os_disk {
+#     name                 = "${var.product}dtgtwy${count.index}-osdisk-${var.env}"
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#     disk_encryption_set_id  = azurerm_disk_encryption_set.pre-des.id
+#   }
+#   # identity {
+#   #   type = "SystemAssigned"
+#   # }module
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-datacenter-gensecond"
-    version   = "latest"
-  }
-  timezone                     = "GMT Standard Time"
-  enable_automatic_updates     = true
-  provision_vm_agent           = true  
-  allow_extension_operations   = true
-  # patch_mode                   = "AutomaticByOS"
-  # hotpatching_enabled          = true
-  tags                         = var.common_tags
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2019-datacenter-gensecond"
+#     version   = "latest"
+#   }
+#   timezone                     = "GMT Standard Time"
+#   enable_automatic_updates     = true
+#   provision_vm_agent           = true  
+#   allow_extension_operations   = true
+#   # patch_mode                   = "AutomaticByOS"
+#   # hotpatching_enabled          = true
+#   tags                         = var.common_tags
 
  
 
-  depends_on = [ module.key-vault]
-}
-
-
-resource "azurerm_managed_disk" "dtgtwaydatadisk" {
-  count                = var.num_datagateway
-  name                 = "${var.product}dtgtwy${count.index}-datadisk-${var.env}"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = 100
-  zone                 = "2"
-  disk_encryption_set_id  = azurerm_disk_encryption_set.pre-des.id
-  tags                 = var.common_tags
-}
-
-resource "azurerm_virtual_machine_data_disk_attachment" "dtgtwy" {
-  count              = var.num_datagateway
-  managed_disk_id    = azurerm_managed_disk.dtgtwaydatadisk.*.id[count.index]
-  virtual_machine_id = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
-  lun                = "3"
-  caching            = "ReadWrite"
-}
-
-resource "azurerm_virtual_machine_extension" "dtgtwayvmextension" {
-  name                 = "IaaSAntimalware"
-  count                = var.num_datagateway
-  virtual_machine_id   = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
-
-  publisher            = "Microsoft.Azure.Security"
-  type                 = "IaaSAntimalware"
-  type_handler_version = "1.3"
-  auto_upgrade_minor_version = true
-  settings = <<SETTINGS
-    {
-    "AntimalwareEnabled": true,
-    "RealtimeProtectionEnabled": "true",
-    "ScheduledScanSettings": {
-    "isEnabled": "true",
-    "day": "1",
-    "time": "120",
-    "scanType": "Quick"
-    },
-    "Exclusions": {
-    "Extensions": "",
-    "Paths": "",
-    "Processes": ""
-    }
-    }
-SETTINGS
-  tags                = var.common_tags
-}
-
-# resource "azurerm_security_center_server_vulnerability_assessment" "vulneass" {
-#   count                  = var.num_datagateway
-#   virtual_machine_id = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
+#   depends_on = [ module.key-vault]
 # }
 
-resource "azurerm_virtual_machine_extension" "dtgtwydaa-agent" {
-  name                       = "DependencyAgentWindows"
-  count                      = var.num_datagateway
-  virtual_machine_id         = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
-  publisher                  = "Microsoft.Azure.Monitoring.DependencyAgent"
-  type                       = "DependencyAgentWindows"
-  type_handler_version       = "9.10"
-  automatic_upgrade_enabled  = true
-  auto_upgrade_minor_version = true
-  tags                    = var.common_tags
-}
+
+# resource "azurerm_managed_disk" "dtgtwaydatadisk" {
+#   count                = var.num_datagateway
+#   name                 = "${var.product}dtgtwy${count.index}-datadisk-${var.env}"
+#   location             = azurerm_resource_group.rg.location
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   storage_account_type = "Standard_LRS"
+#   create_option        = "Empty"
+#   disk_size_gb         = 100
+#   zone                 = "2"
+#   disk_encryption_set_id  = azurerm_disk_encryption_set.pre-des.id
+#   tags                 = var.common_tags
+# }
+
+# resource "azurerm_virtual_machine_data_disk_attachment" "dtgtwy" {
+#   count              = var.num_datagateway
+#   managed_disk_id    = azurerm_managed_disk.dtgtwaydatadisk.*.id[count.index]
+#   virtual_machine_id = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
+#   lun                = "3"
+#   caching            = "ReadWrite"
+# }
+
+# resource "azurerm_virtual_machine_extension" "dtgtwayvmextension" {
+#   name                 = "IaaSAntimalware"
+#   count                = var.num_datagateway
+#   virtual_machine_id   = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
+
+#   publisher            = "Microsoft.Azure.Security"
+#   type                 = "IaaSAntimalware"
+#   type_handler_version = "1.3"
+#   auto_upgrade_minor_version = true
+#   settings = <<SETTINGS
+#     {
+#     "AntimalwareEnabled": true,
+#     "RealtimeProtectionEnabled": "true",
+#     "ScheduledScanSettings": {
+#     "isEnabled": "true",
+#     "day": "1",
+#     "time": "120",
+#     "scanType": "Quick"
+#     },
+#     "Exclusions": {
+#     "Extensions": "",
+#     "Paths": "",
+#     "Processes": ""
+#     }
+#     }
+# SETTINGS
+#   tags                = var.common_tags
+# }
+
+# # resource "azurerm_security_center_server_vulnerability_assessment" "vulneass" {
+# #   count                  = var.num_datagateway
+# #   virtual_machine_id = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
+# # }
+
+# resource "azurerm_virtual_machine_extension" "dtgtwydaa-agent" {
+#   name                       = "DependencyAgentWindows"
+#   count                      = var.num_datagateway
+#   virtual_machine_id         = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
+#   publisher                  = "Microsoft.Azure.Monitoring.DependencyAgent"
+#   type                       = "DependencyAgentWindows"
+#   type_handler_version       = "9.10"
+#   automatic_upgrade_enabled  = true
+#   auto_upgrade_minor_version = true
+#   tags                    = var.common_tags
+# }
 
 
-# Add logging and monitoring extensions
-resource "azurerm_virtual_machine_extension" "dtgtwymonitor-agent" {
-  depends_on = [  azurerm_virtual_machine_extension.dtgtwydaa-agent  ]
-  name                  = "AzureMonitorWindowsAgent"
-  count                      = var.num_datagateway
-  virtual_machine_id         = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
-  publisher             = "Microsoft.Azure.Monitor"
-  type                  = "AzureMonitorWindowsAgent"
-  type_handler_version  =  "1.5"
-  automatic_upgrade_enabled  = true
-  auto_upgrade_minor_version = true
-  tags                    = var.common_tags
-}
+# # Add logging and monitoring extensions
+# resource "azurerm_virtual_machine_extension" "dtgtwymonitor-agent" {
+#   depends_on = [  azurerm_virtual_machine_extension.dtgtwydaa-agent  ]
+#   name                  = "AzureMonitorWindowsAgent"
+#   count                      = var.num_datagateway
+#   virtual_machine_id         = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
+#   publisher             = "Microsoft.Azure.Monitor"
+#   type                  = "AzureMonitorWindowsAgent"
+#   type_handler_version  =  "1.5"
+#   automatic_upgrade_enabled  = true
+#   auto_upgrade_minor_version = true
+#   tags                    = var.common_tags
+# }
 
 
-resource "azurerm_virtual_machine_extension" "dtgtwymsmonitor-agent" {
-  depends_on = [  azurerm_virtual_machine_extension.dtgtwydaa-agent  ]
-  name                  = "MicrosoftMonitoringAgent"  # Must be called this
-  count                 = var.num_datagateway
-  virtual_machine_id    = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
-  publisher             = "Microsoft.EnterpriseCloud.Monitoring"
-  type                  = "MicrosoftMonitoringAgent"
-  type_handler_version  =  "1.0"
-  tags                    = var.common_tags
-  # Not yet supported
-  # automatic_upgrade_enabled  = true
-  # auto_upgrade_minor_version = true
-  settings = <<SETTINGS
-    {
-        "workspaceId": "${data.azurerm_log_analytics_workspace.loganalytics.workspace_id}",
-        "azureResourceId": "${azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]}",
-        "stopOnMultipleConnections": "false"
-    }
-  SETTINGS
-  protected_settings = <<PROTECTED_SETTINGS
-    {
-      "workspaceKey": "${data.azurerm_log_analytics_workspace.loganalytics.primary_shared_key}"
-    }
-  PROTECTED_SETTINGS
-  lifecycle {
-    ignore_changes= [name ]
-  }
-}
+# resource "azurerm_virtual_machine_extension" "dtgtwymsmonitor-agent" {
+#   depends_on = [  azurerm_virtual_machine_extension.dtgtwydaa-agent  ]
+#   name                  = "MicrosoftMonitoringAgent"  # Must be called this
+#   count                 = var.num_datagateway
+#   virtual_machine_id    = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
+#   publisher             = "Microsoft.EnterpriseCloud.Monitoring"
+#   type                  = "MicrosoftMonitoringAgent"
+#   type_handler_version  =  "1.0"
+#   tags                    = var.common_tags
+#   # Not yet supported
+#   # automatic_upgrade_enabled  = true
+#   # auto_upgrade_minor_version = true
+#   settings = <<SETTINGS
+#     {
+#         "workspaceId": "${data.azurerm_log_analytics_workspace.loganalytics.workspace_id}",
+#         "azureResourceId": "${azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]}",
+#         "stopOnMultipleConnections": "false"
+#     }
+#   SETTINGS
+#   protected_settings = <<PROTECTED_SETTINGS
+#     {
+#       "workspaceKey": "${data.azurerm_log_analytics_workspace.loganalytics.primary_shared_key}"
+#     }
+#   PROTECTED_SETTINGS
+#   lifecycle {
+#     ignore_changes= [name ]
+#   }
+# }
 
-module "dynatrace-oneagent-dtgtway" {
+# module "dynatrace-oneagent-dtgtway" {
   
-  source               = "git@github.com:hmcts/terraform-module-dynatrace-oneagent.git?ref=master"
-  count                = var.num_datagateway
-  tenant_id            = "${data.azurerm_key_vault_secret.dynatrace-token.value}"
-  token                = "${data.azurerm_key_vault_secret.dynatrace-tenant-id.value}"
-  virtual_machine_os   = "windows"
-  virtual_machine_type = "vm"
-  virtual_machine_id   = "${azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]}"
-  tags                 = var.common_tags
-}
+#   source               = "git@github.com:hmcts/terraform-module-dynatrace-oneagent.git?ref=master"
+#   count                = var.num_datagateway
+#   tenant_id            = "${data.azurerm_key_vault_secret.dynatrace-token.value}"
+#   token                = "${data.azurerm_key_vault_secret.dynatrace-tenant-id.value}"
+#   virtual_machine_os   = "windows"
+#   virtual_machine_type = "vm"
+#   virtual_machine_id   = "${azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]}"
+#   tags                 = var.common_tags
+# }
 
-resource "azurerm_dev_test_global_vm_shutdown_schedule" "dtgtwyvm" {
-  count                  = var.num_datagateway
-  virtual_machine_id     = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
-  location               = azurerm_resource_group.rg.location
-  enabled                = true
+# resource "azurerm_dev_test_global_vm_shutdown_schedule" "dtgtwyvm" {
+#   count                  = var.num_datagateway
+#   virtual_machine_id     = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
+#   location               = azurerm_resource_group.rg.location
+#   enabled                = true
 
-  daily_recurrence_time = "1800"
-  timezone              = "GMT Standard Time"
+#   daily_recurrence_time = "1800"
+#   timezone              = "GMT Standard Time"
 
 
-  notification_settings {
-    enabled         = false
+#   notification_settings {
+#     enabled         = false
    
-  }
-  tags                = var.common_tags
- }
+#   }
+#   tags                = var.common_tags
+#  }
 
 # resource "azurerm_security_center_server_vulnerability_assessment" "vulass" {
 #   count                  = var.num_vid_edit_vms
