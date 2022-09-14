b9d916c75d0e2354ff054c5c0745a7946d9b58e6
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-14 18:10:44 +0100

    bastionnsg

--- bastionnsg.tf
+++ bastionnsg.tf
@@ -142,6 +142,7 @@ resource "azurerm_network_security_group" "bastionnsg" {
 
 #Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "nsgassoc" {
-    network_interface_id      = azurerm_network_interface.nic.id 
+    count                     = var.num_vid_edit_vms
+    network_interface_id      = [azurerm_network_interface.nic[count.index].id]
     network_security_group_id = azurerm_network_security_group.bastionnsg.id
     }
