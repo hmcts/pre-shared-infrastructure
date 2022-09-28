4115746ce9b08184da5a49fa7413895a5258abe3
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-28 16:29:34 +0100

    automation,domainjoin

--- roles.tf
+++ roles.tf
@@ -2,9 +2,12 @@
  data "azurerm_user_assigned_identity" "managed-identity" {
   name                = "${var.product}-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"
-  depends_on = [ module.key-vault]
 }
 
+
+data "azuread_groups" "groups" {
+  display_names = ["DTS-PRE-VideoEditing-SecurityGroup-${var.env}"]
+}
 
 #Storage Blob Data Contributor Role Assignment for Managed Identity
 resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
@@ -19,6 +22,55 @@ resource "azurerm_role_assignment" "pre_reader_mi" {
   role_definition_name             = "Reader"
   principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
   skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "vm_user_mi" {
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_name             = "Virtual Machine Contributor"
+  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "vm_user_aa" {
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_name             = "Virtual Machine Contributor"
+  principal_id                     = azurerm_automation_account.pre-aa.identity[0].principal_id 
+  skip_service_principal_aad_check = true
+}
+
+# DTS-PRE-VideoEditing-SecurityGroup-
+data "azuread_groups" "groups" {
+  display_names = ["DTS-PRE-VideoEditing-SecurityGroup-${var.env}"]
+}
+
+resource "azurerm_role_assignment" "vmuser_login" {
+  for_each             = toset(data.azuread_groups.groups.object_ids)
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Virtual Machine User Login"
+  principal_id         = each.value
+}
+
+resource "azurerm_role_assignment" "vmnic_reader" {
+  count                = var.num_vid_edit_vms
+  for_each             = toset(data.azuread_groups.groups.object_ids)
+  scope                = [azurerm_network_interface.nic[count.index].id]
+  role_definition_name = "Reader"
+  principal_id         = each.value
+}
+
+resource "azurerm_role_assignment" "vm_reader" {
+  count                = var.num_vid_edit_vms
+  for_each             = toset(data.azuread_groups.groups.object_ids)
+  scope                = azurerm_windows_virtual_machine.vm.*.id[count.index]
+  role_definition_name = "Reader"
+  principal_id         = each.value
+}
+
+resource "azurerm_role_assignment" "bastion_reader" {
+  for_each             = toset(data.azuread_groups.groups.object_ids)
+  scope                = azurerm_bastion_host.bastion.id
+  role_definition_name = "Reader"
+  principal_id         = each.value
 }
 
 # resource "azurerm_role_assignment" "pre_power_app_blob" {
