d7fdecd081963b461aee9bfc70fefbe9e77319d6
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-28 12:37:48 +0100

    vm id

--- joindomain.tf
+++ joindomain.tf
@@ -27,7 +27,7 @@
 # }
 
 resource "azurerm_virtual_machine_extension" "vm_aad" {
-  count                      = var.join_domain == true ? var.num_vid_edit_vms : 0
+  count                      =  var.num_vid_edit_vms
   name                       = "AADLoginForWindows"
   virtual_machine_id         = azurerm_windows_virtual_machine.vm.*.id[count.index]
   publisher                  = "Microsoft.Azure.ActiveDirectory"
