06650cab2d54d3d302473a97cf57775c405a03ee
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-28 13:30:14 +0100

    vm id

--- roles.tf
+++ roles.tf
@@ -26,21 +26,21 @@ resource "azurerm_role_assignment" "pre_reader_mi" {
 
 resource "azurerm_role_assignment" "vm_user_mi" {
   scope                            = azurerm_resource_group.rg.id
-  role_definition_name             = "Virtual Machine User Login"
+  role_definition_name             = "Virtual Machine Contributor"
   principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_role_assignment" "vm_user" {
+resource "azurerm_role_assignment" "vm_user_aa" {
   scope                            = azurerm_resource_group.rg.id
-  role_definition_name             = "Virtual Machine User Login"
+  role_definition_name             = "Virtual Machine Contributor"
   principal_id                     = azurerm_automation_account.pre-aa.identity[0].principal_id 
   skip_service_principal_aad_check = true
 }
 
 resource "azurerm_role_assignment" "vmuser" {
   for_each             = toset(data.azuread_groups.groups.object_ids)
-  scope                = azurerm_resource_group.tenable_rg.id
+  scope                = azurerm_resource_group.rg.id
   role_definition_name = "Virtual Machine User Login"
   principal_id         = each.value
 }
