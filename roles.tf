decf46a68ea7d6e9fdaa8dd80cb315ef4086afbc
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-29 14:45:40 +0100

    ams api

--- roles.tf
+++ roles.tf
@@ -4,8 +4,9 @@
   resource_group_name = "managed-identities-${var.env}-rg"
 }
 
-# DTS-PRE-VideoEditing-SecurityGroup-
-
+data "azuread_groups" "groups" {
+  display_names = ["DTS-PRE-VideoEditing-SecurityGroup-${var.env}"]
+}
 
 #Storage Blob Data Contributor Role Assignment for Managed Identity
 resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
@@ -36,10 +37,35 @@ resource "azurerm_role_assignment" "vm_user_aa" {
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_role_assignment" "vmuser" {
+# DTS-PRE-VideoEditing-SecurityGroup-
+resource "azurerm_role_assignment" "vmuser_login" {
   for_each             = toset(data.azuread_groups.groups.object_ids)
   scope                = azurerm_resource_group.rg.id
   role_definition_name = "Virtual Machine User Login"
+  principal_id         = each.value
+}
+
+resource "azurerm_role_assignment" "vmnic_reader" {
+  
+  for_each             = toset(data.azuread_groups.groups.object_ids)
+  count                = var.num_vid_edit_vms
+  scope                = [azurerm_network_interface.nic[count.index].id]
+  role_definition_name = "Reader"
+  principal_id         = each.value
+}
+
+resource "azurerm_role_assignment" "vm_reader" {
+  # count                = var.num_vid_edit_vms
+  for_each             = toset(data.azuread_groups.groups.object_ids)
+  scope                = azurerm_windows_virtual_machine.vm.*.id #[count.index]
+  role_definition_name = "Reader"
+  principal_id         = each.value
+}
+
+resource "azurerm_role_assignment" "bastion_reader" {
+  for_each             = toset(data.azuread_groups.groups.object_ids)
+  scope                = azurerm_bastion_host.bastion.id
+  role_definition_name = "Reader"
   principal_id         = each.value
 }
 
