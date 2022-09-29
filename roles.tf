dbfe41636de9246727c665f0832f598998a64495
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-29 18:56:14 +0100

    ams api

--- roles.tf
+++ roles.tf
@@ -37,37 +37,37 @@ resource "azurerm_role_assignment" "vm_user_aa" {
   skip_service_principal_aad_check = true
 }
 
-# DTS-PRE-VideoEditing-SecurityGroup-
-resource "azurerm_role_assignment" "vmuser_login" {
-  for_each             = toset(data.azuread_groups.groups.object_ids)
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Virtual Machine User Login"
-  principal_id         = each.value
-}
+# # DTS-PRE-VideoEditing-SecurityGroup-
+# resource "azurerm_role_assignment" "vmuser_login" {
+#   for_each             = toset(data.azuread_groups.groups.object_ids)
+#   scope                = azurerm_resource_group.rg.id
+#   role_definition_name = "Virtual Machine User Login"
+#   principal_id         = each.value
+# }
 
-resource "azurerm_role_assignment" "vmnic_reader" {
+# resource "azurerm_role_assignment" "vmnic_reader" {
   
-  for_each             = toset(data.azuread_groups.groups.object_ids)
-  # count                = var.num_vid_edit_vms
-  scope                = azurerm_network_interface.nic.*.id
-  role_definition_name = "Reader"
-  principal_id         = each.value
-}
+#   for_each             = toset(data.azuread_groups.groups.object_ids)
+#   # count                = var.num_vid_edit_vms
+#   scope                = azurerm_network_interface.nic.*.id
+#   role_definition_name = "Reader"
+#   principal_id         = each.value
+# }
 
-resource "azurerm_role_assignment" "vm_reader" {
-  # count                = var.num_vid_edit_vms
-  for_each             = toset(data.azuread_groups.groups.object_ids)
-  scope                = azurerm_windows_virtual_machine.vm.*.id #[count.index]
-  role_definition_name = "Reader"
-  principal_id         = each.value
-}
+# resource "azurerm_role_assignment" "vm_reader" {
+#   # count                = var.num_vid_edit_vms
+#   for_each             = toset(data.azuread_groups.groups.object_ids)
+#   scope                = azurerm_windows_virtual_machine.vm.*.id #[count.index]
+#   role_definition_name = "Reader"
+#   principal_id         = each.value
+# }
 
-resource "azurerm_role_assignment" "bastion_reader" {
-  for_each             = toset(data.azuread_groups.groups.object_ids)
-  scope                = azurerm_bastion_host.bastion.id
-  role_definition_name = "Reader"
-  principal_id         = each.value
-}
+# resource "azurerm_role_assignment" "bastion_reader" {
+#   for_each             = toset(data.azuread_groups.groups.object_ids)
+#   scope                = azurerm_bastion_host.bastion.id
+#   role_definition_name = "Reader"
+#   principal_id         = each.value
+# }
 
 # resource "azurerm_role_assignment" "pre_power_app_blob" {
 #   scope                            = azurerm_resource_group.rg.id
