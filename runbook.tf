29dcf1207f925c3d27122895e633f5357bcd0d3c
Olu Ayodele <olu.ayodele@hmcts.net>
2022-09-30 13:40:37 +0100

    ams-id

--- runbook.tf
+++ runbook.tf
@@ -19,7 +19,7 @@ resource "azurerm_automation_account" "pre-aa" {
 module "vm_automation" {
   # editvmcount = var.num_vid_edit_vms
   # dtgtwycount = var.num_datagateway
-  for_each = toset( ["${azurerm_windows_virtual_machine.vm.*.name}", "${azurerm_windows_virtual_machine.dtgtwyvm.*.name}"] )
+  # for_each = toset( ["${azurerm_windows_virtual_machine.vm.*.name}", "${azurerm_windows_virtual_machine.dtgtwyvm.*.name}"] )
   source = "git::https://github.com/hmcts/cnp-module-automation-runbook-start-stop-vm"
 
   product                 = var.product
@@ -44,11 +44,43 @@ module "vm_automation" {
                       }
                      ]
   resource_group_name     = azurerm_resource_group.rg.name
-  vm_names                = each.value
+  vm_names                = azurerm_windows_virtual_machine.vm.*.name
   mi_principal_id         = azurerm_automation_account.pre-aa.identity[0].principal_id 
  
 }
 
+module "vm_automation_dtgtwy" {
+  # editvmcount = var.num_vid_edit_vms
+  # dtgtwycount = var.num_datagateway
+  # for_each = toset( ["${azurerm_windows_virtual_machine.vm.*.name}", "${azurerm_windows_virtual_machine.dtgtwyvm.*.name}"] )
+  source = "git::https://github.com/hmcts/cnp-module-automation-runbook-start-stop-vm"
+
+  product                 = var.product
+  env                     = var.env
+  location                = var.location
+  automation_account_name = azurerm_automation_account.pre-aa.name
+  tags                    = var.common_tags
+  schedules               =  [
+                      {
+                        name        = "vm-on"
+                        frequency   = "Day"
+                        interval    = 1
+                        run_time    = "13:00:00"
+                        start_vm    = true
+                      },
+                      {
+                        name        = "vm-off"
+                        frequency   = "Day"
+                        interval    = 1
+                        run_time    = "18:00:00"
+                        start_vm    = false
+                      }
+                     ]
+  resource_group_name     = azurerm_resource_group.rg.name
+  vm_names                = azurerm_windows_virtual_machine.dtgtwyvm.*.name
+  mi_principal_id         = azurerm_automation_account.pre-aa.identity[0].principal_id 
+ 
+}
 # data "azurerm_log_analytics_linked_service" "la_linked_service" {
 #   provider            = azurerm.oms
 #   resource_group_name = data.azurerm_log_analytics_workspace.loganalytics.resource_group_name
