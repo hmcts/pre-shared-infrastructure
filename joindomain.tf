883a74025087265c3c49f5fd2cfaecc4dc68235b
Olu Ayodele <olu.ayodele@hmcts.net>
2022-10-03 16:53:48 +0100

    updates

--- joindomain.tf
+++ joindomain.tf
@@ -29,7 +29,7 @@
 resource "azurerm_virtual_machine_extension" "vm_aad" {
   count                      =  var.num_vid_edit_vms
   name                       = "AADLoginForWindows"
-  virtual_machine_id         = azurerm_windows_virtual_machine.vm.*.id[count.index]
+  virtual_machine_id         = concat([azurerm_windows_virtual_machine.vm.*.name,azurerm_windows_virtual_machine.dtgtwyvm.*.name]) #azurerm_windows_virtual_machine.vm.*.id[count.index]
   publisher                  = "Microsoft.Azure.ActiveDirectory"
   type                       = "AADLoginForWindows"
   type_handler_version       = "1.0"
