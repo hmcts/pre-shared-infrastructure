855a5b50bd71f8f28a7e146f22263b7e0f8d66f1
Olu Ayodele <olu.ayodele@hmcts.net>
2022-10-04 19:16:02 +0100

    peering

--- provider.tf
+++ provider.tf
@@ -54,6 +54,68 @@ provider "azurerm" {
   subscription_id = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].subscription_id
 }
 
+provider "azurerm" {
+  alias                      = "hmcts-control"
+  skip_provider_registration = "true"
+  features {}
+  subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+}
+
+
+provider "azurerm" {
+  subscription_id            = local.hub[var.environment].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub"
+}
+
+provider "azurerm" {
+  subscription_id            = var.private_dns_subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "private-dns"
+}
+
+provider "azurerm" {
+  subscription_id            = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  skip_provider_registration = "true"
+  features {}
+  alias = "private-dns-private-endpoint"
+}
+
+provider "azurerm" {
+  subscription_id            = "ed302caf-ec27-4c64-a05e-85731c3ce90e"
+  skip_provider_registration = "true"
+  features {}
+  alias = "vpn"
+}
+
+provider "azurerm" {
+  alias = "data"
+  features {}
+  subscription_id = module.log_analytics_workspace.subscription_id
+}
+
+provider "azurerm" {
+  subscription_id            = local.hub["sbox"].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub-sbox"
+}
+
+provider "azurerm" {
+  subscription_id            = local.hub["nonprod"].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub-nonprod"
+}
+
+provider "azurerm" {
+  subscription_id            = local.hub["prod"].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub-prod"
+}
 # data "azurerm_key_vault" "cnp_vault" {
 #   provider            = azurerm.cnp
 #   name                = "infra-vault-${local.dynatrace_env}"
