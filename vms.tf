c2eeb86724b8b5f3ce013e1c6e4975291449e2d5
Olu Ayodele <olu.ayodele@hmcts.net>
2022-10-12 18:02:39 +0100

    endpont

--- vms.tf
+++ vms.tf
@@ -246,11 +246,11 @@ module "dynatrace-oneagent" {
   
   source               = "git@github.com:hmcts/terraform-module-dynatrace-oneagent.git?ref=master"
   count                = var.num_vid_edit_vms
-  tenant_id            = "${data.azurerm_key_vault_secret.dynatrace-tenant-id.value}"
-  token                =  "${data.azurerm_key_vault_secret.dynatrace-token.value}"
+  tenant_id            = data.azurerm_key_vault_secret.dynatrace-tenant-id.value
+  token                =  data.azurerm_key_vault_secret.dynatrace-token.value
   virtual_machine_os   = "windows"
   virtual_machine_type = "vm"
-  virtual_machine_id   = "${azurerm_windows_virtual_machine.vm.*.id[count.index]}"
+  virtual_machine_id   = azurerm_windows_virtual_machine.vm.*.id[count.index]
   auto_upgrade_minor_version = true
   server                     = var.server
   hostgroup                  = var.hostgroup
@@ -433,8 +433,8 @@ module "dynatrace-oneagent-dtgtway" {
   
   source                     = "github.com/hmcts/terraform-module-dynatrace-oneagent"
   count                      = var.num_datagateway
-  tenant_id                  = "${data.azurerm_key_vault_secret.dynatrace-tenant-id.value}"
-  token                      = "${data.azurerm_key_vault_secret.dynatrace-token.value}"
+  tenant_id                  = data.azurerm_key_vault_secret.dynatrace-tenant-id.value
+  token                      = data.azurerm_key_vault_secret.dynatrace-token.value
   virtual_machine_os         = "Windows"
   virtual_machine_type       = "vm"
   virtual_machine_id         = "${azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]}"
