97d952ed9af02f6196cf71669a573e506bcabb5c
Olu Ayodele <olu.ayodele@hmcts.net>
2022-10-14 15:15:03 +0100

    update editvm

--- modules/vnet_peering/init.tf
+++ modules/vnet_peering/init.tf
@@ -1,10 +1,10 @@
 
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source                = "hashicorp/azurerm"
-#       version               = ">= 3.3.0"
-#       configuration_aliases = [azurerm.initiator, azurerm.target]
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      version               = ">= 3.3.0"
+      configuration_aliases = [azurerm.initiator, azurerm.target]
+    }
+  }
+}
