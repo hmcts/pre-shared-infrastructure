e4fed333da6b180170853df90abf1333784e3f4e
Olu Ayodele <olu.ayodele@hmcts.net>
2022-10-17 13:43:51 +0100

    vnet peering

--- winupdate.tf
+++ winupdate.tf
@@ -25,6 +25,7 @@ locals {
 #This type should eventually replace the manual deploy via azurerm: azurerm_automation_softwareUpdateConfigurations
 #https://github.com/terraform-providers/terraform-provider-azurerm/issues/2812
 resource "azurerm_template_deployment" "windows" {
+  count               =  var.num_vid_edit_vms
   name                = "windows-update"
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -48,9 +49,63 @@ resource "azurerm_template_deployment" "windows" {
                       "rebootSetting": "${local.update_reboot_settings}"
                   },
                   "azureVirtualMachines": [
-                      "${azurerm_windows_virtual_machine.dtgtwyvm.*.name[0]}",
-                      "${azurerm_windows_virtual_machine.vm.*.name[0]}"
+                      "${azurerm_windows_virtual_machine.vm.*.id[count.index]}"
+                      
                   ],
+                  "nonAzureComputerNames": [
+                  ]
+              },
+              "scheduleInfo": {
+                  "frequency": "Month",
+                  "startTime": "${local.update_date}T${local.update_time}:00",
+                  "timeZone":  "${local.update_timezone}",
+                  "interval": 1,
+                  "advancedSchedule": {
+                      "monthlyOccurrences": [
+                          {
+                            "occurrence": "${local.update_occurrence}",
+                            "day": "${local.update_day}"
+                          }
+                      ]
+                  }
+              }
+          }
+      }
+    ]
+  }
+  DEPLOY
+
+  deployment_mode = "Incremental"
+}
+
+
+resource "azurerm_resource_group_template_deployment" "dtgtwwindows" {
+  count               =  var.num_datagateway
+  name                = "windows-update"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  template_body = <<DEPLOY
+  {
+    "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "resources": [
+      {
+          "apiVersion": "2017-05-15-preview",
+          "type": "Microsoft.Automation/automationAccounts/softwareUpdateConfigurations",
+          "name": "${azurerm_automation_account.pre-aa.name}/windows-updates",
+          "properties": {
+              "updateConfiguration": {
+                  "operatingSystem": "Windows",
+                  "duration": "PT${local.update_max_hours}H",
+                  "windows": {
+                      "excludedKbNumbers": [
+                      ],
+                      "includedUpdateClassifications": "${local.update_classifications}",
+                      "rebootSetting": "${local.update_reboot_settings}"
+                  },
+                  "azureVirtualMachines": [
+                      "${azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]}"
+                      ],
                   "nonAzureComputerNames": [
                   ]
               },
