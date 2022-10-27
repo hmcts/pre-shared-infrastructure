cf020e232616f8c9c56f8920ed982ac87bb00e8e
Olu Ayodele <olu.ayodele@hmcts.net>
2022-10-27 10:10:46 +0100

    ams

--- vms.tf
+++ vms.tf
@@ -482,3 +482,28 @@ resource "azurerm_dev_test_global_vm_shutdown_schedule" "dtgtwyvm" {
  }
 
 
+#####
+## Os Disk Encryption"
+#####
+
+# resource "azurerm_virtual_machine_extension" "disk-encryption" {
+#   name                 = "DiskEncryption"
+#   location             = "${local.location}"
+#   resource_group_name  = "${azurerm_resource_group.environment-rg.name}"
+#   virtual_machine_name = "${azurerm_virtual_machine.server.name}"
+#   publisher            = "Microsoft.Azure.Security"
+#   type                 = "AzureDiskEncryption"
+#   type_handler_version = "2.2"
+
+#   settings = <<SETTINGS
+# {
+#   "EncryptionOperation": "EnableEncryption",
+#   "KeyVaultURL": "https://${local.vaultname}.vault.azure.net",
+#   "KeyVaultResourceId": "/subscriptions/${local.subscriptionid}/resourceGroups/${local.vaultresourcegroup}/providers/Microsoft.KeyVault/vaults/${local.vaultname}",
+#   "KeyEncryptionKeyURL": "https://${local.vaultname}.vault.azure.net/keys/${local.keyname}/${local.keyversion}",
+#   "KekVaultResourceId": "/subscriptions/${local.subscriptionid}/resourceGroups/${local.vaultresourcegroup}/providers/Microsoft.KeyVault/vaults/${local.vaultname}",
+#   "KeyEncryptionAlgorithm": "RSA-OAEP",
+#   "VolumeType": "All"
+# }
+# SETTINGS
+# }
