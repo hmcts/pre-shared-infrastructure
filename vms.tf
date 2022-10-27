6679c8819f2a35bd624192c71a329056dafd8279
Olu Ayodele <olu.ayodele@hmcts.net>
2022-10-27 16:55:55 +0100

    perftest

--- vms.tf
+++ vms.tf
@@ -1,7 +1,7 @@
 ##------------------------------------------------------###################
 ##BASTION
 ##------------------------------------------------------###################
-resource "azurerm_public_ip" "pip" {oneagent-
+resource "azurerm_public_ip" "pip" {
   name                = "${var.product}-bastionpip-${var.env}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
