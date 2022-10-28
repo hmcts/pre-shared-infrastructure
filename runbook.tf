688efc4cd55b378c17c31744d591bd54ae97a749
Don-Olu <98063921+Don-Olu@users.noreply.github.com>
2022-10-28 17:55:03 +0100

    Perftest (#126)

--- runbook.tf
+++ runbook.tf
@@ -6,14 +6,24 @@ resource "azurerm_automation_account" "pre-aa" {
 
  identity {
     type         = "SystemAssigned"
+
+    # UserAssigned"
+    # identity_ids = data.azurerm_user_assigned_identity.managed-identity.principal_id
     }
+  # lifecycle {
+  #   ignore_changes= [ name]
+  # }
+
+
+    }
+
   tags = var.common_tags
 }
 
 module "vm_automation" {
-  # editvmcount = var.num_vid_edit_vms
-  # dtgtwycount = var.num_datagateway
+
   source = "git@github.com:hmcts/cnp-module-automation-runbook-start-stop-vm?ref=master"
+
 
   product                 = var.product
   env                     = var.env
@@ -42,11 +52,46 @@ module "vm_automation" {
  
 }
 
+
+module "vm_automation_dtgtwy" {
+  source = "git@github.com:hmcts/cnp-module-automation-runbook-start-stop-vm?ref=master"
+
+  product                 = "${var.product}-dtgtwy"
+  env                     = var.env
+  location                = var.location
+  automation_account_name = azurerm_automation_account.pre-aa.name
+  tags                    = var.common_tags
+  schedules               =  [
+                      {
+                        name        = "vm-on"
+                        frequency   = "Day"
+                        interval    = 1
+                        run_time    = "06:00:00"
+                        start_vm    = true
+                      },
+                      {
+                        name        = "vm-off"
+                        frequency   = "Day"
+                        interval    = 1
+                        run_time    = "18:00:00"
+                        start_vm    = false
+                      }
+                     ]
+  resource_group_name     = azurerm_resource_group.rg.name
+  vm_names                = azurerm_windows_virtual_machine.dtgtwyvm.*.name
+  mi_principal_id         = azurerm_automation_account.pre-aa.identity[0].principal_id 
+ 
+}
+
+
 # data "azurerm_log_analytics_linked_service" "la_linked_service" {
 #   provider            = azurerm.oms
 #   resource_group_name = data.azurerm_log_analytics_workspace.loganalytics.resource_group_name
 #   workspace_id        = module.log_analytics_workspace.workspace_id
+#   #read_access_id      = azurerm_automation_account.pre-aa.id
+=======
 #   # read_access_id      = azurerm_automation_account.pre-aa.id
+
 # }
 
 
@@ -61,9 +106,25 @@ module "vm_automation" {
 #     publisher = "Microsoft"
 #     product   = "OMSGallery/Updates"
 #   }
+
+#   # depends_on = [
+#   #   azurerm_log_analytics_linked_service.la_linked_service
+#   # ]
+
+# # }
+
+# data "azurerm_automation_account" "pre-aa" {
+#   name                = "${var.product}-${var.env}-aa"
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+
+#  for vms in azurerm_windows_virtual_machine.vm : vms.name
+
+
 #   depends_on = [
 #     azurerm_log_analytics_linked_service.la_linked_service
 #   ]
 
 # }
+
 
