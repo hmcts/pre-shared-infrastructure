d7cec93e1f8df179ac082d5c499799b20c16a1f1
darrenbayliss-dev <61244269+darrenbayliss-dev@users.noreply.github.com>
2022-10-28 11:29:42 +0100

    Merge master into demo (#125)
    merge master to demo

--- roles.tf
+++ roles.tf
@@ -4,6 +4,9 @@
   resource_group_name = "managed-identities-${var.env}-rg"
 }
 
+data "azuread_groups" "groups" {
+  display_names = ["DTS-PRE-VideoEditing-SecurityGroup-${var.env}"]
+}
 
 #Storage Blob Data Contributor Role Assignment for Managed Identity
 resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
@@ -19,6 +22,52 @@ resource "azurerm_role_assignment" "pre_reader_mi" {
   principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
   skip_service_principal_aad_check = true
 }
+
+resource "azurerm_role_assignment" "vm_user_mi" {
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_name             = "Virtual Machine Contributor"
+  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "vm_user_aa" {
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_name             = "Virtual Machine Contributor"
+  principal_id                     = azurerm_automation_account.pre-aa.identity[0].principal_id 
+  skip_service_principal_aad_check = true
+}
+
+# # DTS-PRE-VideoEditing-SecurityGroup-
+# resource "azurerm_role_assignment" "vmuser_login" {
+#   for_each             = toset(data.azuread_groups.groups.object_ids)
+#   scope                = azurerm_resource_group.rg.id
+#   role_definition_name = "Virtual Machine User Login"
+#   principal_id         = each.value
+# }
+
+# resource "azurerm_role_assignment" "vmnic_reader" {
+  
+#   for_each             = toset(data.azuread_groups.groups.object_ids)
+#   # count                = var.num_vid_edit_vms
+#   scope                = azurerm_network_interface.nic.*.id
+#   role_definition_name = "Reader"
+#   principal_id         = each.value
+# }
+
+# resource "azurerm_role_assignment" "vm_reader" {
+#   # count                = var.num_vid_edit_vms
+#   for_each             = toset(data.azuread_groups.groups.object_ids)
+#   scope                = azurerm_windows_virtual_machine.vm.*.id #[count.index]
+#   role_definition_name = "Reader"
+#   principal_id         = each.value
+# }
+
+# resource "azurerm_role_assignment" "bastion_reader" {
+#   for_each             = toset(data.azuread_groups.groups.object_ids)
+#   scope                = azurerm_bastion_host.bastion.id
+#   role_definition_name = "Reader"
+#   principal_id         = each.value
+# }
 
 # resource "azurerm_role_assignment" "pre_power_app_blob" {
 #   scope                            = azurerm_resource_group.rg.id
---
+++ runbook.tf
@@ -0,0 +1,69 @@
+resource "azurerm_automation_account" "pre-aa" {
+  name                = "${var.product}-${var.env}-aa"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Basic"
+
+ identity {
+    type         = "SystemAssigned"
+    }
+  tags = var.common_tags
+}
+
+module "vm_automation" {
+  # editvmcount = var.num_vid_edit_vms
+  # dtgtwycount = var.num_datagateway
+  source = "git@github.com:hmcts/cnp-module-automation-runbook-start-stop-vm?ref=master"
+
+  product                 = var.product
+  env                     = var.env
+  location                = var.location
+  automation_account_name = azurerm_automation_account.pre-aa.name
+  tags                    = var.common_tags
+  schedules               =  [
+                      {
+                        name        = "vm-on"
+                        frequency   = "Day"
+                        interval    = 1
+                        run_time    = "06:00:00"
+                        start_vm    = true
+                      },
+                      {
+                        name        = "vm-off"
+                        frequency   = "Day"
+                        interval    = 1
+                        run_time    = "18:00:00"
+                        start_vm    = false
+                      }
+                     ]
+  resource_group_name     = azurerm_resource_group.rg.name
+  vm_names                = azurerm_windows_virtual_machine.vm.*.name
+  mi_principal_id         = azurerm_automation_account.pre-aa.identity[0].principal_id 
+ 
+}
+
+# data "azurerm_log_analytics_linked_service" "la_linked_service" {
+#   provider            = azurerm.oms
+#   resource_group_name = data.azurerm_log_analytics_workspace.loganalytics.resource_group_name
+#   workspace_id        = module.log_analytics_workspace.workspace_id
+#   # read_access_id      = azurerm_automation_account.pre-aa.id
+# }
+
+
+# resource "azurerm_log_analytics_solution" "update_solution" {
+#   provider              = azurerm.oms
+#   solution_name         = "Updates"
+#   location              = var.location
+#   resource_group_name   = data.azurerm_log_analytics_workspace.loganalytics.resource_group_name #module.log_analytics_workspace.resource_group_name
+#   workspace_resource_id = module.log_analytics_workspace.workspace_id
+#   workspace_name        = module.log_analytics_workspace.name
+#   plan {
+#     publisher = "Microsoft"
+#     product   = "OMSGallery/Updates"
+#   }
+#   depends_on = [
+#     azurerm_log_analytics_linked_service.la_linked_service
+#   ]
+
+# }
+
