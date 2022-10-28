5b838164c86a290fee8f483a2d69f8fc74090fb6
Don-Olu <98063921+Don-Olu@users.noreply.github.com>
2022-10-28 23:47:50 +0100

    Perftest (#137)
    All

--- runbook.tf
+++ runbook.tf
@@ -42,9 +42,8 @@ source =  "git@github.com:hmcts/cnp-module-automation-runbook-start-stop-vm?ref=
                       }
                      ]
   resource_group_name     = azurerm_resource_group.rg.name
-  vm_names                = azurerm_windows_virtual_machine.dtgtwyvm.*.name
+  vm_names                = azurerm_windows_virtual_machine.vm.*.name
   mi_principal_id         = azurerm_automation_account.pre-aa.identity[0].principal_id 
- 
 }
 
 
