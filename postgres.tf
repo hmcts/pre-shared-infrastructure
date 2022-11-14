621d4d8d09b707ae46fe3b02953ee57744dbd192
darren.bayliss <d.bayliss@virgin.net>
2022-11-14 15:45:54 +0000

    Replacing private endpoint with vnet peering and routing

--- postgres.tf
+++ postgres.tf
@@ -39,7 +39,7 @@ module "data-store-db-v14" {
   project   = var.project
 
   common_tags     = var.common_tags
-  name            = "${var.database_name}" #-${var.env}" removed as it looks like env gets added in root module
+  name            = var.database_name #-${var.env}" removed as it looks like env gets added in root module
   pgsql_databases = var.pg_databases
 
   pgsql_delegated_subnet_id = data.azurerm_subnet.ss_subnet_pre_postgresql.id
@@ -75,27 +75,77 @@ data "azurerm_subnet" "ss_subnet_pre_postgresql" {
   resource_group_name  = data.azurerm_resource_group.ss_resource_group.name
 }
 
+#remove section below, cannot use private endpoints with postgres flexi server, replace with routing and peering
 // Private endpoint for postgres in platops resource group
-resource "azurerm_private_endpoint" "endpoint" {
-  name                = format("%s-%s-private-endpoint", var.product, var.env)
-  location            = var.location
-  resource_group_name = data.azurerm_resource_group.ss_resource_group.name
-  subnet_id           = data.azurerm_subnet.ss_subnet_pre_postgresql.id
+#resource "azurerm_private_endpoint" "endpoint" {
+#  name                = format("%s-%s-private-endpoint", var.product, var.env)
+#  location            = var.location
+#  resource_group_name = data.azurerm_resource_group.ss_resource_group.name
+#  subnet_id           = data.azurerm_subnet.ss_subnet_pre_postgresql.id
 
-  private_service_connection {
-    name                           = format("%s-%s-privateserviceconnection", var.product, var.env)
-    private_connection_resource_id = module.data-store-db-v14.instance_id #output from https://github.com/hmcts/terraform-module-postgresql-flexible/blob/master/outputs.tf
-    is_manual_connection           = false
-    subresource_names              = ["postgresqlServer"]
+#  private_service_connection {
+#    name                           = format("%s-%s-privateserviceconnection", var.product, var.env)
+#    private_connection_resource_id = module.data-store-db-v14.instance_id #output from https://github.com/hmcts/terraform-module-postgresql-flexible/blob/master/outputs.tf
+#    is_manual_connection           = false
+#    subresource_names              = ["postgresqlServer"]
+#  }
+#  tags = var.common_tags
+#}
+
+#resource "azurerm_private_dns_a_record" "dns_a" {
+#  provider            = azurerm.DNS
+#  name                = format("%s-%s-postgres-privlink", var.product, var.env)
+#  zone_name           = var.PrivateDNSZone
+#  resource_group_name = var.DNSResGroup
+#  ttl                 = 10
+#  records             = [azurerm_private_endpoint.endpoint.private_service_connection.0.private_ip_address]
+#}
+
+# route table to route traffic to postgres subnet
+resource "azurerm_route_table" "postgres" {
+  name                          = format("%s-%s-route-table", var.product, var.env)
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "route1"
+    address_prefix = data.azurerm_subnet.ss_subnet_pre_postgresql.address_space
+    next_hop_type  = "VnetLocal"
   }
+
   tags = var.common_tags
 }
 
+# Link route table with data gateway subnet
+resource "azurerm_subnet_route_table_association" "dg_subnet" {
+  subnet_id      = azurerm_subnet.datagateway_subnet.id
+  route_table_id = azurerm_route_table.postgres.id
+}
+
+# vnet peering data gateway and postgres
+resource "azurerm_virtual_network_peering" "dg_postgres" {
+  name                      = format("%s-%s-peer-dg-to-postgres", var.product, var.env)
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id = data.azurerm_virtual_network.ss_vnet.id
+}
+
+# connect data gateway vnet to private dns zone (this will contain the A name for postgres)
+resource "azurerm_private_dns_zone_virtual_network_link" "postgres_dg" {
+  name                  = format("%s-%s-virtual-network-link", var.product, var.env)
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = var.PrivateDNSZone
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+# Add the A record for postgres
 resource "azurerm_private_dns_a_record" "dns_a" {
   provider            = azurerm.DNS
-  name                = format("%s-%s-postgres-privlink", var.product, var.env)
+  name                = var.database_name-var.env
   zone_name           = var.PrivateDNSZone
   resource_group_name = var.DNSResGroup
   ttl                 = 10
-  records             = [azurerm_private_endpoint.endpoint.private_service_connection.0.private_ip_address]
+  records             = data.azurerm_postgresql_flexible_server.var.database_name.private_ip_address
+  #depends_on          = [module.data-store-db-v14]
 }
