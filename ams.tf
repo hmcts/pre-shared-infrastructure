778e3b040c2c32204501239921038a97da2b9a2d
darren.bayliss <d.bayliss@virgin.net>
2022-12-01 11:46:03 +0000

    Removing system assigned from ams, leaving in on ams02 due to errors

--- ams.tf
+++ ams.tf
@@ -3,9 +3,9 @@
   location                      = "UKwest"
   resource_group_name           = azurerm_resource_group.rg.name
   
-   identity {
-     type = "SystemAssigned"
-   } 
+   #identity {
+   #  type = "SystemAssigned"
+   #} 
 
 
 
