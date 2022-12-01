707104b4408f53f832703bd29999ce7abcf7d1a3
darren.bayliss <d.bayliss@virgin.net>
2022-12-01 11:59:34 +0000

    Renaming project to business_area as this has just changed in the root postgres flexi module

--- postgres.tf
+++ postgres.tf
@@ -7,16 +7,16 @@ module "data_store_db_v14" {
   source = "git@github.com:hmcts/terraform-module-postgresql-flexible.git?ref=master"
   env    = var.env
 
-  product   = var.product
-  component = var.component
-  project   = var.project
+  product       = var.product
+  component     = var.component
+  business_area = var.project
 
   common_tags     = var.common_tags
   name            = var.database_name #-${var.env}" removed as it looks like env gets added in root module
   pgsql_databases = var.pg_databases
 
-  pgsql_version             = "14"
-  backup_retention_days     = 35
+  pgsql_version         = "14"
+  backup_retention_days = 35
 
   location             = var.location
   resource_group_name  = azurerm_resource_group.rg.name
@@ -45,7 +45,7 @@ resource "azurerm_key_vault_secret" "POSTGRES_PASS" {
 }
 
 provider "azurerm" {
-  alias =  "private_dns"
+  alias           = "private_dns"
   subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
   features {}
 
