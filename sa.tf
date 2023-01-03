d7ce99d85f16542d120d676c22ca9f34a549023c
darren.bayliss <d.bayliss@virgin.net>
2023-01-03 17:38:12 +0000

    Removing blob properties from cors rule due to error

--- sa.tf
+++ sa.tf
@@ -119,15 +119,14 @@ module "ingestsa_storage_account" {
   # #   access_type = "private"
   ## }]
 
-  blob_properties {
-    cors_rule {
+    cors_rule = [
       allowed_headers    = ["*"]
       allowed_methods    = ["GET", "POST"]
       allowed_origins    = ["https://*.justice.gov.uk", "https://*.blob.core.windows.net", "https://*.files.core.windows.net"]
       exposed_headers    = ["*"]
       max_age_in_seconds = 600
-    }
-  }
+    ]
+
   depends_on  = [module.key-vault]
   common_tags = var.common_tags
 
