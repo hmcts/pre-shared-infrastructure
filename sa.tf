5d6d28deca9080b281f6dca5d88b0cd304ef5d3a
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-01-11 12:03:37 +0000

    terraform fmt

--- sa.tf
+++ sa.tf
@@ -49,14 +49,14 @@ module "sa_storage_account" {
 }
 
 module "finalsa_storage_account" {
-  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                      = var.env
-  storage_account_name     = replace("${var.product}finalsa${var.env}", "-", "")
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.location #"UKWest" #As recommended by MS
-  account_kind             = "StorageV2"
-  account_tier             = var.sa_account_tier
-  account_replication_type = var.sa_replication_type
+  source                          = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                             = var.env
+  storage_account_name            = replace("${var.product}finalsa${var.env}", "-", "")
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = var.location #"UKWest" #As recommended by MS
+  account_kind                    = "StorageV2"
+  account_tier                    = var.sa_account_tier
+  account_replication_type        = var.sa_replication_type
   sa_subnets                      = concat([data.azurerm_subnet.jenkins_subnet.id], [azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
   allow_nested_items_to_be_public = false
   ip_rules                        = var.ip_rules
@@ -70,14 +70,14 @@ module "finalsa_storage_account" {
 }
 
 module "ingestsa_storage_account" {
-  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                      = var.env
-  storage_account_name     = replace("${var.product}ingestsa${var.env}", "-", "")
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.location #"UKWest" #As recommended by MS azurerm_resource_group.rg.location
-  account_kind             = "StorageV2"
-  account_tier             = var.sa_account_tier
-  account_replication_type = var.sa_replication_type
+  source                          = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                             = var.env
+  storage_account_name            = replace("${var.product}ingestsa${var.env}", "-", "")
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = var.location #"UKWest" #As recommended by MS azurerm_resource_group.rg.location
+  account_kind                    = "StorageV2"
+  account_tier                    = var.sa_account_tier
+  account_replication_type        = var.sa_replication_type
   sa_subnets                      = concat([data.azurerm_subnet.jenkins_subnet.id], [azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
   allow_nested_items_to_be_public = false
   ip_rules                        = var.ip_rules
