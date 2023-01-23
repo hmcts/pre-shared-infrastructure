0153c60b7ffe3dde7853c1513b7e927bc968f772
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-01-23 13:56:25 +0000

    one thing at a time

--- vms.tf
+++ vms.tf
@@ -85,14 +85,6 @@ resource "azurerm_windows_virtual_machine" "vm" {
   network_interface_ids      = [azurerm_network_interface.nic[count.index].id]
   encryption_at_host_enabled = true
 
-  timezone                   = "GMT Standard Time"
-  enable_automatic_updates   = true
-  provision_vm_agent         = true
-  allow_extension_operations = true
-  tags                       = var.common_tags
-  custom_data                = base64encode(data.template_file.edit-vm-init.rendered)
-
-  depends_on = [null_resource.Encryption, module.key-vault, azurerm_disk_encryption_set.pre-des]
   # encryption_at_host_enabled  = true
   additional_capabilities {
     ultra_ssd_enabled = true
@@ -115,6 +107,15 @@ resource "azurerm_windows_virtual_machine" "vm" {
     type         = "SystemAssigned, UserAssigned"
     identity_ids = module.key-vault.managed_identity_id
   }
+
+  timezone                   = "GMT Standard Time"
+  enable_automatic_updates   = true
+  provision_vm_agent         = true
+  allow_extension_operations = true
+  tags                       = var.common_tags
+  custom_data                = base64encode(data.template_file.edit-vm-init.rendered)
+
+  depends_on = [null_resource.Encryption, module.key-vault, azurerm_disk_encryption_set.pre-des]
 }
 
 # # Datadisk 
@@ -277,12 +278,6 @@ resource "azurerm_windows_virtual_machine" "dtgtwyvm" {
   network_interface_ids      = [azurerm_network_interface.dtgwnic[count.index].id]
   encryption_at_host_enabled = true
 
-  timezone                   = "GMT Standard Time"
-  enable_automatic_updates   = true
-  provision_vm_agent         = true
-  allow_extension_operations = true
-  tags                       = var.common_tags
-  depends_on                 = [module.key-vault]
   os_disk {
     name                   = "${var.product}dtgtwy${count.index}-osdisk-${var.env}"
     caching                = "ReadWrite"
@@ -299,6 +294,13 @@ resource "azurerm_windows_virtual_machine" "dtgtwyvm" {
     sku       = "2019-datacenter-gensecond"
     version   = "latest"
   }
+
+  timezone                   = "GMT Standard Time"
+  enable_automatic_updates   = true
+  provision_vm_agent         = true
+  allow_extension_operations = true
+  tags                       = var.common_tags
+  depends_on                 = [module.key-vault]
 }
 
 resource "azurerm_managed_disk" "dtgtwaydatadisk" {
