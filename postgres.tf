0ca053b0e78a6d1a115740b91935e31f85ba7bd3
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-01-30 11:46:47 +0000

    test postgres against branch

--- postgres.tf
+++ postgres.tf
@@ -4,7 +4,7 @@
 ////////////////////////////////
 
 module "data_store_db_v14" {
-  source = "git@github.com:hmcts/terraform-module-postgresql-flexible.git?ref=master"
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible.git?ref=db-collation"
   env    = var.env
 
   product       = var.product
