f8b2c77423cdbddac283e396d9d5900b4cefc370
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-01 17:43:01 +0000

    add a space

--- vms.tf
+++ vms.tf
@@ -283,6 +283,7 @@ resource "azurerm_windows_virtual_machine" "dtgtwyvm" {
   encryption_at_host_enabled = true
 
   custom_data = base64encode(data.template_file.dtgw-vm-init.rendered)
+  
   os_disk {
     name                   = "${var.product}dtgtwy${count.index}-osdisk-${var.env}"
     caching                = "ReadWrite"
