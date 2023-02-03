330e096f4433ef75dd1976dfb5fea3e8c4441e06
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-03 11:55:17 +0000

    rm jenkins files, remove unused variables

--- dev.tfvars
+++ dev.tfvars
@@ -7,17 +7,17 @@ num_vid_edit_vms           = 1
 mgmt_net_name              = "ss-ptl-vnet"
 mgmt_net_rg_name           = "ss-ptl-network-rg"
 env                        = "dev"
-managed_oid                = "2008e85a-5f57-4a54-870e-abbacd6b4d09" #most envs using pre-stg-mi. tbd: use the right ones
+#managed_oid                = "2008e85a-5f57-4a54-870e-abbacd6b4d09" #most envs using pre-stg-mi. tbd: use the right ones
 dts_cft_developers_oid     = "b2a1773c-a5ae-48b5-b5fa-95b0e05eee05" #giving cft-developers additional access to our vault for some reason
-dts_pre_project_admin      = "56a29187-3d5f-4262-99d6-c635776e0eac" #this id doesn't even show up in AD
-pre_mi_principal_id        = "d03f73e6-40ed-40a2-a0ec-059286505905" #they're all declaring pre-demo-mi but doesn't look like it's being called anywhere
-pre_mi_tenant_id           = "531ff96d-0ae9-462a-8d2d-bec7c0b42082" #likely useless
-dts_pre_app_admin          = "d055ba21-5814-4278-8752-aaffa7eaac62" #can't see this called anywhere
+# dts_pre_project_admin      = "56a29187-3d5f-4262-99d6-c635776e0eac" #this id doesn't even show up in AD
+#pre_mi_principal_id        = "d03f73e6-40ed-40a2-a0ec-059286505905" #they're all declaring pre-demo-mi but doesn't look like it's being called anywhere
+#pre_mi_tenant_id           = "531ff96d-0ae9-462a-8d2d-bec7c0b42082" #likely useless
+#dts_pre_app_admin          = "d055ba21-5814-4278-8752-aaffa7eaac62" #can't see this called anywhere
 devops_admin               = "a0c6507c-299c-4f46-96c6-8275d2c45242" #should be removed
 dts_pre_appreg_oid         = "b521e9e3-58e1-4c25-98b5-7f3157f68c16"
 dts_pre_ent_appreg_oid     = "9168b884-7ccd-4e71-860f-7f63455818e1"
 dts_pre_oid                = "b1fd4154-355f-4683-a795-d09cdb814d16"
-mgmt_subscription_id       = "6c4d2513-a873-41b4-afdd-b05a33206631"
+# mgmt_subscription_id       = "6c4d2513-a873-41b4-afdd-b05a33206631"
 pg_databases = [
   {
     name : "pre-pdb-dev"
--- sa.tf
+++ sa.tf
@@ -5,39 +5,19 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  mgmt_network_name    = var.mgmt_net_name
-  mgmt_network_rg_name = var.mgmt_net_rg_name
-}
-
-data "azurerm_subnet" "jenkins_subnet" {
-  provider             = azurerm.mgmt
-  name                 = "iaas"
-  virtual_network_name = local.mgmt_network_name
-  resource_group_name  = local.mgmt_network_rg_name
-}
-
-# adding this to allow postgres pipeline agent located on ss-ptlsbox-vnet to access keyvault
-data "azurerm_subnet" "pipelineagent_subnet" {
-  provider             = azurerm.mgmt
-  name                 = "aks-00"
-  virtual_network_name = local.mgmt_network_name
-  resource_group_name  = local.mgmt_network_rg_name
-}
-
 ###################################################
 #                 STORAGES               #
 ###################################################
 module "sa_storage_account" {
   source                          = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
   env                             = var.env
-  storage_account_name            = replace("${var.product}sa${var.env}", "-", "")
+  storage_account_name            = "${var.product}sa${var.env}"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
   account_kind                    = "StorageV2"
   account_tier                    = var.sa_account_tier
   account_replication_type        = var.sa_replication_type
-  sa_subnets                      = concat([data.azurerm_subnet.jenkins_subnet.id], [azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
+  sa_subnets                      = concat([azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
   allow_nested_items_to_be_public = false
   ip_rules                        = var.ip_rules
   default_action                  = "Deny"
@@ -50,13 +30,13 @@ module "sa_storage_account" {
 module "finalsa_storage_account" {
   source                          = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
   env                             = var.env
-  storage_account_name            = replace("${var.product}finalsa${var.env}", "-", "")
+  storage_account_name            = "${var.product}finalsa${var.env}"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = var.location #"UKWest" #As recommended by MS
   account_kind                    = "StorageV2"
   account_tier                    = var.sa_account_tier
   account_replication_type        = var.sa_replication_type
-  sa_subnets                      = concat([data.azurerm_subnet.jenkins_subnet.id], [azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
+  sa_subnets                      = concat([azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
   allow_nested_items_to_be_public = false
   ip_rules                        = var.ip_rules
   default_action                  = "Deny"
@@ -71,13 +51,13 @@ module "finalsa_storage_account" {
 module "ingestsa_storage_account" {
   source                          = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
   env                             = var.env
-  storage_account_name            = replace("${var.product}ingestsa${var.env}", "-", "")
+  storage_account_name            = "${var.product}ingestsa${var.env}"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = var.location #"UKWest" #As recommended by MS azurerm_resource_group.rg.location
   account_kind                    = "StorageV2"
   account_tier                    = var.sa_account_tier
   account_replication_type        = var.sa_replication_type
-  sa_subnets                      = concat([data.azurerm_subnet.jenkins_subnet.id], [azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
+  sa_subnets                      = concat([azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
   allow_nested_items_to_be_public = false
   ip_rules                        = var.ip_rules
   default_action                  = "Deny"
