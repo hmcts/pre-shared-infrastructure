68c36f5c9dac8502d72df9d42f4ac49132597ff0
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-10 11:09:24 +0000

    object id

--- components/pre-app-infra/postgres.tf
+++ components/pre-app-infra/postgres.tf
@@ -24,6 +24,8 @@ module "data_store_db_v14" {
   pgsql_sku            = var.pgsql_sku
   pgsql_storage_mb     = var.pgsql_storage_mb
 
+  admin_user_object_id = data.azurerm_client_config.current.object_id
+
 }
 
 ////////////////////////////////
