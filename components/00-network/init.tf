86d0cc6ef884e3d29d07ec1059452bdc67e002ab
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-13 22:06:35 +0000

    mm

--- components/00-network/init.tf
+++ components/00-network/init.tf
@@ -15,3 +15,9 @@ provider "azurerm" {
   features {}
   alias = "hub"
 }
+
+provider "azurerm" {
+  alias           = "private_dns"
+  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  features {}
+}
