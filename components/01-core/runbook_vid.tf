bace1a13957a981f444da0afbf1907e458b5ebbc
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-13 23:47:19 +0000

    locals

--- components/01-core/runbook_vid.tf
+++ components/01-core/runbook_vid.tf
@@ -1,7 +1,7 @@
 resource "azurerm_automation_account" "pre-aa" {
   name                = "${var.prefix}-${var.env}-aa"
   location            = var.location
-  resource_group_name = data.azurerm_resource_group.rg.id
+  resource_group_name = local.resource_group_name
   sku_name            = "Basic"
 
   identity {
