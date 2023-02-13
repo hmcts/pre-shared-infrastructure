1a948236d0ad605245ebef8a91316edbe4dc8e9b
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-13 10:07:06 +0000

    try

--- components/pre-app-infra/variables.tf
+++ components/pre-app-infra/variables.tf
@@ -49,8 +49,6 @@ variable "datagateway_spec" {
   default = "Standard_F8s_v2"
 }
 
-# variable "mgmt_subscription_id" {} // set by jenkins library
-
 variable "power_app_user_oid" {
   default = "56a29187-3d5f-4262-99d6-c635776e0eac"
 }
