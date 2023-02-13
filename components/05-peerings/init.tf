683c6b6a97558eb073e4056e524a2a4687e40598
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-13 18:17:22 +0000

    locals

--- components/05-peerings/init.tf
+++ components/05-peerings/init.tf
@@ -5,11 +5,15 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "= 3.33.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.2.0"
+    }
   }
 }
 
 provider "azurerm" {
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  #subscription_id            = local.hub[var.env].subscription
   subscription_id            = local.hub[local.hub_name].subscription
   skip_provider_registration = "true"
   features {}
