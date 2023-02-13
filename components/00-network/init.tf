0f0575e27f34b0e3945d33d069d89c54e15856cb
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-13 22:32:32 +0000

    features

--- components/00-network/init.tf
+++ components/00-network/init.tf
@@ -1,12 +1,13 @@
 terraform {
   backend "azurerm" {}
-  features {
-    use_providers = true
-  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "= 3.33.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.6"
     }
   }
 }
