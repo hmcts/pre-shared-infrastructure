45243f8c682ebb1df10ef668640361e2cf6f6940
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-14 19:12:36 +0000

    data

--- components/01-hub-peering/main.tf
+++ components/01-hub-peering/main.tf
@@ -13,7 +13,7 @@ data "azurerm_virtual_network" "vnet" {
 resource "azurerm_virtual_network_peering" "to_hub" {
   name                         = "hub"
   resource_group_name          = local.resource_group_name
-  virtual_network_name         = azurerm_virtual_network.vnet.name
+  virtual_network_name         = data.azurerm_virtual_network.vnet.name
   remote_virtual_network_id    = data.azurerm_virtual_network.hub.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
@@ -26,7 +26,7 @@ resource "azurerm_virtual_network_peering" "from_hub" {
   name                         = var.PeeringFromHubName
   resource_group_name          = local.hub[local.hub_name].ukSouth.name
   virtual_network_name         = local.hub[local.hub_name].ukSouth.name
-  remote_virtual_network_id    = azurerm_virtual_network.vnet.id
+  remote_virtual_network_id    = data.azurerm_virtual_network.vnet.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
 }
