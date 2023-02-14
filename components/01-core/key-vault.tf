a6f35db21d6aaba1ea68eece73beba2db024313d
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-14 00:05:15 +0000

    name

--- components/01-core/key-vault.tf
+++ components/01-core/key-vault.tf
@@ -112,7 +112,7 @@ resource "azurerm_key_vault_key" "pre_kv_key" {
 
 resource "azurerm_disk_encryption_set" "pre-des" {
   name                = "pre-des-${var.env}"
-  resource_group_name = data.azurerm_resource_group.rg.id
+  resource_group_name = data.azurerm_resource_group.rg.name
   location            = var.location
   key_vault_key_id    = azurerm_key_vault_key.pre_kv_key.id
   identity {
