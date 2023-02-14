c75f0c8191f1f7b3b43249837f60bee6be15dda8
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-14 19:23:45 +0000

    tags

--- components/01-hub-peering/variables.tf
+++ components/01-hub-peering/variables.tf
@@ -7,3 +7,8 @@ variable "env" {}
 variable "PeeringFromHubName" {
   default = "pre-recorded-evidence"
 }
+
+variable "builtFrom" {
+  description = "The GitHub URL for the repository that contains the infrastructure code."
+  default     = "hmcts/pre-shared-infrastructure"
+}
