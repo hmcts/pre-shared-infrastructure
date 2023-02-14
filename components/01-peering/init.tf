8b934746577b50374ad0a151f6257e89c7593ef2
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-14 11:32:47 +0000

    separate out peering stage

---
+++ components/01-peering/init.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 3.33.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id            = local.hub[local.hub_name].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub"
+}
+
+provider "azurerm" {
+  alias           = "private_dns"
+  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  features {}
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+provider "time" {
+  features {}
+}
