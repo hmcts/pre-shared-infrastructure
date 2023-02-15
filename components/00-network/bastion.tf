064fbe2210894db28ba36577495cf53ce7bc3473
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-15 18:05:44 +0000

    bits

--- components/00-network/bastion.tf
+++ components/00-network/bastion.tf
@@ -40,3 +40,66 @@ resource "null_resource" "Encryption" {
 	  EOF
   }
 }
+
+resource "azurerm_monitor_diagnostic_setting" "bastion" {
+  name                       = azurerm_bastion_host.bastion.name
+  target_resource_id         = azurerm_bastion_host.bastion.id
+  log_analytics_workspace_id = module.log_analytics_workspace.workspace_id
+
+  log {
+    category = "BastionAuditLogs"
+
+    retention_policy {
+      enabled = true
+      days    = 14
+    }
+  }
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = true
+      days    = 14
+    }
+  }
+}
+
+#logs
+resource "azurerm_monitor_diagnostic_setting" "bastionpip" {
+  name                       = azurerm_public_ip.pip.name
+  target_resource_id         = azurerm_public_ip.pip.id
+  log_analytics_workspace_id = module.log_analytics_workspace.workspace_id
+
+  log {
+    category = "DDoSProtectionNotifications"
+
+    retention_policy {
+      enabled = true
+      days    = 14
+    }
+  }
+  log {
+    category = "DDoSMitigationFlowLogs"
+
+    retention_policy {
+      enabled = true
+      days    = 14
+    }
+  }
+  log {
+    category = "DDoSMitigationReports"
+
+    retention_policy {
+      enabled = true
+      days    = 14
+    }
+  }
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = true
+      days    = 14
+    }
+  }
+}
