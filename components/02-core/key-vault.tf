19498090930eb9e56f8b08e1bca8a8a5981bdc84
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-15 17:48:16 +0000

    remove dependson

--- components/02-core/key-vault.tf
+++ components/02-core/key-vault.tf
@@ -69,23 +69,17 @@ module "key-vault" {
 data "azurerm_key_vault" "keyvault" {
   name                = var.env == "prod" ? "${var.prefix}-hmctskv-${var.env}" : "${var.prefix}-${var.env}" #module.key-vault.key_vault_name
   resource_group_name = data.azurerm_resource_group.rg.name
-
-  depends_on = [module.key-vault]
 }
 
 # Dynatrace
 data "azurerm_key_vault_secret" "dynatrace-token" {
   name         = "dynatrace-token"
   key_vault_id = module.key-vault.key_vault_id
-
-  depends_on = [module.key-vault]
 }
 
 data "azurerm_key_vault_secret" "dynatrace-tenant-id" {
   name         = "dynatrace-tenant-id"
   key_vault_id = module.key-vault.key_vault_id
-
-  depends_on = [module.key-vault]
 }
 
 # Disk Encryption 
