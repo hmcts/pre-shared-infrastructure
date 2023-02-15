561595d9501e9d2fdce85d8fa8440bee1e7a008c
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-15 10:22:22 +0000

    save

--- components/01-hub-peering/variables.tf
+++ components/01-hub-peering/variables.tf
@@ -12,10 +12,10 @@ variable "location" {
   default = "UK South"
 }
 
-# variable "builtFrom" {
-#   description = "The GitHub URL for the repository that contains the infrastructure code."
-#   default     = "hmcts/pre-shared-infrastructure"
-# }
+variable "builtFrom" {
+  description = "The GitHub URL for the repository that contains the infrastructure code."
+  default     = "hmcts/pre-shared-infrastructure"
+}
 
 # variable "PeeringFromHubName" {
 #   default = "pre-recorded-evidence"
