f5dfdd285db5da00529ad4919c25d2e52e93e343
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-16 11:42:36 +0000

    var?

--- components/03-postgres/variables.tf
+++ components/03-postgres/variables.tf
@@ -36,7 +36,9 @@ variable "pgsql_storage_mb" {
   default = "32768"
 }
 
-# variable "dts_pre_ent_appreg_oid" {}
+variable "dts_pre_ent_appreg_oid" {
+  default = "9168b884-7ccd-4e71-860f-7f63455818e1"
+}
 
 variable "builtFrom" {
   description = "The GitHub URL for the repository that contains the infrastructure code."
