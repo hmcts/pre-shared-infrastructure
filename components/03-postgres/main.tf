dab789ef7c7418baee599b95efd28181376948ea
Zoe Cope <zoecope@SoliriusAdmins-Laptop.local>
2023-02-16 19:53:44 +0000

    plain

--- components/03-postgres/main.tf
+++ components/03-postgres/main.tf
@@ -41,7 +41,7 @@ module "data_store_db_v14" {
   pgsql_sku            = var.pgsql_sku
   pgsql_storage_mb     = var.pgsql_storage_mb
 
-  admin_user_object_id = toString(data.azurerm_client_config.current.object_id)
+  admin_user_object_id = data.azurerm_client_config.current.object_id
 }
 
 data "azurerm_key_vault" "keyvault" {
