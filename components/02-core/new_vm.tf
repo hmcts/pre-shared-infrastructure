fa7cbd3b2f0586165d33b38ff1ad20079043466c
Zoe Cope <zoe.cope@hmcts.net>
2023-02-22 15:41:43 +0000

    try

--- components/02-core/new_vm.tf
+++ components/02-core/new_vm.tf
@@ -7,7 +7,7 @@ module "data_gateway_vm" {
   vm_location          = var.location
   vm_size              = local.vm_size
   vm_admin_name        = "data_gateway${count.index}_${random_string.dtgtwy_username[count.index].result}"
-  vm_admin_password    = random_password.vm_password[count.index].result
+  vm_admin_password    = "Wednesday123" # random_password.vm_password[count.index].result
   vm_availabilty_zones = local.vm_availabilty_zones[count.index]
   managed_disks        = var.vm_data_disks[count.index]
 
