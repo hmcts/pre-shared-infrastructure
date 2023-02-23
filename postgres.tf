8a0ace18ebc740796853494206acd4f46aed53ff
Zoe Cope <zoe.cope@hmcts.net>
2023-02-23 17:03:52 +0000

    pg

--- postgres.tf
+++ postgres.tf
@@ -4,7 +4,7 @@
 ////////////////////////////////
 
 module "data_store_db_v14" {
-  source = "git@github.com:hmcts/terraform-module-postgresql-flexible.git?ref=db-collation"
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible.git?ref=master"
   env    = var.env
 
   product       = var.product
