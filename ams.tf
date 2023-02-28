182e0a97643d075b4921093af0d97c802abfe7a6
Zoe Cope <zoe.cope@hmcts.net>
2023-02-28 14:24:40 +0000

    Upgrade AMS

--- ams.tf
+++ ams.tf
@@ -4,10 +4,9 @@ resource "azurerm_media_services_account" "ams" {
   resource_group_name = azurerm_resource_group.rg.name
 
   identity {
-    type = "SystemAssigned"
+    type         = "UserAssigned"
+    identity_ids = [data.azurerm_user_assigned_identity.managed-identity.id]
   }
-
-
 
   storage_account {
     id         = module.ingestsa_storage_account.storageaccount_id
@@ -22,6 +21,31 @@ resource "azurerm_media_services_account" "ams" {
   tags = var.common_tags
 
 }
+
+# resource "azurerm_media_services_account" "ams" {
+#   name                = "${var.product}ams${var.env}"
+#   location            = var.location #"UKwest"
+#   resource_group_name = azurerm_resource_group.rg.name
+
+#   identity {
+#     type = "SystemAssigned"
+#   }
+
+
+
+#   storage_account {
+#     id         = module.ingestsa_storage_account.storageaccount_id
+#     is_primary = true
+#   }
+
+#   storage_account {
+#     id         = module.finalsa_storage_account.storageaccount_id
+#     is_primary = false
+#   }
+
+#   tags = var.common_tags
+
+# }
 resource "azurerm_media_transform" "analysevideo" {
   name                        = "AnalyseVideo"
   resource_group_name         = azurerm_resource_group.rg.name
@@ -56,44 +80,44 @@ resource "azurerm_media_transform" "EncodeToMP" {
   }
 }
 
-resource "null_resource" "amsid" {
-  triggers = {
-    always_run = "${timestamp()}"
-  }
+# resource "null_resource" "amsid" {
+#   triggers = {
+#     always_run = "${timestamp()}"
+#   }
 
-  depends_on = [azurerm_media_services_account.ams]
-
-
-}
+#   depends_on = [azurerm_media_services_account.ams]
 
 
-resource "azapi_update_resource" "ams_auth" {
-  depends_on  = [null_resource.amsid]
-  type        = "Microsoft.Media/mediaservices@2021-11-01"
-  resource_id = azurerm_media_services_account.ams.id
+# }
 
-  body = jsonencode({
-    properties = {
-      storageAuthentication = "ManagedIdentity"
-      storageAccounts = [
-        {
-          id   = module.ingestsa_storage_account.storageaccount_id
-          type = "Primary",
-          identity = {
-            userAssignedIdentity      = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
-            useSystemAssignedIdentity = "false"
-          }
-        },
 
-        {
-          id   = module.finalsa_storage_account.storageaccount_id
-          type = "Secondary",
-          identity = {
-            userAssignedIdentity      = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
-            useSystemAssignedIdentity = "false"
-          }
-        }
-      ]
-    }
-  })
-}
+# resource "azapi_update_resource" "ams_auth" {
+#   depends_on  = [null_resource.amsid]
+#   type        = "Microsoft.Media/mediaservices@2021-11-01"
+#   resource_id = azurerm_media_services_account.ams.id
+
+#   body = jsonencode({
+#     properties = {
+#       storageAuthentication = "ManagedIdentity"
+#       storageAccounts = [
+#         {
+#           id   = module.ingestsa_storage_account.storageaccount_id
+#           type = "Primary",
+#           identity = {
+#             userAssignedIdentity      = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
+#             useSystemAssignedIdentity = "false"
+#           }
+#         },
+
+#         {
+#           id   = module.finalsa_storage_account.storageaccount_id
+#           type = "Secondary",
+#           identity = {
+#             userAssignedIdentity      = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pre-${var.env}-mi"
+#             useSystemAssignedIdentity = "false"
+#           }
+#         }
+#       ]
+#     }
+#   })
+# }
