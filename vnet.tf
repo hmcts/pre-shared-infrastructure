30636c4bf8cb594bc2d9ba3084beb83d7b45984a
Zoe Cope <zoe.cope@hmcts.net>
2023-03-01 11:48:41 +0000

    moe

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.product}-vnet01-${var.env}"
+  name                = "${var.product}-vnet-${var.env}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = [var.vnet_address_space]
