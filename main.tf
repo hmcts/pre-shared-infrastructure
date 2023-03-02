1e4ea0e18ea98c2f91e09b5bf2ce38a3942e051c
Zoe Cope <zoe.cope@hmcts.net>
2023-03-02 11:30:00 +0000

    refactor sa's

--- main.tf
+++ main.tf
@@ -45,13 +45,6 @@ resource "azurerm_role_assignment" "vmuser_login" {
   principal_id         = each.value
 }
 
-resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
-  scope                            = azurerm_resource_group.rg.id
-  role_definition_name             = "Storage Blob Data Contributor"
-  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id #var.pre_mi_principal_id
-  skip_service_principal_aad_check = true
-}
-
 resource "azurerm_automation_account" "pre-aa" {
   name                = "${var.product}-${var.env}-aa"
   location            = var.location
