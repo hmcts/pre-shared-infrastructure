6e1beba7a4f62388bc2adb06c6ac8630302e2708
Zoe Cope <zoe.cope@hmcts.net>
2023-03-07 16:07:53 +0000

    shorter name vm

--- provider.tf
+++ provider.tf
@@ -44,3 +44,10 @@ provider "azurerm" {
   skip_provider_registration = true
   features {}
 }
+
+provider "azurerm" {
+  alias           = "private_dns"
+  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  features {}
+
+}
--- vm-datagateway.tf
+++ vm-datagateway.tf
@@ -21,7 +21,7 @@ resource "azurerm_windows_virtual_machine" "dtgtwyvm" {
   resource_group_name        = data.azurerm_resource_group.rg.name
   location                   = data.azurerm_resource_group.rg.location
   size                       = var.datagateway_spec
-  admin_username             = "Dtgtwy${count.index}_${data.azurerm_key_vault_secret.dtgtwy_username[count.index].value}"
+  admin_username             = "DG${count.index}_${data.azurerm_key_vault_secret.dtgtwy_username[count.index].value}"
   admin_password             = data.azurerm_key_vault_secret.dtgtwy_password[count.index].value
   network_interface_ids      = [azurerm_network_interface.dtgwnic[count.index].id]
   encryption_at_host_enabled = true
@@ -68,34 +68,6 @@ resource "azurerm_virtual_machine_data_disk_attachment" "dtgtwy" {
   virtual_machine_id = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
   lun                = "3"
   caching            = "ReadWrite"
-}
-
-resource "azurerm_virtual_machine_extension" "dtgtwayvmextension" {
-  name                       = "IaaSAntimalware"
-  count                      = var.num_datagateway
-  virtual_machine_id         = azurerm_windows_virtual_machine.dtgtwyvm.*.id[count.index]
-  publisher                  = "Microsoft.Azure.Security"
-  type                       = "IaaSAntimalware"
-  type_handler_version       = "1.3"
-  auto_upgrade_minor_version = true
-  settings                   = <<SETTINGS
-    {
-    "AntimalwareEnabled": true,
-    "RealtimeProtectionEnabled": "true",
-    "ScheduledScanSettings": {
-    "isEnabled": "true",
-    "day": "1",
-    "time": "120",
-    "scanType": "Quick"
-    },
-    "Exclusions": {
-    "Extensions": "",
-    "Paths": "",
-    "Processes": ""
-    }
-    }
-SETTINGS
-  tags                       = var.common_tags
 }
 
 resource "azurerm_virtual_machine_extension" "dtgtwydaa-agent" {
