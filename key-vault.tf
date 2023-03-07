9eebee705aed73543660fec736f7f204d13a2548
Zoe Cope <zoe.cope@hmcts.net>
2023-03-07 10:24:42 +0000

    naming

--- key-vault.tf
+++ key-vault.tf
@@ -46,14 +46,14 @@ resource "random_password" "vm_password" {
   min_numeric      = 1
 }
 
-resource "azurerm_key_vault_secret" "vm_username_secret" {
+resource "azurerm_key_vault_secret" "vm_username" {
   count        = var.num_vid_edit_vms
   name         = "videditvm${count.index}-username"
   value        = "videdit${count.index}_${random_string.vm_username[count.index].result}"
   key_vault_id = module.key-vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "vm_password_secret" {
+resource "azurerm_key_vault_secret" "vm_password" {
   count        = var.num_vid_edit_vms
   name         = "videditvm${count.index}-password"
   value        = random_password.vm_password[count.index].result
@@ -77,14 +77,14 @@ resource "random_password" "dtgtwy_password" {
   min_numeric      = 1
 }
 
-resource "azurerm_key_vault_secret" "dtgtwy_username_secret" {
+resource "azurerm_key_vault_secret" "dtgtwy_username" {
   count        = var.num_datagateway
   name         = "Dtgtwy${count.index}-username"
   value        = "Dtgtwy${count.index}_${random_string.dtgtwy_username[count.index].result}"
   key_vault_id = module.key-vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "dtgtwy_password_secret" {
+resource "azurerm_key_vault_secret" "dtgtwy_password" {
   count        = var.num_datagateway
   name         = "Dtgtwy${count.index}-password"
   value        = random_password.dtgtwy_password[count.index].result
