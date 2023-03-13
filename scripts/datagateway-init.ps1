66d93ff8411fd0ec8e16079faa4f5abb1856b20d
Zoe Cope <zoe.cope@hmcts.net>
2023-03-13 22:13:53 +0000

    try null resource

--- scripts/datagateway-init.ps1
+++ scripts/datagateway-init.ps1
@@ -1,3 +1,9 @@
+#.NET 6
+Invoke-WebRequest -Uri https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.ps1 -OutFile dotnet-install.ps1
+.\dotnet-install.ps1 -Channel STS
+
+
+#Data Gateway
 $url = "https://go.microsoft.com/fwlink/?LinkId=2116849&clcid=0x409"
 $output = "$env:USERPROFILE\Downloads\MicrosoftDataGatewayInstaller.exe"
 
