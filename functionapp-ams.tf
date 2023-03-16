1fc10f2b105547ca9bebb538cf2be12fc5d46f22
Zoe Cope <zoe.cope@hmcts.net>
2023-03-16 16:08:04 +0000

    add windows fa

---
+++ functionapp-ams.tf
@@ -0,0 +1,355 @@
+
+module "ams_function_app" {
+  #    source  =  "git@github.com:hmcts/pre-module-terraform-function-app.git?ref=master"
+  #   source = "./modules"
+  source              = "git@github.com:hmcts/pre-shared-infrastructure//modules?ref=preview"
+  os_type             = "Linux"
+  product             = var.product
+  create_service_plan = true
+
+  resource_group_name = data.azurerm_resource_group.rg.name
+  name                = "pre-ams-integration"
+  location            = var.location
+  worker_count        = 1
+  common_tags         = var.common_tags
+  app_settings = {
+    "ACCOUNTKEY" = ""
+    "ALGO"       = "['RS256']"
+    #"APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.example.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = ""
+    "AZURE_CLIENT_ID"                       = data.azurerm_client_config.current.client_id
+    "AZURE_MEDIA_SERVICES_ACCOUNT_NAME"     = "preams${var.env}"
+    "AZURE_STORAGE_ACCOUNT_NAME"            = "prefinalsa${var.env}"
+    "AZURE_TENANT_ID"                       = data.azurerm_client_config.current.tenant_id
+    "AzureWebJobsStorage"                   = ""
+    "DRMSYMMETRICKEY"                       = ""
+    "FUNCTIONS_EXTENSION_VERSION"           = ""
+    "FUNCTIONS_WORKER_RUNTIME"              = ""
+    "ISSUER"                                = ""
+    "JWKSURI"                               = ""
+    "SCOPE"                                 = ""
+    "TOKENENDPOINT"                         = ""
+    "TOKENSIGNINGKEY"                       = ""
+    # "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = ""
+    # "WEBSITE_CONTENTSHARE"     = ""
+    # "WEBSITE_RUN_FROM_PACKAGE" = ""
+  }
+
+  storage_account_name = module.ingestsa_storage_account.storageaccount_name
+  storage_account_key  = module.ingestsa_storage_account.storageaccount_primary_access_key
+}
+
+resource "azurerm_function_app_function" "content_key_policy" {
+  name            = "CreateContentKeyPolicy"
+  function_app_id = module.ams_function_app.function_app_id
+  #   language        = "Javascript"
+
+  #   file {
+  #     name    = "run.js"
+  #     content = file("exampledata/run.js")
+  #   }
+
+
+  #   test_data = jsonencode({
+  #     "name" = "Azure"
+  #   })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+resource "azurerm_function_app_function" "sas_url" {
+  name            = "getSasUrl"
+  function_app_id = module.ams_function_app.function_app_id
+  #   language        = "Javascript"
+
+  #   file {
+  #     name    = "run.js"
+  #     content = file("exampledata/run.js")
+  #   }
+
+
+  #   test_data = jsonencode({
+  #     "name" = "Azure"
+  #   })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+resource "azurerm_function_app_function" "streaming_url" {
+  name            = "GetStreamingUrl"
+  function_app_id = module.ams_function_app.function_app_id
+  #   language        = "Javascript"
+
+  #   file {
+  #     name    = "run.js"
+  #     content = file("exampledata/run.js")
+  #   }
+
+
+  #   test_data = jsonencode({
+  #     "name" = "Azure"
+  #   })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+resource "azurerm_function_app_function" "token" {
+  name            = "getToken"
+  function_app_id = module.ams_function_app.function_app_id
+  #   language        = "Javascript"
+  #   test_data = jsonencode({
+  #     "name" = "Azure"
+  #   })
+
+  #   file {
+  #     name    = "run.js"
+  #     content = file("exampledata/run.js")
+  #   }
+
+
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+resource "azurerm_function_app_function" "list_container_assets" {
+  name            = "listContainerAssets"
+  function_app_id = module.ams_function_app.function_app_id
+  #   language        = "Javascript"
+
+  #   file {
+  #     name    = "run.js"
+  #     content = file("exampledata/run.js")
+  #   }
+
+  #   test_data = jsonencode({
+  #     "name" = "Azure"
+  #   })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+resource "azurerm_function_app_function" "list_containers" {
+  name            = "listContainers"
+  function_app_id = module.ams_function_app.function_app_id
+  #   language        = "Javascript"
+
+  #   file {
+  #     name    = "run.js"
+  #     content = file("exampledata/run.js")
+  #   }
+
+  #   test_data = jsonencode({
+  #     "name" = "Azure"
+  #   })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+resource "azurerm_function_app_function" "play_ready_content" {
+  name            = "PlayReadyContent"
+  function_app_id = module.ams_function_app.function_app_id
+  #   language        = "Javascript"
+
+  #   file {
+  #     name    = "run.js"
+  #     content = file("exampledata/run.js")
+  #   }
+
+  #   test_data = jsonencode({
+  #     "name" = "Azure"
+  #   })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+resource "azurerm_function_app_function" "verify_token" {
+  name            = "verifyToken"
+  function_app_id = module.ams_function_app.function_app_id
+  #   language        = "Javascript"
+
+  #   file {
+  #     name    = "run.js"
+  #     content = file("exampledata/run.js")
+  #   }
+
+
+  #   test_data = jsonencode({
+  #     "name" = "Azure"
+  #   })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+
+module "video_test" {
+  #    source  =  "git@github.com:hmcts/pre-module-terraform-function-app.git?ref=master"
+  #   source = "./modules"
+  source              = "git@github.com:hmcts/pre-shared-infrastructure//modules?ref=preview"
+  os_type             = "Windows"
+  product             = var.product
+  create_service_plan = true
+
+  resource_group_name = data.azurerm_resource_group.rg.name
+  name                = "pre-rec-video-test"
+  location            = var.location
+  worker_count        = 1
+  common_tags         = var.common_tags
+  app_settings = {
+    "ACCOUNTKEY" = ""
+    "ALGO"       = "['RS256']"
+    #"APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.example.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = ""
+    "AZURE_CLIENT_ID"                       = data.azurerm_client_config.current.client_id
+    "AZURE_MEDIA_SERVICES_ACCOUNT_NAME"     = "preams${var.env}"
+    "AZURE_STORAGE_ACCOUNT_NAME"            = "prefinalsa${var.env}"
+    "AZURE_TENANT_ID"                       = data.azurerm_client_config.current.tenant_id
+    "AzureWebJobsStorage"                   = ""
+    "FUNCTIONS_EXTENSION_VERSION"           = ""
+    "FUNCTIONS_WORKER_RUNTIME"              = ""
+    "AppSecret"                             = ""
+    "ApplicationId"                         = ""
+
+    # "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = ""
+    # "WEBSITE_CONTENTSHARE"     = ""
+    # "WEBSITE_RUN_FROM_PACKAGE" = ""
+  }
+
+  storage_account_name = module.ingestsa_storage_account.storageaccount_name
+  storage_account_key  = module.ingestsa_storage_account.storageaccount_primary_access_key
+}
--- functionapp-video.tf
+++ functionapp-video.tf
@@ -1,14 +1,15 @@
 
-module "ams_function_app" {
+
+module "video_test" {
   #    source  =  "git@github.com:hmcts/pre-module-terraform-function-app.git?ref=master"
   #   source = "./modules"
   source              = "git@github.com:hmcts/pre-shared-infrastructure//modules?ref=preview"
-  os_type             = "Linux"
+  os_type             = "Windows"
   product             = var.product
   create_service_plan = true
 
   resource_group_name = data.azurerm_resource_group.rg.name
-  name                = "pre-ams-integration"
+  name                = "pre-rec-video-test"
   location            = var.location
   worker_count        = 1
   common_tags         = var.common_tags
@@ -22,14 +23,11 @@ module "ams_function_app" {
     "AZURE_STORAGE_ACCOUNT_NAME"            = "prefinalsa${var.env}"
     "AZURE_TENANT_ID"                       = data.azurerm_client_config.current.tenant_id
     "AzureWebJobsStorage"                   = ""
-    "DRMSYMMETRICKEY"                       = ""
     "FUNCTIONS_EXTENSION_VERSION"           = ""
     "FUNCTIONS_WORKER_RUNTIME"              = ""
-    "ISSUER"                                = ""
-    "JWKSURI"                               = ""
-    "SCOPE"                                 = ""
-    "TOKENENDPOINT"                         = ""
-    "TOKENSIGNINGKEY"                       = ""
+    "AppSecret"                             = ""
+    "ApplicationId"                         = ""
+
     # "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = ""
     # "WEBSITE_CONTENTSHARE"     = ""
     # "WEBSITE_RUN_FROM_PACKAGE" = ""
@@ -38,124 +36,10 @@ module "ams_function_app" {
   storage_account_name = module.ingestsa_storage_account.storageaccount_name
   storage_account_key  = module.ingestsa_storage_account.storageaccount_primary_access_key
 }
-resource "azurerm_function_app_function" "content_key_policy" {
-  name            = "CreateContentKeyPolicy"
+
+resource "azurerm_function_app_function" "api" {
+  name            = "API"
   function_app_id = module.ams_function_app.function_app_id
-  #   language        = "Javascript"
-
-  #   file {
-  #     name    = "run.js"
-  #     content = file("exampledata/run.js")
-  #   }
-
-
-  #   test_data = jsonencode({
-  #     "name" = "Azure"
-  #   })
-  config_json = jsonencode({
-    "bindings" = [
-      {
-        "authLevel" = "function"
-        "direction" = "in"
-        "methods" = [
-          "get",
-          "post",
-        ]
-        "name" = "req"
-        "type" = "httpTrigger"
-      },
-      {
-        "direction" = "out"
-        "name"      = "$return"
-        "type"      = "http"
-      },
-    ]
-  })
-}
-
-resource "azurerm_function_app_function" "sas_url" {
-  name            = "getSasUrl"
-  function_app_id = module.ams_function_app.function_app_id
-  #   language        = "Javascript"
-
-  #   file {
-  #     name    = "run.js"
-  #     content = file("exampledata/run.js")
-  #   }
-
-
-  #   test_data = jsonencode({
-  #     "name" = "Azure"
-  #   })
-  config_json = jsonencode({
-    "bindings" = [
-      {
-        "authLevel" = "function"
-        "direction" = "in"
-        "methods" = [
-          "get",
-          "post",
-        ]
-        "name" = "req"
-        "type" = "httpTrigger"
-      },
-      {
-        "direction" = "out"
-        "name"      = "$return"
-        "type"      = "http"
-      },
-    ]
-  })
-}
-
-resource "azurerm_function_app_function" "streaming_url" {
-  name            = "GetStreamingUrl"
-  function_app_id = module.ams_function_app.function_app_id
-  #   language        = "Javascript"
-
-  #   file {
-  #     name    = "run.js"
-  #     content = file("exampledata/run.js")
-  #   }
-
-
-  #   test_data = jsonencode({
-  #     "name" = "Azure"
-  #   })
-  config_json = jsonencode({
-    "bindings" = [
-      {
-        "authLevel" = "function"
-        "direction" = "in"
-        "methods" = [
-          "get",
-          "post",
-        ]
-        "name" = "req"
-        "type" = "httpTrigger"
-      },
-      {
-        "direction" = "out"
-        "name"      = "$return"
-        "type"      = "http"
-      },
-    ]
-  })
-}
-
-resource "azurerm_function_app_function" "token" {
-  name            = "getToken"
-  function_app_id = module.ams_function_app.function_app_id
-  #   language        = "Javascript"
-  #   test_data = jsonencode({
-  #     "name" = "Azure"
-  #   })
-
-  #   file {
-  #     name    = "run.js"
-  #     content = file("exampledata/run.js")
-  #   }
-
 
   config_json = jsonencode({
     "bindings" = [
@@ -178,19 +62,10 @@ resource "azurerm_function_app_function" "token" {
   })
 }
 
-resource "azurerm_function_app_function" "list_container_assets" {
-  name            = "listContainerAssets"
+resource "azurerm_function_app_function" "DownloadFile" {
+  name            = "DownloadFile"
   function_app_id = module.ams_function_app.function_app_id
-  #   language        = "Javascript"
 
-  #   file {
-  #     name    = "run.js"
-  #     content = file("exampledata/run.js")
-  #   }
-
-  #   test_data = jsonencode({
-  #     "name" = "Azure"
-  #   })
   config_json = jsonencode({
     "bindings" = [
       {
@@ -212,19 +87,10 @@ resource "azurerm_function_app_function" "list_container_assets" {
   })
 }
 
-resource "azurerm_function_app_function" "list_containers" {
-  name            = "listContainers"
+resource "azurerm_function_app_function" "EditingOrchestrator" {
+  name            = "EditingOrchestrator"
   function_app_id = module.ams_function_app.function_app_id
-  #   language        = "Javascript"
 
-  #   file {
-  #     name    = "run.js"
-  #     content = file("exampledata/run.js")
-  #   }
-
-  #   test_data = jsonencode({
-  #     "name" = "Azure"
-  #   })
   config_json = jsonencode({
     "bindings" = [
       {
@@ -246,19 +112,11 @@ resource "azurerm_function_app_function" "list_containers" {
   })
 }
 
-resource "azurerm_function_app_function" "play_ready_content" {
-  name            = "PlayReadyContent"
+
+resource "azurerm_function_app_function" "EditVideo" {
+  name            = "EditVideo"
   function_app_id = module.ams_function_app.function_app_id
-  #   language        = "Javascript"
 
-  #   file {
-  #     name    = "run.js"
-  #     content = file("exampledata/run.js")
-  #   }
-
-  #   test_data = jsonencode({
-  #     "name" = "Azure"
-  #   })
   config_json = jsonencode({
     "bindings" = [
       {
@@ -280,20 +138,10 @@ resource "azurerm_function_app_function" "play_ready_content" {
   })
 }
 
-resource "azurerm_function_app_function" "verify_token" {
-  name            = "verifyToken"
+resource "azurerm_function_app_function" "HttpTriggerAMS" {
+  name            = "HttpTriggerAMS"
   function_app_id = module.ams_function_app.function_app_id
-  #   language        = "Javascript"
 
-  #   file {
-  #     name    = "run.js"
-  #     content = file("exampledata/run.js")
-  #   }
-
-
-  #   test_data = jsonencode({
-  #     "name" = "Azure"
-  #   })
   config_json = jsonencode({
     "bindings" = [
       {
@@ -315,3 +163,52 @@ resource "azurerm_function_app_function" "verify_token" {
   })
 }
 
+resource "azurerm_function_app_function" "HttpTriggerAMS2" {
+  name            = "HttpTriggerAMS2"
+  function_app_id = module.ams_function_app.function_app_id
+
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
+
+resource "azurerm_function_app_function" "UploadFile" {
+  name            = "UploadFile"
+  function_app_id = module.ams_function_app.function_app_id
+
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
