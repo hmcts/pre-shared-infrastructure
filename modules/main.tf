0f33a464f32ac5f342da4f5b84edd0b50ce28105
Zoe Cope <zoe.cope@hmcts.net>
2023-03-16 14:20:21 +0000

    count

--- modules/main.tf
+++ modules/main.tf
@@ -1,33 +1,35 @@
 resource "azurerm_service_plan" "this" {
-  name                   = "${var.product}-asp-${var.name}"
-  resource_group_name    = var.resource_group_name
-  location               = var.location
-  os_type                = var.os_type #"Windows"
-  sku_name               = "Y1"
-  worker_count           = var.worker_count
+  name                = "${var.product}-asp-${var.name}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = var.os_type #"Windows"
+  sku_name            = "Y1"
+  worker_count        = var.worker_count
   # zone_balancing_enabled = true
 
   tags = var.common_tags
 }
 
-# resource "azurerm_windows_function_app" "this" {
-#   name                = var.name
-#   resource_group_name = var.resource_group_name
-#   location            = var.location
+resource "azurerm_windows_function_app" "this" {
+  count               = var.os_type == "windows" ? 1 : 0
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
 
-#   storage_account_name       = var.storage_account_name
-#   storage_account_access_key = var.storage_account_key #azurerm_storage_account.this.primary_access_key
-#   service_plan_id            = azurerm_service_plan.this.id
+  storage_account_name       = var.storage_account_name
+  storage_account_access_key = var.storage_account_key #azurerm_storage_account.this.primary_access_key
+  service_plan_id            = azurerm_service_plan.this.id
 
-#   app_settings = var.app_settings
-#   https_only   = true
+  app_settings = var.app_settings
+  https_only   = true
 
-#   tags = var.common_tags
+  tags = var.common_tags
 
-#   site_config {}
-# }
+  site_config {}
+}
 
 resource "azurerm_linux_function_app" "this" {
+  count               = var.os_type == "linux" ? 1 : 0
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
