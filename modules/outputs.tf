7a53a46549d22d43e667b5c6f2e8ed883016773b
Zoe Cope <zoe.cope@hmcts.net>
2023-03-16 13:33:05 +0000

    now with tags

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,7 +1,7 @@
 output "function_app_id" {
-  value = lower(var.os_type) == "windows" ?   azurerm_windows_function_app.this.id : azurerm_linux_function_app.this.id
+  value = lower(var.os_type) == "windows" ? azurerm_windows_function_app.this.id : azurerm_linux_function_app.this.id
 }
 
 output "function_app_default_hostname" {
-  value = lower(var.os_type) == "windows" ?  azurerm_windows_function_app.this.default_hostname : azurerm_linux_function_app.this.default_hostname
+  value = lower(var.os_type) == "windows" ? azurerm_windows_function_app.this.default_hostname : azurerm_linux_function_app.this.default_hostname
 }
