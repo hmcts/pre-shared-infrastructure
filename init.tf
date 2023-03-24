6689cb5ec6b11905649de81a53d70f67f2381d3c
Zoe Cope <zoe.cope@hmcts.net>
2023-03-24 19:05:39 +0000

    update providers

--- init.tf
+++ init.tf
@@ -3,14 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.37.0"
+      version = "3.37.0"
     }
     random = {
       source  = "hashicorp/random"
       version = ">= 2.2.0"
-    }
-    azapi = {
-      source = "Azure/azapi"
     }
 
     time = {
@@ -20,8 +17,6 @@ terraform {
   }
 }
 
-provider "azapi" {
-}
 
 provider "azurerm" {
   skip_provider_registration = true
