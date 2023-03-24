132937fa7fe0a376bedd72ab054ca97d69c5d57f
Zoe Cope <zoe.cope@hmcts.net>
2023-03-24 10:29:20 +0000

    perms

--- main.tf
+++ main.tf
@@ -44,7 +44,11 @@ resource "azurerm_role_assignment" "admin_finalsa_contributor" {
   principal_id         = var.pre_app_admin
 }
 
-
+# resource "azurerm_role_assignment" "admin_ingestsa_contributor" {
+#   scope                = module.ingestsa_storage_account.storageaccount_id
+#   role_definition_name = "Storage Blob Data Contributor"
+#   principal_id         = var.pre_app_admin
+# }
 
 # DTS-PRE-VideoEditing-SecurityGroup-
 # resource "azurerm_role_assignment" "vmuser_login" {
