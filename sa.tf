daf915e3a2a2d1eb815ada5c8b8bee79082dbfe8
Zoe Cope <zoe.cope@hmcts.net>
2023-03-27 15:16:47 +0100

    amend

--- sa.tf
+++ sa.tf
@@ -42,7 +42,6 @@ module "sa_storage_account" {
   ip_rules                        = var.ip_rules
   default_action                  = "Deny"
   enable_data_protection          = true
-  enable_change_feed              = true
 
   private_endpoint_subnet_id = azurerm_subnet.endpoint_subnet.id
 
@@ -89,7 +88,6 @@ module "ingestsa_storage_account" {
   ip_rules                        = var.ip_rules
   default_action                  = "Deny"
   enable_data_protection          = true
-  enable_change_feed              = true
 
   private_endpoint_subnet_id = azurerm_subnet.endpoint_subnet.id
 
