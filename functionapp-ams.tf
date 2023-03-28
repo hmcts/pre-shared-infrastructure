22379d550c4de57aabfe3c8012ce213f6c055ccf
Zoe Cope <zoe.cope@hmcts.net>
2023-03-28 19:02:49 +0100

    add new storage accounts as part of module

--- functionapp-ams.tf
+++ functionapp-ams.tf
@@ -36,9 +36,6 @@ module "ams_function_app" {
     # "WEBSITE_CONTENTSHARE"     = ""
     # "WEBSITE_RUN_FROM_PACKAGE" = ""
   }
-
-  storage_account_name = module.sa_storage_account.storageaccount_name
-  storage_account_key  = module.sa_storage_account.storageaccount_primary_access_key
 }
 
 # resource "azurerm_storage_account" "zc_storage" {
@@ -66,9 +63,6 @@ module "zc_function_app" {
 
   # app_insights_key = azurerm_application_insights.appinsight.instrumentation_key
   app_settings = {}
-
-  storage_account_name = azurerm_storage_account.zc_storage.name
-  storage_account_key  = azurerm_storage_account.zc_storage.primary_access_key
 }
 
 # resource "azurerm_function_app_function" "content_key_policy" {
