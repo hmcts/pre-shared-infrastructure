5074874d0085364438c1a652a628748f1559eca1
Zoe Cope <zoe.cope@hmcts.net>
2023-03-28 17:16:02 +0100

    env var

--- functionapp-ams.tf
+++ functionapp-ams.tf
@@ -10,6 +10,7 @@ module "ams_function_app" {
   location            = var.location
   worker_count        = 1
   common_tags         = var.common_tags
+  env                 = var.env
 
   # app_insights_key = azurerm_application_insights.appinsight.instrumentation_key
   app_settings = {
