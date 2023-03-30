e4740cdd9dbe24856877a67bf8887884c3a911fc
Zoe Cope <zoe.cope@hmcts.net>
2023-03-30 11:53:18 +0100

    vault

--- modules/backup_vault/main.tf
+++ modules/backup_vault/main.tf
@@ -1,39 +1,27 @@
-resource "azurerm_data_protection_backup_vault" "this" {
+data "azurerm_data_protection_backup_vault" "this" {
   name                = "${var.product}-backup-vault-${var.env}"
   resource_group_name = var.resource_group_name
-  location            = var.location
-  datastore_type      = "VaultStore"
-  redundancy          = "LocallyRedundant"
-  tags                = var.tags
-  identity {
-    type = "SystemAssigned"
-  }
 }
 
-# data "azurerm_data_protection_backup_vault" "this" {
-#   name                = "${var.product}-backup-vault-${var.env}"
-#   resource_group_name = var.resource_group_name
-# }
+output "azurerm_data_protection_backup_vault_principal_id" {
+  value = data.azurerm_data_protection_backup_vault.example.identity.0.principal_id
+}
 
 resource "azurerm_role_assignment" "sa_backup_contributor" {
   scope                = var.storage_account_id
   role_definition_name = "Storage Account Backup Contributor"
-  principal_id         = azurerm_data_protection_backup_vault.this.identity[0].principal_id
-
-  depends_on = [
-    azurerm_data_protection_backup_vault.this
-  ]
+  principal_id         = data.azurerm_data_protection_backup_vault.example.identity.0.principal_id
 }
 
 resource "azurerm_data_protection_backup_policy_blob_storage" "this" {
   name               = "${var.product}-backup-policy-${var.env}"
-  vault_id           = azurerm_data_protection_backup_vault.this.id
+  vault_id           = data.azurerm_data_protection_backup_vault.this.id
   retention_duration = var.retention_duration
 }
 
 resource "azurerm_data_protection_backup_instance_blob_storage" "this" {
   name               = "${var.storage_account_id}-backup-${var.env}"
-  vault_id           = azurerm_data_protection_backup_vault.this.id
+  vault_id           = data.azurerm_data_protection_backup_vault.this.id
   location           = var.location
   storage_account_id = var.storage_account_id
   backup_policy_id   = azurerm_data_protection_backup_policy_blob_storage.this.id
