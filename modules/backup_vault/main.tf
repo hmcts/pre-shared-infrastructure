d4bf642c0732f64735d8917866906f3a34b13d45
Zoe Cope <zoe.cope@hmcts.net>
2023-03-30 09:52:59 +0100

    correct backups module to not use data source

--- modules/backup_vault/main.tf
+++ modules/backup_vault/main.tf
@@ -10,27 +10,30 @@ resource "azurerm_data_protection_backup_vault" "this" {
   }
 }
 
-data "azurerm_data_protection_backup_vault" "this" {
-  name                = "${var.product}-backup-vault-${var.env}"
-  resource_group_name = "var.resource_group_name"
-}
+# data "azurerm_data_protection_backup_vault" "this" {
+#   name                = "${var.product}-backup-vault-${var.env}"
+#   resource_group_name = var.resource_group_name
+# }
 
 resource "azurerm_role_assignment" "sa_backup_contributor" {
   scope                = var.storage_account_id
   role_definition_name = "Storage Account Backup Contributor"
-  principal_id         = data.azurerm_data_protection_backup_vault.this.identity[0].principal_id
-}
+  principal_id         = azurerm_data_protection_backup_vault.this.identity[0].principal_id
 
+  depends_on = [
+    azurerm_data_protection_backup_vault.this
+  ]
+}
 
 resource "azurerm_data_protection_backup_policy_blob_storage" "this" {
   name               = "${var.product}-backup-policy-${var.env}"
-  vault_id           = data.azurerm_data_protection_backup_vault.this.id
+  vault_id           = azurerm_data_protection_backup_vault.this.id
   retention_duration = var.retention_duration
 }
 
 resource "azurerm_data_protection_backup_instance_blob_storage" "this" {
   name               = "${var.storage_account_id}-backup-${var.env}"
-  vault_id           = data.azurerm_data_protection_backup_vault.this.id
+  vault_id           = azurerm_data_protection_backup_vault.this.id
   location           = var.location
   storage_account_id = var.storage_account_id
   backup_policy_id   = azurerm_data_protection_backup_policy_blob_storage.this.id
