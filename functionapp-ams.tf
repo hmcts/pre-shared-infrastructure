d0de6650ab494d88bdcacb082c0be3b42350916c
Zoe Cope <zoe.cope@hmcts.net>
2023-03-31 10:47:02 +0100

    No sa key

--- functionapp-ams.tf
+++ functionapp-ams.tf
@@ -1,7 +1,7 @@
-data "azurerm_key_vault_secret" "sa_key" {
-  name         = "ams-sa-key"
-  key_vault_id = data.azurerm_key_vault.keyvault.id
-}
+# data "azurerm_key_vault_secret" "sa_key" {
+#   name         = "ams-sa-key"
+#   key_vault_id = data.azurerm_key_vault.keyvault.id
+# }
 
 data "azurerm_key_vault_secret" "symmetrickey" {
   name         = "symmetrickey"
@@ -41,8 +41,8 @@ module "ams_function_app" {
     "TOKENENDPOINT"                     = "https://login.microsoftonline.com/531ff96d-0ae9-462a-8d2d-bec7c0b42082/oauth2/token"
     "AZURE_RESOURCE_GROUP"              = "pre-${var.env}"
     "AZURE_SUBSCRIPTION_ID"             = "${data.azurerm_subscriptions.current.id}"
-    "AZURE_STORAGE_ACCOUNT_KEY"         = "${data.azurerm_key_vault_secret.sa_key}"
-    "AZURE_CLIENT_SECRET"               = "${data.azurerm_key_vault_secret.client_secret}"
+    # "AZURE_STORAGE_ACCOUNT_KEY"         = ""
+    "AZURE_CLIENT_SECRET" = "${data.azurerm_key_vault_secret.client_secret}"
   }
 }
 
