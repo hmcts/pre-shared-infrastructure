95cef0bdb1c39ec21dfbbbdcbc3b05fa137c3508
darren.bayliss <d.bayliss@virgin.net>
2023-04-13 16:15:20 +0100

    Reverted back to use the original backup build as many resources only need to be created once

--- sa-final.tf
+++ sa-final.tf
@@ -27,19 +27,19 @@ module "finalsa_storage_account" {
 }
 
 
-module "finalsa_backup" {
-  count  = var.env == "stg" || var.env == "prod"  || var.env == "sbox" ? 1 : 0
-  source = "git@github.com:hmcts/pre-shared-infrastructure.git//modules/backup_vault?ref=sandbox"
+#module "finalsa_backup" {
+#  count  = var.env == "stg" || var.env == "prod"  || var.env == "sbox" ? 1 : 0
+#  source = "git@github.com:hmcts/pre-shared-infrastructure.git//modules/backup_vault?ref=sandbox"
 
-  env                  = var.env
-  product              = var.product
-  resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_name = "${var.product}finalsa${var.env}"
-  location             = var.location
-  storage_account_id   = module.finalsa_storage_account.storageaccount_id
-  tags                 = var.common_tags
-  retention_duration   = var.retention_duration
-}
+#  env                  = var.env
+#  product              = var.product
+#  resource_group_name  = azurerm_resource_group.rg.name
+#  storage_account_name = "${var.product}finalsa${var.env}"
+#  location             = var.location
+#  storage_account_id   = module.finalsa_storage_account.storageaccount_id
+#  tags                 = var.common_tags
+#  retention_duration   = var.retention_duration
+#}
 
 resource "azurerm_key_vault_secret" "finalsa_storage_account_connection_string" {
   name         = "finalsa-storage-account-connection-string"
