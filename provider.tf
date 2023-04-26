4c82351920e1f33ab23b8febbca80651a3c959fb
darren.bayliss <d.bayliss@virgin.net>
2023-04-26 11:57:32 +0100

    Adding provider for postgres network

--- provider.tf
+++ provider.tf
@@ -52,3 +52,10 @@ provider "azurerm" {
   skip_provider_registration = true
   features {}
 }
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
