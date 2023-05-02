dc684648d40deaa4ce099f7fd763afdbf95b2e95
Zoe Cope <zoe.cope@hmcts.net>
2023-05-02 21:08:17 +0100

    add env to function app name

--- modules/function_app/main.tf
+++ modules/function_app/main.tf
@@ -54,7 +54,7 @@ resource "azurerm_windows_function_app" "this" {
 
 resource "azurerm_linux_function_app" "this" {
   count               = var.os_type == "Linux" ? 1 : 0
-  name                = var.name
+  name                = "${var.name}-${var.env}"
   resource_group_name = var.resource_group_name
   location            = var.location
 
