08dc5565a5e9440207b475db9f0e9ac219df3569
Zoe Cope <zoe.cope@hmcts.net>
2023-05-02 20:35:16 +0100

    add timestamp function app, rm zc function app

--- functionapp-ams.tf
+++ functionapp-ams.tf
@@ -45,19 +45,3 @@ module "ams_function_app" {
     "AZURE_CLIENT_SECRET"               = "${data.azurerm_key_vault_secret.client_secret.value}"
   }
 }
-
-module "zc_function_app" {
-  source              = "git@github.com:hmcts/pre-shared-infrastructure.git//modules/function_app?ref=preview"
-  os_type             = "Linux"
-  product             = var.product
-  create_service_plan = true
-
-  resource_group_name = data.azurerm_resource_group.rg.name
-  name                = "zc-function-app"
-  location            = var.location
-  common_tags         = var.common_tags
-  env                 = var.env
-
-  # app_insights_key = azurerm_application_insights.appinsight.instrumentation_key
-  app_settings = {}
-}
