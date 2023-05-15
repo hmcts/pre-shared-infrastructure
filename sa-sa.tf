2a99bc77c0f6e5db3e9222d83146f0c4f3c97294
darren.bayliss <d.bayliss@virgin.net>
2023-05-15 11:16:35 +0100

    Adding access for backup SPN to get keys and enumerate storage account

--- sa-sa.tf
+++ sa-sa.tf
@@ -24,3 +24,10 @@ resource "azurerm_key_vault_secret" "sa_storage_account_connection_string" {
   value        = module.sa_storage_account.storageaccount_primary_connection_string
   key_vault_id = module.key-vault.key_vault_id
 }
+
+# Used in storage account backups to get key and list containers
+resource "azurerm_role_assignment" "powerapp_appreg_sa_cont" {
+  scope                = module.sa_storage_account.storageaccount_id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = var.dts_pre_backup_appreg_oid
+}
