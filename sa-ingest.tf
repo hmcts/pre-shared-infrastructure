0e57dacec4d9aecc354e2c0282d1d2ae6f7390a9
Zoe Cope <zoe.cope@hmcts.net>
2023-05-30 11:10:15 +0100

    Enable private endpoints for AMS and storage

--- sa-ingest.tf
+++ sa-ingest.tf
@@ -15,8 +15,7 @@ module "ingestsa_storage_account" {
   restore_policy_days             = var.restore_policy_days
   managed_identity_object_id      = data.azurerm_user_assigned_identity.managed-identity.principal_id
   enable_change_feed              = true
-
-  # private_endpoint_subnet_id = azurerm_subnet.endpoint_subnet.id
+  private_endpoint_subnet_id      = azurerm_subnet.endpoint_subnet.id
   role_assignments = [
     "Storage Blob Data Contributor"
   ]
