afe5ffdcee5c60895075be37c694d2d4dae02108
Zoe Cope <zoe.cope@hmcts.net>
2023-06-02 10:57:56 +0100

    add retention days to sa metrics

--- sa-ingest.tf
+++ sa-ingest.tf
@@ -50,6 +50,7 @@ resource "azurerm_monitor_diagnostic_setting" "storageblobingestsa" {
 
     retention_policy {
       enabled = true
+      days    = 14
     }
   }
 
@@ -59,6 +60,7 @@ resource "azurerm_monitor_diagnostic_setting" "storageblobingestsa" {
 
     retention_policy {
       enabled = true
+      days    = 14
     }
   }
 
@@ -68,6 +70,7 @@ resource "azurerm_monitor_diagnostic_setting" "storageblobingestsa" {
 
     retention_policy {
       enabled = true
+      days    = 14
     }
   }
 
@@ -77,6 +80,7 @@ resource "azurerm_monitor_diagnostic_setting" "storageblobingestsa" {
 
     retention_policy {
       enabled = true
+      days    = 14
     }
   }
 }
