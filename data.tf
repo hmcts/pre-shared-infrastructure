76d33fe8d879da7899feb1c2f46a51eda429e58c
Zoe Cope <zoe.cope@hmcts.net>
2023-06-12 16:17:47 +0100

    add data.tf

---
+++ data.tf
@@ -0,0 +1,5 @@
+data "azurerm_log_analytics_workspace" "loganalytics" {
+  provider            = azurerm.oms
+  name                = module.log_analytics_workspace.name
+  resource_group_name = module.log_analytics_workspace.resource_group_name
+}
