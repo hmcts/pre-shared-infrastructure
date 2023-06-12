c13f61e27a70523dab4cc56fbeae5e8034ff08c3
Zoe Cope <zoe.cope@hmcts.net>
2023-06-12 16:29:29 +0100

    move roles

--- data.tf
+++ data.tf
@@ -1,5 +1,45 @@
+data "azurerm_subscription" "current" {}
+
+data "azurerm_resource_group" "rg" {
+  name = "${var.product}-${var.env}"
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azuread_application" "appreg" {
+  display_name = "dts_pre_${var.env}"
+}
 data "azurerm_log_analytics_workspace" "loganalytics" {
   provider            = azurerm.oms
   name                = module.log_analytics_workspace.name
   resource_group_name = module.log_analytics_workspace.resource_group_name
+}
+
+data "azurerm_user_assigned_identity" "managed-identity" {
+  name                = "${var.product}-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
+data "azuread_groups" "groups" {
+  display_names = ["DTS-PRE-VideoEditing-SecurityGroup-${var.env}"]
+}
+
+data "azuread_groups" "pre-groups" {
+  display_names = ["DTS Pre-recorded Evidence"]
+}
+
+data "azurerm_subnet" "jenkins_subnet" {
+  provider             = azurerm.mgmt
+  name                 = "iaas"
+  virtual_network_name = local.mgmt_network_name
+  resource_group_name  = local.mgmt_network_rg_name
+}
+
+
+# adding this to allow postgres pipeline agent located on ss-ptlsbox-vnet to access keyvault
+data "azurerm_subnet" "pipelineagent_subnet" {
+  provider             = azurerm.mgmt
+  name                 = "aks-00"
+  virtual_network_name = local.mgmt_network_name
+  resource_group_name  = local.mgmt_network_rg_name
 }
--- main.tf
+++ main.tf
@@ -26,20 +26,3 @@ resource "azurerm_resource_group" "rg_backup" {
   location = var.location_backup
   tags     = var.common_tags
 }
-
-
-data "azurerm_subnet" "jenkins_subnet" {
-  provider             = azurerm.mgmt
-  name                 = "iaas"
-  virtual_network_name = local.mgmt_network_name
-  resource_group_name  = local.mgmt_network_rg_name
-}
-
-
-# adding this to allow postgres pipeline agent located on ss-ptlsbox-vnet to access keyvault
-data "azurerm_subnet" "pipelineagent_subnet" {
-  provider             = azurerm.mgmt
-  name                 = "aks-00"
-  virtual_network_name = local.mgmt_network_name
-  resource_group_name  = local.mgmt_network_rg_name
-}
