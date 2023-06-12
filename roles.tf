5088d3f57de8d624ff8021ef2f92b9814d93dc12
Zoe Cope <zoe.cope@hmcts.net>
2023-06-12 16:34:17 +0100

    move roles

--- roles.tf
+++ roles.tf
@@ -1,59 +1,16 @@
-
-data "azurerm_user_assigned_identity" "managed-identity" {
-  name                = "${var.product}-${var.env}-mi"
-  resource_group_name = "managed-identities-${var.env}-rg"
-}
-
-data "azuread_groups" "groups" {
-  display_names = ["DTS-PRE-VideoEditing-SecurityGroup-${var.env}"]
-}
-
-data "azuread_groups" "pre-groups" {
-  display_names = ["DTS Pre-recorded Evidence"]
-}
-
-# #Storage Blob Data Contributor Role Assignment for Managed Identity
-# resource "azurerm_role_assignment" "pre_BlobContributor_mi" {
-#   scope                            = azurerm_resource_group.rg.id
-#   role_definition_name             = "Storage Blob Data Contributor"
-#   principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id #var.pre_mi_principal_id
-#   skip_service_principal_aad_check = true
-# }
-
-#resource "azurerm_role_assignment" "mi_storage_1" {
-#  scope                            = module.ingestsa_storage_account.storageaccount_id
-#  role_definition_name             = "Storage Blob Data Contributor"
-#  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id #var.pre_mi_principal_id
-#  skip_service_principal_aad_check = true
-#}
-
-#resource "azurerm_role_assignment" "mi_storage_2" {
-#  scope                            = module.finalsa_storage_account.storageaccount_id
-#  role_definition_name             = "Storage Blob Data Contributor"
-#  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id #var.pre_mi_principal_id
-#  skip_service_principal_aad_check = true
-#}
-
 resource "azurerm_role_assignment" "pre_reader_mi" {
   scope                            = azurerm_resource_group.rg.id
   role_definition_name             = "Reader"
-  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
+  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id
   skip_service_principal_aad_check = true
 }
 
 resource "azurerm_role_assignment" "vm_user_mi" {
   scope                            = azurerm_resource_group.rg.id
   role_definition_name             = "Virtual Machine Contributor"
-  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id 
+  principal_id                     = data.azurerm_user_assigned_identity.managed-identity.principal_id # var.pre_mi_principal_id
   skip_service_principal_aad_check = true
 }
-
-#resource "azurerm_role_assignment" "vm_user_aa" {
-#  scope                            = azurerm_resource_group.rg.id
-#  role_definition_name             = "Virtual Machine Contributor"
-#  principal_id                     = azurerm_automation_account.pre-aa.identity[0].principal_id
-#  skip_service_principal_aad_check = true
-#}
 
 # Give PowerApp Appreg contributor access to resource groups
 resource "azurerm_role_assignment" "powerapp_appreg" {
