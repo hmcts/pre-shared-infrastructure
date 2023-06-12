48f64428baf61cfc64f51775dfd258ef21cb3873
Zoe Cope <zoe.cope@hmcts.net>
2023-06-12 16:37:47 +0100

    move roles

--- functionapp-ams.tf
+++ functionapp-ams.tf
@@ -3,8 +3,6 @@
 #   key_vault_id = data.azurerm_key_vault.keyvault.id
 # }
 
-data "azurerm_subscription" "current" {}
-
 module "ams_function_app" {
   source              = "git@github.com:hmcts/pre-shared-infrastructure.git//modules/function_app?ref=preview"
   os_type             = "Linux"
