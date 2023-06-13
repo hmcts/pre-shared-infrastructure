451a2fcb0451d3f0f056d6e2afc7d1eff548ffc5
Zoe Cope <zoe.cope@hmcts.net>
2023-06-13 13:52:04 +0100

    more refactoring

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
+locals {
+  prefix              = "${var.product}-${var.env}"
+  resource_group_name = local.prefix
+  key_vault_name      = "${var.product}-kv-${var.env}"
+  env_long_name       = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
+
+  mgmt_network_name    = var.mgmt_net_name
+  mgmt_network_rg_name = var.mgmt_net_rg_name
+}
+
 module "log_analytics_workspace" {
   source      = "git@github.com:hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
   environment = var.env
 }
 
-locals {
-  prefix                     = "${var.product}-${var.env}"
-  resource_group_name        = local.prefix
-  prefix_backup              = "${var.product}-${var.env}-backup"
-  resource_group_name_backup = local.prefix_backup
-  key_vault_name             = "${var.product}-kv-${var.env}"
-  env_long_name              = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
-
-  mgmt_network_name    = var.mgmt_net_name
-  mgmt_network_rg_name = var.mgmt_net_rg_name
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = local.resource_group_name
@@ -22,7 +21,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_resource_group" "rg_backup" {
-  name     = local.resource_group_name_backup
+  name     = "${var.product}-${var.env}-backup"
   location = var.location_backup
   tags     = var.common_tags
 }
