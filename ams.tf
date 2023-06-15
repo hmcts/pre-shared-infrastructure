3d8befa5e0302264006888ee58220096370a2805
Zoe Cope <zoe.cope@hmcts.net>
2023-06-15 17:06:59 +0100

    Update content key policy

--- ams.tf
+++ ams.tf
@@ -93,8 +93,8 @@ resource "azurerm_media_content_key_policy" "ams_default_policy" {
     clear_key_configuration_enabled = true
     token_restriction {
       token_type                  = "Jwt"
-      audience                    = ""
-      issuer                      = ""
+      audience                    = "api://${var.pre_ent_appreg_app_id}"
+      issuer                      = "https://sts.windows.net/531ff96d-0ae9-462a-8d2d-bec7c0b42082/"
       primary_symmetric_token_key = "tokenSigningKey"
     }
   }
