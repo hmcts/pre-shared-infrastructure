65e5db58ce1cbdbd5387ad915f953fc8ac906e91
Zoe Cope <zoe.cope@hmcts.net>
2023-06-15 18:07:38 +0100

    Add subnets to sa sa

--- sa-sa.tf
+++ sa-sa.tf
@@ -12,6 +12,7 @@ module "sa_storage_account" {
   enable_data_protection          = true
   restore_policy_days             = var.restore_policy_days
   enable_change_feed              = true
+  sa_subnets                      = concat([data.azurerm_subnet.jenkins_subnet.id], [azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
   private_endpoint_subnet_id      = azurerm_subnet.endpoint_subnet.id
 
   common_tags = var.common_tags
