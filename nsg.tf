9f6b8129c9b2437a386071711693400bf8715dc3
Zoe Cope <zoe.cope@hmcts.net>
2023-06-16 15:49:08 +0100

    flag off changes not to go to prod

--- nsg.tf
+++ nsg.tf
@@ -1,4 +1,5 @@
 resource "azurerm_network_security_group" "dg_nsg" {
+  count               = local.dg_env_to_deploy
   name                = "dg-nsg-${var.env}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -7,6 +8,7 @@ resource "azurerm_network_security_group" "dg_nsg" {
 }
 
 resource "azurerm_network_security_rule" "dg_allow_outbound" {
+  count             = local.dg_env_to_deploy
   name              = "dg_allow_outbound"
   priority          = 1000
   direction         = "Outbound"
@@ -22,10 +24,11 @@ resource "azurerm_network_security_rule" "dg_allow_outbound" {
   source_address_prefix       = var.data_gateway_snet_address
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.dg_nsg.name
+  network_security_group_name = azurerm_network_security_group.dg_nsg[count.index].name
 }
 
 resource "azurerm_subnet_network_security_group_association" "dg_subnet_nsg" {
+  count                     = local.dg_env_to_deploy
   subnet_id                 = azurerm_subnet.datagateway_subnet.id
-  network_security_group_id = azurerm_network_security_group.dg_nsg.id
+  network_security_group_id = azurerm_network_security_group.dg_nsg[count.index].id
 }
