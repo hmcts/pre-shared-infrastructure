5716f2c2ab4018190f8be68cb1b7c2e2da03df6f
Zoe Cope <zoe.cope@hmcts.net>
2023-06-19 09:32:11 +0100

    remove pe from sa sa

--- sa-sa.tf
+++ sa-sa.tf
@@ -11,8 +11,8 @@ module "sa_storage_account" {
   default_action                  = "Deny"
   enable_data_protection          = true
   managed_identity_object_id      = data.azurerm_user_assigned_identity.managed_identity.principal_id
-  sa_subnets                      = concat([data.azurerm_subnet.jenkins_subnet.id], [data.azurerm_subnet.endpoint_subnet.id], [data.azurerm_subnet.datagateway_subnet.id], [data.azurerm_subnet.videoedit_subnet.id])
-  private_endpoint_subnet_id      = data.azurerm_subnet.endpoint_subnet.id
+  sa_subnets                      = concat([data.azurerm_subnet.jenkins_subnet.id], [azurerm_subnet.endpoint_subnet.id], [azurerm_subnet.datagateway_subnet.id], [azurerm_subnet.videoeditvm_subnet.id])
+  # private_endpoint_subnet_id      = data.azurerm_subnet.endpoint_subnet.id
 
   role_assignments = [
     "Storage Blob Data Contributor"
