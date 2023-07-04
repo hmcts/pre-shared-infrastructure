57fdcdc8fcc6d77bb72444488a119c84659461b6
Zoe Cope <zoe.cope@hmcts.net>
2023-07-04 11:40:07 +0100

    amend script

--- scripts/datagateway-vm/datagateway-init.ps1
+++ scripts/datagateway-vm/datagateway-init.ps1
@@ -37,28 +37,18 @@ Start-Process -Wait -FilePath "dotnet-install.exe" -ArgumentList "/q /norestart"
 # use Powershell 7 from this point
 pwsh
 
-$Psversion = (Get-Host).Version
-if ($Psversion.Major -ge 7)
-{
-    if (!(Get-Module "DataGateway")) {
-        Install-Module -Name DataGateway -Force
-    }
+Import-Module DataGateway
+Install-Module -Name DataGateway -Force
 
-    # Connect to Azure and select the key vault
+# Gateway Login
+Connect-DataGatewayServiceAccount -ApplicationId $clientId -ClientSecret $clientSecret -Tenant $Tenant
 
-    # Gateway Login
-    Connect-DataGatewayServiceAccount -ApplicationId $clientId -ClientSecret $clientSecret -Tenant $Tenant
+# Install Gateway
+Install-DataGateway -AcceptConditions
 
-    # Install Gateway
-    Install-DataGateway -AcceptConditions
+# Configure Gateway
+Add-DataGatewayCluster -OverwriteExistingGateway -RegionKey uksouth -Name $GatewayName -RecoveryKey $recoveryKey
 
-    # Configure Gateway
-    Add-DataGatewayCluster -OverwriteExistingGateway -RegionKey uksouth -Name $GatewayName -RecoveryKey $recoveryKey
-
-    $gateway = (Get-DataGatewayCluster -RegionKey uksouth)[$gatewayNumber].Id
-    # Add User as Admin
-    Add-DataGatewayClusterUser -GatewayClusterId $gateway -PrincipalObjectId $userIDToAddasAdmin -AllowedDataSourceTypes $null -Role Admin -RegionKey uksouth
-}
-else {
-    exit 1
-}
+$gateway = (Get-DataGatewayCluster -RegionKey uksouth)[$gatewayNumber].Id
+# Add User as Admin
+Add-DataGatewayClusterUser -GatewayClusterId $gateway -PrincipalObjectId $userIDToAddasAdmin -AllowedDataSourceTypes $null -Role Admin -RegionKey uksouth
