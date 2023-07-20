f40418ee41f4c4bda623225365115cfa50bc2d08
Zoe Cope <zoe.cope@hmcts.net>
2023-07-20 09:18:17 +0100

    rm depends_on

--- sa-sa.tf
+++ sa-sa.tf
@@ -16,7 +16,6 @@ module "sa_storage_account" {
   private_endpoint_subnet_id      = azurerm_subnet.endpoint_subnet.id
 
   common_tags = var.common_tags
-  depends_on  = [module.key-vault, module.vnet_peer_to_hub]
 }
 
 resource "azurerm_key_vault_secret" "sa_storage_account_connection_string" {
