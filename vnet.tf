3fc0e9fa37d6813ef14d6652c56ab5814e1cf50f
Khaled Siddat <khaled.siddat@justice.gov.uk>
2023-07-28 11:11:48 +0100

    ukwest removal

--- vnet.tf
+++ vnet.tf
@@ -62,11 +62,6 @@ locals {
         peering_name = "hubUkS"
         next_hop_ip  = "10.11.72.36"
       }
-      ukWest = {
-        name         = "ukw-hub-nonprodi"
-        peering_name = "hubUkW"
-        next_hop_ip  = "10.49.72.36"
-      }
     }
     sbox = {
       subscription = "ea3a8c1e-af9d-4108-bc86-a7e2d267f49c"
@@ -75,11 +70,6 @@ locals {
         peering_name = "hubUkS"
         next_hop_ip  = "10.10.200.36"
       }
-      ukWest = {
-        name         = "ukw-hub-sbox-int"
-        peering_name = "hubUkW"
-        next_hop_ip  = "10.48.200.36"
-      }
     }
     prod = {
       subscription = "0978315c-75fe-4ada-9d11-1eb5e0e0b214"
@@ -87,12 +77,6 @@ locals {
         name         = "hmcts-hub-prod-int"
         peering_name = "hubUkS"
         next_hop_ip  = "10.11.8.36"
-      }
-      ukWest = {
-        name         = "ukw-hub-prod-int"
-        peering_name = "hubUkW"
-        next_hop_ip  = "10.49.8.36"
-      }
     }
   }
 
@@ -103,8 +87,7 @@ locals {
   }
 
   regions = [
-    "ukSouth",
-    "ukWest"
+    "ukSouth"
   ]
 
   hub_name = [for x in keys(local.hub_to_env_mapping) : x if contains(local.hub_to_env_mapping[x], var.env)][0]
