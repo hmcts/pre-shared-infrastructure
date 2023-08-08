d7603dc0f5a5f6604a5881f38a1dc66813159c5c
Zoe Cope <zoe.cope@hmcts.net>
2023-08-08 17:08:36 +0100

    remove data gateway custom_data

--- vm-edit.tf
+++ vm-edit.tf
@@ -176,15 +176,3 @@ resource "azurerm_key_vault_secret" "edit_password" {
   value        = random_password.vm_password[count.index].result
   key_vault_id = data.azurerm_key_vault.pre_kv.id
 }
-
-#experimental - to be removed
-resource "azurerm_role_assignment" "edit_admin_db" {
-  scope                = data.azurerm_resource_group.rg.id
-  role_definition_name = "Virtual Machine Administrator Login"
-  principal_id         = "a0c6507c-299c-4f46-96c6-8275d2c45242"
-}
-resource "azurerm_role_assignment" "edit_admin_zc" {
-  scope                = data.azurerm_resource_group.rg.id
-  role_definition_name = "Virtual Machine Administrator Login"
-  principal_id         = "3083d1ef-be3d-4c21-a98a-ce52b97dcdc3"
-}
