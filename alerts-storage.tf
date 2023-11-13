9309b5bf7eb667e5951993b14d98ed64656296b3
Zoe Cope <zoe.cope@hmcts.net>
2023-11-13 16:19:00 +0000

    make storage account name

--- alerts-storage.tf
+++ alerts-storage.tf
@@ -2,7 +2,7 @@ resource "azurerm_monitor_metric_alert" "storage_final_alert_capacity" {
   count               = var.env == "sbox" ? 1 : 0
   name                = "used_capacity"
   resource_group_name = data.azurerm_resource_group.rg.name
-  scopes              = module.finalsa_storage_account.storageaccount_id
+  scopes              = module.finalsa_storage_account.storageaccount_name
   description         = "When the used storage capacity is over 4TiB"
 
   criteria {
@@ -21,7 +21,7 @@ resource "azurerm_monitor_metric_alert" "storage_ingest_alert_capacity" {
   count               = var.env == "sbox" ? 1 : 0
   name                = "used_capacity"
   resource_group_name = data.azurerm_resource_group.rg.name
-  scopes              = module.ingestsa_storage_account.storageaccount_id
+  scopes              = module.ingestsa_storage_account.storageaccount_name
   description         = "When the used storage capacity is over 4TiB"
 
   criteria {
