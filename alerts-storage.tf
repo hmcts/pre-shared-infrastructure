9a96192e6c17db4a34e5e073d13ba4d3df933dea
Zoe Cope <zoe.cope@hmcts.net>
2023-11-13 16:33:30 +0000

    capitals

--- alerts-storage.tf
+++ alerts-storage.tf
@@ -7,7 +7,7 @@ resource "azurerm_monitor_metric_alert" "storage_final_alert_capacity" {
 
   criteria {
     metric_namespace = "Microsoft.Storage/storageAccounts"
-    metric_name      = "used_capacity"
+    metric_name      = "UsedCapacity"
     aggregation      = "Total"
     operator         = "GreaterThan"
     threshold        = 4294967296 # 4 TiB in bytes (1 TiB = 2^40 bytes)
@@ -19,7 +19,7 @@ resource "azurerm_monitor_metric_alert" "storage_final_alert_capacity" {
 
 resource "azurerm_monitor_metric_alert" "storage_ingest_alert_capacity" {
   count               = var.env == "sbox" ? 1 : 0
-  name                = "used_capacity"
+  name                = "UsedCapacity"
   resource_group_name = data.azurerm_resource_group.rg.name
   scopes              = [module.ingestsa_storage_account.storageaccount_id]
   description         = "When the used storage capacity is over 4TiB"
