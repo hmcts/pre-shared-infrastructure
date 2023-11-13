43ee5b7224cd2ca55baf568fba5503c9b82cfde8
Zoe Cope <zoe.cope@hmcts.net>
2023-11-13 16:26:33 +0000

    to list

--- alerts-vms.tf
+++ alerts-vms.tf
@@ -1,27 +1,8 @@
-resource "azurerm_monitor_metric_alert" "vm_alert_cpu_utilization" {
-  count               = var.env == "sbox" ? 1 : 0
-  name                = "vm_cpu_utilization_80"
-  resource_group_name = data.azurerm_resource_group.rg.name
-  scopes              = [module.data_store_db_v14.instance_id]
-  description         = "Whenever the cpu utilization is greater than 80"
-
-  criteria {
-    metric_namespace = "Microsoft.Compute/virtualMachines"
-    metric_name      = "cpu_percent"
-    aggregation      = "Average"
-    operator         = "GreaterThan"
-    threshold        = 80
-  }
-  action {
-    action_group_id = azurerm_monitor_action_group.pre-support.id
-  }
-}
-
 resource "azurerm_monitor_metric_alert" "vm_alert_availability" {
   count               = var.env == "sbox" ? 1 : 0
   name                = "vm_availability"
   resource_group_name = data.azurerm_resource_group.rg.name
-  scopes              = "${module.edit_vm.*.vm_id[count.index]}"
+  scopes              = [module.edit_vm.*.vm_id[count.index]]
   description         = "Whenever the vm is unavailabile"
 
   criteria {
@@ -36,23 +17,42 @@ resource "azurerm_monitor_metric_alert" "vm_alert_availability" {
   }
 }
 
-resource "azurerm_monitor_metric_alert" "vm_alert_memory" {
-  count               = var.env == "sbox" ? 1 : 0
-  name                = "vm_memory_percent_95"
-  resource_group_name = data.azurerm_resource_group.rg.name
-  scopes              = [module.data_store_db_v14.instance_id]
-  description         = "Whenever the memory utilization is greater than 95"
+# resource "azurerm_monitor_metric_alert" "vm_alert_cpu_utilization" {
+#   count               = var.env == "sbox" ? 1 : 0
+#   name                = "vm_cpu_utilization_80"
+#   resource_group_name = data.azurerm_resource_group.rg.name
+#   scopes              = [module.data_store_db_v14.instance_id]
+#   description         = "Whenever the cpu utilization is greater than 80"
 
-  criteria {
-    metric_namespace = "Microsoft.DBforPostgreSQL/flexibleServers"
-    metric_name      = "memory_percent"
-    aggregation      = "Average"
-    operator         = "GreaterThan"
-    threshold        = 95
-  }
-  window_size = "PT30M"
-  frequency   = "PT5M"
-  action {
-    action_group_id = azurerm_monitor_action_group.pre-support.id
-  }
-}
+#   criteria {
+#     metric_namespace = "Microsoft.Compute/virtualMachines"
+#     metric_name      = "cpu_percent"
+#     aggregation      = "Average"
+#     operator         = "GreaterThan"
+#     threshold        = 80
+#   }
+#   action {
+#     action_group_id = azurerm_monitor_action_group.pre-support.id
+#   }
+# }
+
+# resource "azurerm_monitor_metric_alert" "vm_alert_memory" {
+#   count               = var.env == "sbox" ? 1 : 0
+#   name                = "vm_memory_percent_95"
+#   resource_group_name = data.azurerm_resource_group.rg.name
+#   scopes              = [module.data_store_db_v14.instance_id]
+#   description         = "Whenever the memory utilization is greater than 95"
+
+#   criteria {
+#     metric_namespace = "Microsoft.DBforPostgreSQL/flexibleServers"
+#     metric_name      = "memory_percent"
+#     aggregation      = "Average"
+#     operator         = "GreaterThan"
+#     threshold        = 95
+#   }
+#   window_size = "PT30M"
+#   frequency   = "PT5M"
+#   action {
+#     action_group_id = azurerm_monitor_action_group.pre-support.id
+#   }
+# }
