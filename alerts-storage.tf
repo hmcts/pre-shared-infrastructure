626f63572829227ae51974fe9ac9867e345153f2
Zoe Cope <zoe.cope@hmcts.net>
2023-11-14 11:41:17 +0000

    switch env to be prod

--- alerts-storage.tf
+++ alerts-storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_monitor_metric_alert" "storage_final_alert_capacity" {
-  count               = var.env == "sbox" ? 1 : 0
+  count               = var.env == "prod" ? 1 : 0
   name                = "used_capacity"
   resource_group_name = data.azurerm_resource_group.rg.name
   scopes              = [module.finalsa_storage_account.storageaccount_id]
@@ -20,7 +20,7 @@ resource "azurerm_monitor_metric_alert" "storage_final_alert_capacity" {
 }
 
 resource "azurerm_monitor_metric_alert" "storage_ingest_alert_capacity" {
-  count               = var.env == "sbox" ? 1 : 0
+  count               = var.env == "prod" ? 1 : 0
   name                = "used_capacity"
   resource_group_name = data.azurerm_resource_group.rg.name
   scopes              = [module.ingestsa_storage_account.storageaccount_id]
--- alerts.tf
+++ alerts.tf
@@ -4,6 +4,7 @@ data "azurerm_key_vault_secret" "slack_monitoring_address" {
 }
 
 resource "azurerm_application_insights" "this" {
+  count               = var.env == "prod"
   name                = "pre-${var.env}-appinsights"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
@@ -11,7 +12,7 @@ resource "azurerm_application_insights" "this" {
 }
 
 resource "azurerm_monitor_action_group" "pre-support" {
-  # count    = var.env == "prod" ? 1 : 0
+  count               = var.env == "prod" ? 1 : 0
   name                = "CriticalAlertsAction"
   resource_group_name = data.azurerm_resource_group.rg.name
   short_name          = "pre-support"
@@ -21,35 +22,3 @@ resource "azurerm_monitor_action_group" "pre-support" {
     email_address = data.azurerm_key_vault_secret.slack_monitoring_address.value
   }
 }
-
-# module "pre-action-group" {
-#   # count    = var.env == "prod" ? 1 : 0
-#   source   = "git@github.com:hmcts/cnp-module-action-group?ref=master"
-#   location = "global"
-#   env      = var.env
-
-#   resourcegroup_name     = data.azurerm_resource_group.rg.name
-#   action_group_name      = "pre-support"
-#   short_name             = "pre-support"
-#   email_receiver_name    = "PRE Support Mailing List"
-#   email_receiver_address = data.azurerm_key_vault_secret.slack_monitoring_address.value
-# }
-
-# module "vms-alert" {
-#   source            = "git@github.com:hmcts/cnp-module-metric-alert?ref=master"
-#   location          = azurerm_application_insights.this.location
-#   app_insights_name = azurerm_application_insights.this.name
-
-#   alert_name                 = "vms-alert"
-#   alert_desc                 = "Triggers when a VM exception is received in a 5 minute poll."
-#   app_insights_query         = "requests | where toint(resultCode) >= 400 | sort by timestamp desc"
-#   frequency_in_minutes       = 15
-#   time_window_in_minutes     = 15
-#   severity_level             = "3"
-#   action_group_name          = azurerm_monitor_action_group.pre-support.id
-#   custom_email_subject       = "Virtual Machine Exception"
-#   trigger_threshold_operator = "GreaterThan"
-#   trigger_threshold          = 0
-#   resourcegroup_name         = data.azurerm_resource_group.rg.name
-#   common_tags                = var.common_tags
-# }
