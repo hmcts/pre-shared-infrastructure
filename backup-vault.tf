3d5023cf1781619a2aada227709c1e73a9dcb5a2
Zoe Cope <zoe.cope@hmcts.net>
2023-12-04 15:31:37 +0000

    remove comment

--- backup-vault.tf
+++ backup-vault.tf
@@ -1,6 +1,6 @@
 module "backup_vault" {
   count              = var.env == "prod" || var.env == "test"  ? 1 : 0
-  source             = "git@github.com:hmcts/pre-shared-infrastructure.git//modules/backup_vault?ref=remove_vaults" #"./modules/backup_vault"
+  source             = "git@github.com:hmcts/pre-shared-infrastructure.git//modules/backup_vault?ref=remove_vaults"
   rg_name            = data.azurerm_resource_group.rg.name
   location           = var.location
   env                = var.env
