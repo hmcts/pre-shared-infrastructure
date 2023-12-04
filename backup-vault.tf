47775a2d5fdf8dda4ba2e214591a56255e1409b6
Zoe Cope <zoe.cope@hmcts.net>
2023-12-04 15:31:10 +0000

    only create for prod and test

--- backup-vault.tf
+++ backup-vault.tf
@@ -1,11 +1,11 @@
 module "backup_vault" {
-  count              = var.env == "sbox" ? 1 : 0
+  count              = var.env == "prod" || var.env == "test"  ? 1 : 0
   source             = "git@github.com:hmcts/pre-shared-infrastructure.git//modules/backup_vault?ref=remove_vaults" #"./modules/backup_vault"
   rg_name            = data.azurerm_resource_group.rg.name
   location           = var.location
   env                = var.env
   product            = var.product
-  retention_duration = var.retention_duration #"P100D"
+  retention_duration = var.retention_duration
   storageaccount_ids = [
     module.finalsa_storage_account.storageaccount_id,
     module.ingestsa_storage_account.storageaccount_id,
