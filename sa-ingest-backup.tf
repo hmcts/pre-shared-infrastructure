f620805565e10a0b507cc03ff0a6ed52a25d31db
Zoe Cope <zoe.cope@hmcts.net>
2023-12-07 14:49:47 +0000

    keep all vaults so they don't delete yet

--- sa-ingest-backup.tf
+++ sa-ingest-backup.tf
@@ -1,5 +1,5 @@
 module "ingestsa_storage_account_backup" {
-  count                           = var.env == "prod" || var.env == "test" || var.env == "sbox" ? 1 : 0
+  # count                           = var.env == "prod" || var.env == "test" || var.env == "sbox" ? 1 : 0
   source                          = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                             = var.env
   storage_account_name            = "${var.product}ingestsabackup${var.env}"
@@ -21,7 +21,7 @@ module "ingestsa_storage_account_backup" {
 resource "azurerm_management_lock" "storage-backup-ingest" {
   count      = var.env == "prod" ? 1 : 0
   name       = "storage-backup"
-  scope      = module.ingestsa_storage_account_backup[0].storageaccount_id
+  scope      = module.ingestsa_storage_account_backup.storageaccount_id
   lock_level = "CanNotDelete"
   notes      = "prevent users from deleting storage accounts"
 }
@@ -34,8 +34,8 @@ resource "azurerm_role_assignment" "powerapp_appreg_ingest" {
 }
 
 resource "azurerm_role_assignment" "powerapp_appreg_ingestfinal" {
-  count                = var.env == "prod" || var.env == "test" ? 1 : 0
-  scope                = module.ingestsa_storage_account_backup[0].storageaccount_id
+  # count                = var.env == "prod" || var.env == "test" ? 1 : 0
+  scope                = module.ingestsa_storage_account_backup.storageaccount_id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = var.dts_pre_backup_appreg_oid
 }
