ef39178c6b2b0eeb933f7fba8072c61c1d66b147
Zoe Cope <zoe.cope@hmcts.net>
2023-12-15 14:16:49 +0000

    remove sbox again

--- sa-final-backup.tf
+++ sa-final-backup.tf
@@ -1,5 +1,5 @@
 module "finalsa_storage_account_backup" {
-  count                           = var.env == "prod" || var.env == "test" || var.env == "sbox" ? 1 : 0
+  count                           = var.env == "prod" || var.env == "test" ? 1 : 0
   source                          = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                             = var.env
   storage_account_name            = "${var.product}finalsabackup${var.env}"
@@ -12,8 +12,8 @@ module "finalsa_storage_account_backup" {
   allow_nested_items_to_be_public = false
   default_action                  = "Allow"
   enable_data_protection          = false #cannot be true as restore policy conflicts with immutability
-  immutable_enabled               = var.env == "prod" || var.env == "test" ? true : false
-  immutability_period             = var.env == "prod" || var.env == "test" ? var.immutability_period_backup : null
+  immutable_enabled               = true
+  immutability_period             = var.immutability_period_backup
 
   common_tags = var.common_tags
 }
@@ -35,7 +35,7 @@ resource "azurerm_role_assignment" "powerapp_appreg_final" {
 }
 
 resource "azurerm_role_assignment" "powerapp_appreg_finalbackup" {
-  count                = var.env == "prod" || var.env == "test" || var.env == "sbox" ? 1 : 0
+  count                = var.env == "prod" || var.env == "test" ? 1 : 0
   scope                = module.finalsa_storage_account_backup[0].storageaccount_id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = var.dts_pre_backup_appreg_oid
