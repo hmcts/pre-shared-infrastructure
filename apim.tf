24f9066ffbae2a40cedf6327d17af71109ec62d3
Jason Paige <jasonpaige@users.noreply.github.com>
2024-01-09 16:07:26 +0000

    Set the API to not require a subcription (#687)
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

--- apim.tf
+++ apim.tf
@@ -15,19 +15,20 @@ module "ams_product" {
 }
 
 module "ams_api" {
-  count          = local.env_to_deploy
-  source         = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
-  name           = "pre-api"
-  api_mgmt_rg    = "ss-${var.env}-network-rg"
-  api_mgmt_name  = "sds-api-mgmt-${var.env}"
-  display_name   = "Pre Recorded Evidence API"
-  revision       = "9"
-  product_id     = module.ams_product[0].product_id
-  path           = "pre-api"
-  service_url    = var.apim_service_url
-  swagger_url    = "https://raw.githubusercontent.com/hmcts/cnp-api-docs/master/docs/specs/pre-api.json"
-  content_format = "openapi+json-link"
-  protocols      = ["http", "https"]
+  count                 = local.env_to_deploy
+  source                = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
+  name                  = "pre-api"
+  api_mgmt_rg           = "ss-${var.env}-network-rg"
+  api_mgmt_name         = "sds-api-mgmt-${var.env}"
+  display_name          = "Pre Recorded Evidence API"
+  revision              = "9"
+  product_id            = module.ams_product[0].product_id
+  path                  = "pre-api"
+  service_url           = var.apim_service_url
+  swagger_url           = "https://raw.githubusercontent.com/hmcts/cnp-api-docs/master/docs/specs/pre-api.json"
+  content_format        = "openapi+json-link"
+  protocols             = ["http", "https"]
+  subscription_required = false
 }
 
 module "pre-api-mgmt-api-policy" {
