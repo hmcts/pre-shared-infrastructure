6388f4bf67ff4bbd403f1e526016cad2aa11f278
Jason Paige <jasonpaige@users.noreply.github.com>
2024-01-17 11:52:31 +0000

    Create 2 APIm subscriptions and store keys in KV (#692)

--- apim.tf
+++ apim.tf
@@ -31,6 +31,50 @@ module "ams_api" {
   subscription_required = false
 }
 
+module "apim_subscription_smoketest" {
+  count            = local.env_to_deploy
+  sub_display_name = "pre-api smoke test subscription"
+  source           = "git@github.com:hmcts/cnp-module-api-mgmt-subscription?ref=master"
+  api_mgmt_name    = "sds-api-mgmt-${var.env}"
+  api_mgmt_rg      = "ss-${var.env}-network-rg"
+  state            = "active"
+  allow_tracing    = var.env == "stg" || var.env == "demo" ? true : false
+}
+resource "azurerm_key_vault_secret" "apim_subscription_smoketest_primary_key" {
+  count        = local.env_to_deploy
+  name         = "apim-sub-smoketest-primary-key"
+  value        = module.apim_subscription_smoketest[0].subscription_primary_key
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
+resource "azurerm_key_vault_secret" "apim_subscription_smoketest_secondary_key" {
+  count        = local.env_to_deploy
+  name         = "apim-sub-smoketest-secondary-key"
+  value        = module.apim_subscription_smoketest[0].subscription_secondary_key
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
+
+module "apim_subscription_powerplatform" {
+  count            = local.env_to_deploy
+  sub_display_name = "PRE Power Platform subscription"
+  source           = "git@github.com:hmcts/cnp-module-api-mgmt-subscription?ref=master"
+  api_mgmt_name    = "sds-api-mgmt-${var.env}"
+  api_mgmt_rg      = "ss-${var.env}-network-rg"
+  state            = "active"
+  allow_tracing    = var.env == "stg" || var.env == "demo" ? true : false
+}
+resource "azurerm_key_vault_secret" "apim_subscription_powerplatform_primary_key" {
+  count        = local.env_to_deploy
+  name         = "apim-sub-powerplatform-primary-key"
+  value        = module.apim_subscription_powerplatform[0].subscription_primary_key
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
+resource "azurerm_key_vault_secret" "apim_subscription_powerplatform_secondary_key" {
+  count        = local.env_to_deploy
+  name         = "apim-sub-powerplatform-secondary-key"
+  value        = module.apim_subscription_powerplatform[0].subscription_secondary_key
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
+
 module "pre-api-mgmt-api-policy" {
   count         = local.env_to_deploy
   source        = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
