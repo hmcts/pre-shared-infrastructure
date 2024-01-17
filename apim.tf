ee86ee0d417b7c66c8ccc1d7915b63760acce35a
Jason Paige <j@jasonpaige.co.uk>
2024-01-17 11:42:52 +0000

    fix

--- apim.tf
+++ apim.tf
@@ -38,7 +38,7 @@ module "apim_subscription_smoketest" {
   api_mgmt_name    = "sds-api-mgmt-${var.env}"
   api_mgmt_rg      = "ss-${var.env}-network-rg"
   state            = "active"
-  allowing_tracing = var.env == "stg" || var.env == "demo" ? true : false
+  allow_tracing    = var.env == "stg" || var.env == "demo" ? true : false
 }
 resource "azurerm_key_vault_secret" "apim_subscription_smoketest_primary_key" {
   name         = "apim-sub-smoketest-primary-key"
@@ -58,7 +58,7 @@ module "apim_subscription_powerplatform" {
   api_mgmt_name    = "sds-api-mgmt-${var.env}"
   api_mgmt_rg      = "ss-${var.env}-network-rg"
   state            = "active"
-  allowing_tracing = var.env == "stg" || var.env == "demo" ? true : false
+  allow_tracing    = var.env == "stg" || var.env == "demo" ? true : false
 }
 resource "azurerm_key_vault_secret" "apim_subscription_powerplatform_primary_key" {
   name         = "apim-sub-powerplatform-primary-key"
