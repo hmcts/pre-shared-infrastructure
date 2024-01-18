0e03354bddae1dd4cbf1c339462509e2c01b2a3d
Zoe Cope <zoe.cope@hmcts.net>
2024-01-18 10:09:49 +0000

    typo

--- postgres.tf
+++ postgres.tf
@@ -1,37 +1,37 @@
-# module "data_store_db_v14" {
-#   providers = {
-#     azurerm.postgres_network = azurerm.postgres_network
-#   }
+module "data_store_db_v14" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
 
-#   source = "git@github.com:hmcts/terraform-module-postgresql-flexible.git?ref=master"
-#   env    = var.env
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible.git?ref=master"
+  env    = var.env
 
-#   product       = var.product
-#   component     = var.product
-#   business_area = var.project
+  product       = var.product
+  component     = var.product
+  business_area = var.project
 
-#   common_tags = var.common_tags
-#   name        = var.database_name
-#   pgsql_databases = [
-#     {
-#       name : "pre-pdb-${var.env}"
-#     },
-#     {
-#       name : "api"
-#     }
-#   ]
+  common_tags = var.common_tags
+  name        = var.database_name
+  pgsql_databases = [
+    {
+      name : "pre-pdb-${var.env}"
+    },
+    {
+      name : "api"
+    }
+  ]
 
-#   pgsql_version         = "14"
-#   backup_retention_days = 35
+  pgsql_version         = "14"
+  backup_retention_days = 35
 
-#   location             = var.location
-#   resource_group_name  = data.azurerm_resource_group.rg.name
-#   pgsql_admin_username = var.pgsql_admin_username
-#   pgsql_storage_mb     = var.pgsql_storage_mb
+  location             = var.location
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  pgsql_admin_username = var.pgsql_admin_username
+  pgsql_storage_mb     = var.pgsql_storage_mb
 
-#   admin_user_object_id = var.jenkins_AAD_objectId
+  admin_user_object_id = var.jenkins_AAD_objectId
 
-# }
+}
 
 # @todo remove once we have switched PowerApps to use the API
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
