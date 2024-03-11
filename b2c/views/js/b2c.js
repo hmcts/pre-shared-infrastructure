a137ce407581e6845730a55b8c8287bd04b4a15c
Jason Paige <j@jasonpaige.co.uk>
2024-03-11 11:13:41 +0000

    Move js to external file

---
+++ b2c/views/js/b2c.js
@@ -0,0 +1,57 @@
+function moveForgotPassword() {
+  const forgotPasswordId = 'forgotPassword'
+  const forgotPassword = document.getElementById(forgotPasswordId);
+
+  if (forgotPassword) {
+    const text = forgotPassword.innerHTML;
+    forgotPassword.remove();
+
+    const newForgotPassword = document.createElement('a');
+    newForgotPassword.id = forgotPasswordId;
+    newForgotPassword.innerHTML = text;
+    newForgotPassword.href = forgotPassword.href;
+
+    const div = document.createElement('div');
+    div.appendChild(newForgotPassword);
+
+    const passwordInput = document.getElementById('password');
+    passwordInput.parentNode.insertBefore(div, passwordInput.nextSibling);
+  }
+}
+function moveRetryCode() {
+  const retryCode = document.getElementById('retryCode');
+
+  if (retryCode) {
+    const text = retryCode.innerHTML;
+    retryCode.remove();
+
+    const newRetryCode = document.createElement('a');
+    newRetryCode.id = 'retryCode';
+    newRetryCode.innerHTML = text;
+
+    const div = document.createElement('div');
+    div.appendChild(newRetryCode);
+
+    const verificationCodeInput = document.getElementById('verificationCode');
+    verificationCodeInput.parentNode.insertBefore(div, verificationCodeInput.nextSibling);
+  }
+}
+
+function addTsAndCsLink() {
+  const tsAndCsBlock = document.getElementsByClassName('CheckboxMultiSelect')[0];
+  if (tsAndCsBlock) {
+    const tsAndCsLabel = tsAndCsBlock.getElementsByTagName('label')[0];
+    const tsAndCsText = tsAndCsBlock.getElementsByTagName('label')[1];
+    // fix the wrong for attribute supplied by B2C
+    tsAndCsLabel.setAttribute('for', tsAndCsText.getAttribute('for'));
+    if (tsAndCsText) {
+      tsAndCsText.innerHTML = tsAndCsText.innerHTML = 'I agree to the <a href="https://pre-portal.staging.platform.hmcts.net/terms-and-conditions" target="_blank">Terms &amp; Conditions</a> (including Acceptable Use) for the Section 28 Video on Demand Portal.'
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  moveForgotPassword();
+  moveRetryCode();
+  addTsAndCsLink();
+});
