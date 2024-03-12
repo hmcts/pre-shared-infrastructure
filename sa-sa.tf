6d533bcd090bea3b2219102a79ba1a18ee8366b4
Jason Paige <j@jasonpaige.co.uk>
2024-03-12 10:14:08 +0000

    formatting

--- sa-sa.tf
+++ sa-sa.tf
@@ -63,15 +63,17 @@ resource "azurerm_monitor_diagnostic_setting" "storageblobsa" {
 }
 
 resource "azurerm_storage_blob" "b2c_config" {
-  for_each               = { for idx, file_name in local.b2c_files : idx => file_name }
-  name                   = each.value
-  content_type           = strcontains(each.value, ".html") ? "text/html" :
+  for_each = { for idx, file_name in local.b2c_files : idx => file_name }
+  name     = each.value
+  content_type = (
+    strcontains(each.value, ".html") ? "text/html" :
     strcontains(each.value, ".css") ? "text/css" :
     strcontains(each.value, ".png") ? "image/x-png" :
     strcontains(each.value, ".ico") ? "image/x-ico" :
     strcontains(each.value, ".svg") ? "image/svg+xml" :
     strcontains(each.value, ".js") ? "application/javascript" :
     "application/octet-stream"
+  )
   content_md5            = filemd5("./b2c/views/${each.value}")
   storage_account_name   = module.sa_storage_account.storageaccount_name
   storage_container_name = local.b2c_container_name
