0af8541fcf9bd4e52820c0ff48d481cc2ac922f0
Zoe Cope <zoe.cope@hmcts.net>
2024-05-09 09:27:27 +0100

    more readable

--- b2c-templates.tf
+++ b2c-templates.tf
@@ -1,5 +1,8 @@
 locals {
   b2c_file_paths = fileset(path.module, "b2c/views/**")
+  asset_file     = ["png", "svg", "ico", "woff", "woff2"]
+  content_file   = ["css", "html"]
+
   b2c_file_details = {
     for b2c_file_path in local.b2c_file_paths :
     b2c_file_path => {
@@ -9,26 +12,23 @@ locals {
       content_md5   = filemd5("${path.module}/${b2c_file_path}")
       path          = "${path.module}/${b2c_file_path}"
 
-      content = contains(local.asset_file, split(".", b2c_file_path)[1]) ? "" : replace(replace(file("${path.module}/${b2c_file_path}"),
-        "{env}", var.env),
-      "{env_long_name}", local.env_long_name)
-      content_type = (split(".", b2c_file_path)[1] == "css" ? "text/css" :
-        split(".", b2c_file_path)[1] == "js" ? "application/javascript" :
-        split(".", b2c_file_path)[1] == "png" ? "image/png" :
-        split(".", b2c_file_path)[1] == "svg" ? "image/svg+xml" :
-        split(".", b2c_file_path)[1] == "ico" ? "image/x-ico" :
-        split(".", b2c_file_path)[1] == "html" ? "text/html" :
-        split(".", b2c_file_path)[1] == "woff" ? "font/woff" :
-        split(".", b2c_file_path)[1] == "woff2" ? "font/woff2" :
-      "application/octet-stream")
+      content = contains(local.asset_file, split(".", b2c_file_path)[1]) ? "" : replace(replace(file("${path.module}/${b2c_file_path}"), "{env}", var.env), "{env_long_name}", local.env_long_name)
 
+      content_type = {
+        "css"   = "text/css"
+        "js"    = "application/javascript"
+        "png"   = "image/png"
+        "svg"   = "image/svg+xml"
+        "ico"   = "image/x-ico"
+        "html"  = "text/html"
+        "woff"  = "font/woff"
+        "woff2" = "font/woff2"
+        default = "application/octet-stream"
+      }[split(".", b2c_file_path)[1]]
     }
   }
-  asset_file   = ["png", "svg", "ico", "woff", "woff2"]
-  content_file = ["css", "html"]
 
-  b2c_files = { for k, v in local.b2c_file_details : k => v }
-
+  b2c_files          = { for k, v in local.b2c_file_details : k => v }
   b2c_container_name = "${var.product}-b2c-container"
   containers = [{
     name        = local.b2c_container_name
