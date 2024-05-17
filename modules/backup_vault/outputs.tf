11328909146f8a832b7ce85803c365d0e459abf7
Jason Paige <jasonpaige@users.noreply.github.com>
2024-05-17 09:16:19 +0100

    Move backup vault to external module (#828)
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

--- modules/backup_vault/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  value       = azurerm_resource_group.this.name
-  description = "The backup resource group Resource ID."
-}
-
-output "resource_group_id" {
-  value       = azurerm_resource_group.this.id
-  description = "The backup resource group name."
-}
