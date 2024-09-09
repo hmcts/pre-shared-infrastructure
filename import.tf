a1e3e8598a498d045a375be8d2acb77d34d38bd4
damongreen123 <damon.green@btinternet.com>
2024-09-09 16:53:35 +0100

    Update import.tf
    Co-authored-by: Jason Paige <jasonpaige@users.noreply.github.com>

--- import.tf
+++ import.tf
@@ -1,4 +1,5 @@
 import {
-  to = module.edit_vm[0].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
-  id = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/pre-stg/providers/Microsoft.Compute/virtualMachines/edit-vm1-stg/extensions/Dynatrace"
+  count = var.env == "stg" ? 1 : 0
+  to    = module.edit_vm[0].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
+  id    = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/pre-stg/providers/Microsoft.Compute/virtualMachines/edit-vm1-stg/extensions/Dynatrace"
 }
