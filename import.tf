aea76848ee27f4bf95af402edcc3848419642401
damongreen123 <damon.green@btinternet.com>
2024-09-10 09:35:11 +0100

    add Dynatrace to state for VM Create import.tf (#900)
    * add Dynatrace to state for VM Create import.tf
    
    * Update import.tf
    
    Co-authored-by: Jason Paige <jasonpaige@users.noreply.github.com>
    
    * conditional import for stg only
    
    ---------
    
    Co-authored-by: Jason Paige <jasonpaige@users.noreply.github.com>
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

---
+++ import.tf
@@ -0,0 +1,9 @@
+locals {
+  vm_state_import = var.env == "stg" ? ["something"] : []
+}
+
+import {
+  for_each = local.vm_state_import
+  to       = module.edit_vm[0].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
+  id       = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/pre-stg/providers/Microsoft.Compute/virtualMachines/edit-vm1-stg/extensions/Dynatrace"
+}
