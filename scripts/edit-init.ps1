8ff0a9b766ceff9493578c452d3ef75a18191074
Jason Paige <j@jasonpaige.co.uk>
2024-09-17 09:58:36 +0100

    now run by bootstrap module

--- scripts/edit-init.ps1
+++ scripts/edit-init.ps1
@@ -6,14 +6,6 @@ $currentPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
 
 Start-Sleep -Seconds 5
 
-# Install platform required packages and modules from
-# https://raw.githubusercontent.com/hmcts/CIS-harderning/master/windows-install.ps1
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -force
-Install-Module -Name AuditPolicyDsc -force
-Install-Module -Name SecurityPolicyDsc -force
-Install-Module -Name NetworkingDsc -force
-Install-Module -Name PSDesiredStateConfiguration -force
-
 # Install PRE specific requirements
 choco feature enable -n allowGlobalConfirmation;
 choco install ffmpeg;
