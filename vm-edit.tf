bbcfc208a78b9f917088e47f860764351505e4f9
Jason Paige <jasonpaige@users.noreply.github.com>
2024-09-17 10:03:58 +0100

    Reintroduce VM Bootstrap (#908)
    * Reintroduce VM Bootstrap
    
    * maybe we don't need this import now...
    
    * remove file
    
    * now run by bootstrap module
    
    ---------
    
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

--- vm-edit.tf
+++ vm-edit.tf
@@ -49,7 +49,7 @@ module "edit_vm" {
   dynatrace_tenant_id = var.tenant_id
   dynatrace_token     = try(data.azurerm_key_vault_secret.dynatrace-token.value, null)
 
-  run_command = false
+  run_command = true
   tags        = var.common_tags
 }
 
