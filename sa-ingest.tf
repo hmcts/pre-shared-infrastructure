cb455f580ca978771974e2dee3ca34603bd3727e
damongreen123 <damon.green@btinternet.com>
2024-10-23 14:17:20 +0100

    Update sa-ingest.tf

--- sa-ingest.tf
+++ sa-ingest.tf
@@ -1,5 +1,5 @@
 module "ingestsa_storage_account" {
-  source                          = "git@github.com:hmcts/cnp-module-storage-account?ref=4.x"
+  source                          = "git@github.com:hmcts/cnp-module-storage-account?ref=fix/defender-azapi"
   env                             = var.env
   storage_account_name            = "${var.product}ingestsa${var.env}"
   resource_group_name             = data.azurerm_resource_group.rg.name
