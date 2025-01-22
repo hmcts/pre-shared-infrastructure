e98f48a92315e2ac8ce7e880bd252956ecc440b4
damongreen123 <damon.green@btinternet.com>
2025-01-22 16:01:53 +0000

    Update sa-ingest.tf

--- sa-ingest.tf
+++ sa-ingest.tf
@@ -38,7 +38,7 @@ resource "azurerm_storage_management_policy" "example" {
         operation = "=="
         value     = "true"
       }
-      prefix_match = "[a-f0-9]" # only process blobs with a name starting with a hex character
+      prefix_match = '[a-f0-9]' # only process blobs with a name starting with a hex character
     }
     actions {
       base_blob {
