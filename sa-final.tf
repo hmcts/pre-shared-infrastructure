118ef48c622b9122108a46b591a347f249b0f338
damongreen123 <damon.green@btinternet.com>
2025-02-25 10:37:14 +0000

    Update sa-final.tf

--- sa-final.tf
+++ sa-final.tf
@@ -32,11 +32,6 @@ import {
   to = module.ingestsa_storage_account.azurerm_private_endpoint.this[0]
 }
 
-import {
-  id = "/subscriptions/867a878b-cb68-4de5-9741-361ac9e178b6/resourceGroups/pre-dev/providers/Microsoft.Network/privateEndpoints/prefinalsadev"
-  to = module.finalsa_storage_account.azurerm_private_endpoint.this[0]
-}
-
 resource "azurerm_key_vault_secret" "finalsa_storage_account_connection_string" {
   name            = "finalsa-storage-account-connection-string"
   value           = module.finalsa_storage_account.storageaccount_primary_connection_string
