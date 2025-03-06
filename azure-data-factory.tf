b5167ac890bcb2344d99fe57a00414f416faffdd
Jason Paige <jasonpaige@users.noreply.github.com>
2025-03-06 08:51:06 +0000

    Add Azure Data Factory to Prod (#957)
    * Add Azure Data Factory to Prod
    
    * correct module path
    
    * Updating Terraform Formatting
    
    * linked_blob_storage...
    
    * fmt
    
    * syntax
    
    ---------
    
    Co-authored-by: 52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

---
+++ azure-data-factory.tf
@@ -0,0 +1,19 @@
+module "datafactory" {
+  count  = var.num_adf # We only need it in prod
+  source = "git@github.com:hmcts/terraform-module-azure-datafactory.git?ref=main"
+
+  env         = var.env
+  product     = "pre"
+  component   = "vodafone"
+  location    = var.location
+  common_tags = var.common_tags
+
+  existing_resource_group_name = data.azurerm_resource_group.rg.name
+
+  linked_blob_storage = {
+    "prevoda" = {
+      connection_string    = module.vodasa_storage_account.storageaccount_primary_connection_string
+      use_managed_identity = false
+    }
+  }
+}
