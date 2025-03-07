0a4654e66fede46888239ca61ab59a633bbd9e2b
Jason Paige <j@jasonpaige.co.uk>
2025-03-07 14:23:07 +0000

    Grant Jason and Damon voda SA contrib role

--- azure-data-factory.tf
+++ azure-data-factory.tf
@@ -29,9 +29,23 @@ resource "azurerm_role_assignment" "adf_jason_contrib" {
   principal_id         = "d0c32eaa-f190-4b8e-9884-adefce62b143" # Jason
 }
 
+resource "azurerm_role_assignment" "adf_jason_voda_sa_contrib" {
+  count                = var.num_adf # We only need it in prod
+  scope                = module.vodasa_storage_account.storageaccount_id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = "c12b6257-1c4b-47a7-bb78-cc6edb9df68f" # Jason
+}
+
 resource "azurerm_role_assignment" "adf_damon_contrib" {
   count                = var.num_adf # We only need it in prod
   scope                = module.datafactory[0].id
   role_definition_name = "Data Factory Contributor"
+  principal_id         = "c12b6257-1c4b-47a7-bb78-cc6edb9df68f" # Damon
+}
+
+resource "azurerm_role_assignment" "adf_damon_voda_sa_contrib" {
+  count                = var.num_adf # We only need it in prod
+  scope                = module.vodasa_storage_account.storageaccount_id
+  role_definition_name = "Storage Account Contributor"
   principal_id         = "c12b6257-1c4b-47a7-bb78-cc6edb9df68f" # Damon
 }
