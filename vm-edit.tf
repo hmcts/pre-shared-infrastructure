9a9941ced03671dc7414c13e4dcd5b625bed99d9
Jason Paige <j@jasonpaige.co.uk>
2025-04-16 11:44:14 +0100

    test

--- vm-edit.tf
+++ vm-edit.tf
@@ -115,7 +115,7 @@ SETTINGS
 
 import {
   id = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/pre-stg/providers/Microsoft.Compute/virtualMachines/edit-vm1-stg/extensions/toolingScript"
-  to = module.edit_vm[var.num_vid_edit_vms].azurerm_windows_virtual_machine.winvm.id
+  to = module.edit_vm["1"].azurerm_windows_virtual_machine.winvm.id
 }
 
 # DTS-PRE-VideoEditing-SecurityGroup-
