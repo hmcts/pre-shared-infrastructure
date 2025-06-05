b2308c667815a54c2ac5a475bfd4a2628665c99e
Damon Green <damon.green@btinternet.com>
2025-06-05 10:49:25 +0100

    Add key to KV https://tools.hmcts.net/jira/browse/S28-3935

--- sa-final-backup.tf
+++ sa-final-backup.tf
@@ -51,6 +51,6 @@ resource "azurerm_role_assignment" "powerapp_appreg_finalbackup" {
 
 resource "azurerm_key_vault_secret" "bkup-finalsa_storage_account_primary_access_key" {
   name         = "bkup-finalsa-storage-account-primary-access-key"
-  value        = module.finalsa_storage_account_backup[0].storageaccount_primary_access_key
+  value        = module.finalsa_storage_account_backup.storageaccount_primary_access_key
   key_vault_id = data.azurerm_key_vault.keyvault.id
 }
