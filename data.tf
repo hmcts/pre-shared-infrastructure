98b9496e6ebe6825830d9e0a3854abf9601fe021
Oliver Scott <oliver.scott@justice.gov.uk>
2025-08-19 11:21:41 +0100

    Use [0] for simplicity

--- data.tf
+++ data.tf
@@ -58,7 +58,7 @@ data "azurerm_resource_group" "rg-cache" {
 data "azurerm_redis_cache" "portal_redis_cache" {
   count               = var.env == "dev" ? 0 : 1
   name                = "pre-portal-${var.env}"
-  resource_group_name = data.azurerm_resource_group.rg-cache[count.index].name
+  resource_group_name = data.azurerm_resource_group.rg-cache[0].name
 }
 
 data "azurerm_virtual_network" "vnet" {
