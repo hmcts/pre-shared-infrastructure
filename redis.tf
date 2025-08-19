891383ce8e5779ed1c6a5a9bb64e6a47d2341d83
Oliver Scott <oliver.scott@justice.gov.uk>
2025-08-19 10:03:11 +0100

    Create on staging only, not test

--- redis.tf
+++ redis.tf
@@ -1,5 +1,5 @@
 resource "azurerm_monitor_metric_alert" "redis_alert_errors" {
-  count               = var.env == "prod" ? 0 : 1
+  count               = var.env == "stg" ? 1 : 0
   name                = "redis_errors"
   resource_group_name = data.azurerm_resource_group.rg-cache.name
   scopes              = [data.azurerm_redis_cache.portal_redis_cache.id]
