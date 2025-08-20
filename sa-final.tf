280ee523b2ec30558e58a57b1e4d7da2d3c7c1b7
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-08-20 14:54:18 +0100

    revert change to azurerm_monitor_metric_alert names

--- sa-final.tf
+++ sa-final.tf
@@ -80,7 +80,7 @@ resource "azurerm_monitor_diagnostic_setting" "storageblobfinalsa" {
 
 resource "azurerm_monitor_metric_alert" "storage_final_alert_capacity" {
   count               = var.env == "prod" ? 1 : 0
-  name                = "used_capacity_final"
+  name                = "used_capacity"
   resource_group_name = data.azurerm_resource_group.rg.name
   scopes              = [module.finalsa_storage_account.storageaccount_id]
   description         = "When the used storage capacity is over 4TiB"
