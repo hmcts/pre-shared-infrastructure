87d69df2194d02701ef4c246eb62947b61dfd935
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-08-20 11:52:05 +0100

    Correction to import block resource addresses

--- import.tf
+++ import.tf
@@ -1,14 +1,14 @@
 import {
-  to = module.data_store_db_v14.azurerm_postgresql_flexible_server_database["pre-pdb-stg"]
+  to = module.data_store_db_v14.azurerm_postgresql_flexible_server_database.pg_databases["pre-pdb-stg"]
   id = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/pre-stg/providers/Microsoft.DBforPostgreSQL/flexibleServers/pre-db-stg/databases/pre-pdb-stg"
 }
 
 import {
-  to = module.data_store_db_v14.azurerm_postgresql_flexible_server_database["api"]
+  to = module.data_store_db_v14.azurerm_postgresql_flexible_server_database.pg_databases["api"]
   id = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/pre-stg/providers/Microsoft.DBforPostgreSQL/flexibleServers/pre-db-stg/databases/api"
 }
 
 import {
-  to = module.data_store_db_v14.azurerm_postgresql_flexible_server_active_directory_administrator[0]
+  to = module.data_store_db_v14.azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_adadmin
   id = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/pre-stg/providers/Microsoft.DBforPostgreSQL/flexibleServers/pre-db-stg/administrators/e7ea2042-4ced-45dd-8ae3-e051c6551789"
 }
