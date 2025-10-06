8a7680bb63f265be5283115b12f102089033cd2f
Jason Paige <j@jasonpaige.co.uk>
2025-10-06 14:54:25 +0100

    Grant access to B2C app

--- pre-apim-b2c.tf
+++ pre-apim-b2c.tf
@@ -32,3 +32,24 @@ resource "azuread_application" "api_app" {
     }
   }
 }
+
+resource "azuread_service_principal" "resource_api_sp" {
+  client_id = data.azuread_application.pre_apim_b2c_app.client_id
+}
+
+# --- declare the API permission on the client (delegated scope) ---
+resource "azuread_application_api_access" "client_needs_api" {
+  application_id = data.azuread_application.pre_apim_b2c_app.client_id
+  api_client_id  = azuread_service_principal.resource_api_sp.client_id
+  scope_ids      = [
+    azuread_service_principal.resource_api_sp.oauth2_permission_scope_ids["api.request.b2c"]
+  ]
+}
+
+# --- admin consent for that client (required since scope type = "Admin") ---
+resource "azuread_service_principal_delegated_permission_grant" "client_consent" {
+  service_principal_object_id          = data.azuread_application.pre_apim_b2c_app.object_id
+  resource_service_principal_object_id = azuread_service_principal.resource_api_sp.object_id
+  consent_type                         = "AllPrincipals"
+  claim_values                         = ["api.request.b2c"]
+}
