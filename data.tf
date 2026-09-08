c6a22da2f603815d7923db70b547851a725a74b1
ruth.bovell <ruth.bovell@justice.gov.uk>
2026-09-08 16:57:34 +0100

    Use a data lookup for the Halo action group to see if plan still succeeds

--- data.tf
+++ data.tf
@@ -72,3 +72,8 @@ data "azurerm_application_insights" "app_insights" {
   name                = "pre-${var.env}-appinsights"
   resource_group_name = "${var.product}-${var.env}"
 }
+
+data "azurerm_monitor_action_group" "halo_itsm_nonprod" {
+  name                = "halo-itsm-nonprod"
+  resource_group_name = "monitoring-development-rg"
+}
--- pre-halo-test-alert.tf
+++ pre-halo-test-alert.tf
@@ -17,9 +17,9 @@ EOF
   frequency_in_minutes       = "5"
   time_window_in_minutes     = "5"
   severity_level             = "3"
-  action_group_name          = "halo-itsm-nonprod"
+  action_group_name          = data.azurerm_monitor_action_group.halo_itsm_nonprod.name
+  resourcegroup_name         = data.azurerm_monitor_action_group.halo_itsm_nonprod.resource_group_name
   custom_email_subject       = "PRE API Halo Webhook Test (Not a real alert)"
   trigger_threshold_operator = "GreaterThan"
   trigger_threshold          = "3"
-  resourcegroup_name         = "${var.product}-${var.env}"
 }
